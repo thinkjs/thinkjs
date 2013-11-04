bb1216eff6b01f5b45f70c4565b41d360fc7677d
welefen <welefen@gmail.com>
2013-11-04 17:35:02 +0800

    by welefen

--- App/Lib/Action/Home/IndexAction.class.js
+++ App/Lib/Action/Home/IndexAction.class.js
@@ -1,16 +1,7 @@
 module.exports = Action(function(){
     return {
-        init: function(){
-            
-        },
         indexAction: function(){
-            var cookie = this.cookie();
-            this.echo(cookie);
-            var delay = this.get('delay');
-            var self = this;
-            setTimeout(function(){
-                self.end(self.cookie());
-            }, delay * 1000)
+            console.log(this.http);
         },
         testAction: function(test, test2){
             this.echo("welefen");
