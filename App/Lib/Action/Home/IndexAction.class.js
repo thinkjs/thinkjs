881a2984f0ea3403b1906c24de5ac63307255b0b
welefen <welefen@gmail.com>
2013-11-05 19:50:29 +0800

    by welefen

--- App/Lib/Action/Home/IndexAction.class.js
+++ App/Lib/Action/Home/IndexAction.class.js
@@ -2,12 +2,15 @@ module.exports = Action(function(){
     return {
         indexAction: function(){
             var self = this;
-            var user = think_require("UserModel");
-            var test = think_require("TestModel");
-            test();
-            user();
-            test();
-            this.end();
+            var config = {
+                user: "root",
+                password: "SuredY0706$",
+                database: "www.ueapp.com"
+            }
+            var instance = think_require("MysqlSocket")(config);
+            instance.query("select * from meinv_group limit 1").then(function(data){
+                self.end(data);
+            });
         },
         testAction: function(test, test2){
             this.echo("welefen");
