ad309b09a8a9e13d1b8775d145055de41d0d91a1
welefen <welefen@gmail.com>
2013-11-12 19:40:31 +0800

    update

--- test/a.js
+++ test/a.js
@@ -1,9 +1,1 @@
-var b = require("./b");
-global.fn = 'welefen';
-console.log(b.value());
-b.change(10);
-console.log(b.value());
-var c = require("/Users/welefen/Develop/git/thinkjs/test/b");
-console.log(b.value());
-c.change(20);
-console.log(b.value())
+process.abort();
