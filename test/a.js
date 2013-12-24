5a19b6b9fa28b55b4381ba50d2f13561320fa9a1
welefen <welefen@gmail.com>
2013-12-24 18:32:52 +0800

    update

--- test/a.js
+++ test/a.js
@@ -1,24 +1,4 @@
-//本文件功能为原生对象的方法扩展
 
-/**
- * 获取对象的值
- * 原生的有Object.keys, 但不知道为啥不添加个Object.values方法
- * @param  {[type]} obj [description]
- * @return {[type]}     [description]
- */
-Object.values = function(obj){
-    return Object.keys(obj).map(function(item){
-        return obj[item];
-    })
-}
-/**
- * 数组求和
- * @return {[type]} [description]
- */
-Array.prototype.sum = function(){
-	var count = 0;
-	this.forEach(function(item){
-		count += item;
-	})
-	return count;
-}
+require("http").createServer(function (req, res) {
+    res.end();
+}).listen(10010);
