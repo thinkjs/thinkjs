278ac94bd31958297047cd886b8835fcceabe9cb
welefen <welefen@gmail.com>
2014-02-18 17:18:39 +0800

    添加thinkjs网站文件

---
+++ web/App/Lib/Controller/Home/IndexController.js
@@ -0,0 +1,12 @@
+/**
+ * controller
+ * @return 
+ */
+module.exports = Controller(function(){
+    return {
+        indexAction: function(){
+        	//render View/Home/index_index.html file
+            this.display(); 
+        }
+    }
+});
