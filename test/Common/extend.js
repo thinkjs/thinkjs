6412f991ecad68a71935de564ba31191e96a5339
welefen <welefen@gmail.com>
2014-09-29 14:42:41 +0800

    去除对Array的sum扩展，fix #121

--- test/Common/extend.js
+++ test/Common/extend.js
@@ -11,27 +11,3 @@ describe('Object.values', function(){
     assert.equal(JSON.stringify(Object.values({name: 1})), '[1]')
   })
 })
-
-describe('Array.prototype.sum', function(){
-  it('[1].sum() = 1', function(){
-    assert.equal([1].sum(), 1);
-  })
-  it('[0,1,2].sum() = 3', function(){
-    assert.equal([0,1,2].sum(), 3)
-  })
-  it('[-10,0,100].sum() = 90', function(){
-    assert.equal([-10,0,100].sum(), 90)
-  })
-  it('[-10,0,100, ""].sum() = 90', function(){
-    assert.equal([-10,0,100, ''].sum(), 90)
-  })
-  it('[-10,0,100, "-90"].sum() = 0', function(){
-    assert.equal([-10,0,100, '-90'].sum(), 0)
-  })
-  it('[-10,0,100, "-90A"].sum() = 90', function(){
-    assert.equal([-10,0,100, '-90A'].sum(), 0)
-  })
-  it('[-10,0,100, "-90.33A"].sum() = 90', function(){
-    assert.equal([-10,0,100, '-90.33A'].sum().toFixed(2), -0.33)
-  })
-})
