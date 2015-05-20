1a0f602a6dad56f65ce066e9cffa141b105bbf06
welefen <welefen@gmail.com>
2015-05-20 10:02:23 +0800

    rewrite locate_template middleware by es6

--- src/middleware/locate_template.js
+++ src/middleware/locate_template.js
@@ -4,36 +4,35 @@
  * @param  {Array}  
  * @return {}
  */
-module.exports = think.middleware({
+module.exports = class extends think.middleware.base {
   /**
    * run
    * @param  {String} templateFile [template filepath]
    * @return {}              []
    */
-  run: function(templateFile){
-    var http = this.http;
-    var depr = this.config('tpl.file_depr');
-    var ext = this.config('tpl.file_ext');
-    var pathPrefix = think.getPath(http.module, think.dirname.view) + '/';
+  run(templateFile){
+    let http = this.http;
+    let {file_depr, file_ext} = this.config('tpl');
+    let pathPrefix = think.getPath(http.module, think.dirname.view) + '/';
     // this.display()
     if (!templateFile) {
-      return pathPrefix + http.controller + depr + http.action + ext;
+      return pathPrefix + http.controller + file_depr + http.action + file_ext;
     }
     // this.display('detail')
     // this.display('group/detail')
     // this.display('admin/group/detail')
     // this.display('admin/group/detail.html')
-    var paths = templateFile.split('/');
-    var action = paths.pop();
-    var controller = paths.pop() || http.controller;
-    var module = paths.pop() || http.module;
+    let paths = templateFile.split('/');
+    let action = paths.pop();
+    let controller = paths.pop() || http.controller;
+    let module = paths.pop() || http.module;
     if (module !== http.module) {
       pathPrefix = think.getPath(module, think.dirname.view) + '/';
     }
-    templateFile = pathPrefix + controller + depr + action;
+    templateFile = pathPrefix + controller + file_depr + action;
     if (action.indexOf('.') === -1) {
-      templateFile += ext;
+      templateFile += file_ext;
     }
     return templateFile;
   }
-});
+}
