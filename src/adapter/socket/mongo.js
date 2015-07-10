e7b966c4825021988194fb9c41aa573807b7d6db
welefen <welefen@gmail.com>
2015-07-11 07:16:49 +0800

    update mongo socket

--- src/adapter/socket/mongo.js
+++ src/adapter/socket/mongo.js
@@ -1,4 +1,7 @@
 'use strict';
+
+import querystring from 'querystring';
+
 /**
  * mongodb socket
  */
@@ -25,17 +28,28 @@ export default class extends think.adapter.socket {
     let deferred = think.defer();
     let mongo = await think.npm('mongodb');
     let client = mongo.MongoClient;
+    let Logger = mongo.Logger;
     let auth = '';
     let config = this.config;
     if(this.config.user){
       auth = `${config.user}:${config.pwd}@`;
     }
-    let url = `mongodb://${auth}${config.host}:${config.port}/${config.name}`;
+    // connection options
+    // http://mongodb.github.io/node-mongodb-native/2.0/tutorials/urls/
+    let options = '';
+    if(config.options){
+      options = '?' + querystring.stringify(config.options);
+    }
+    let url = `mongodb://${auth}${config.host}:${config.port}/${config.name}${options}`;
     client.connect(url, this.config, (err, connection) => {
       if(err){
         deferred.reject(err);
         this.close();
       }else{
+        //set logger level
+        if(config.log_level){
+          Logger.setLevel(config.log_level);
+        }
         this.connection = connection;
         deferred.resolve(this.connection);
       }
