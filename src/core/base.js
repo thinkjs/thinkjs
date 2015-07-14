ff894490e377d92a4f0d9b3224a82f8812c03351
welefen <welefen@gmail.com>
2015-07-14 11:38:27 +0800

    remove core/_base.js

--- src/core/base.js
+++ src/core/base.js
@@ -1,13 +1,19 @@
 'use strict';
 
-import Base from './_base.js';
-
 /**
  * Base Class
  * @param  {Object} http
  * @return {Class}
  */
-export default class extends Base {
+export default class {
+  /**
+   * constructor
+   * @param  {Object} http []
+   * @return {}      []
+   */
+  constructor(...args){
+    this.init(...args);
+  }
   /**
    * init
    * @param  {Object} http []
@@ -15,6 +21,22 @@ export default class extends Base {
    */
   init(http = {}){
     this.http = http;
+  }
+  /**
+   * invoke method, support __before & __after magic methods
+   * @param  {String} method []
+   * @param  {mixed} data []
+   * @return {Promise}     []
+   */
+  async invoke(method, data = []){
+    if (think.isFunction(this.__before)) {
+      await think.co.wrap(this.__before).bind(this)(this);
+    }
+    let result = await think.co.wrap(this[method]).apply(this, data);
+    if (think.isFunction(this.__after)) {
+      await think.co.wrap(this.__after).bind(this)(this);
+    }
+    return result;
   }
   /**
    * get or set config
--- src/core/think.js
+++ src/core/think.js
@@ -11,7 +11,6 @@ import thinkit from 'thinkit';
 import co from 'co';
 import colors from 'colors/safe';
 
-import _base from './_base.js'
 import base from './base.js';
 import {} from './_cache.js';
 
@@ -97,11 +96,6 @@ think.version = (() => {
  * @type {Array}
  */
 think.module = [];
-/**
- * base class
- * @type {[type]}
- */
-think._base = _base;
 /**
  * base class
  * @type {Class}
