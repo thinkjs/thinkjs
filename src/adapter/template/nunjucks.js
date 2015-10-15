f938c50afa32dadbfe7042b590431d87ee86dee9
liuguanyu <lgy573@126.com>
2015-10-15 14:25:27 +0800

    add nunjucks template engine adaptor

---
+++ src/adapter/template/nunjucks.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import nunjucks from 'nunjucks';
+
+/**
+ * swig template
+ * @type {Class}
+ */
+export default class extends think.adapter.template {
+  /**
+   * run
+   * @param  {String} templateFile [template filepath]
+   * @param  {Object} tVar         [data]
+   * @return {String}              []
+   */
+  async run(templateFile, tVar, config){
+    nunjucks.configure(think.extend({}, think.config('view.options'), config && config.options));
+    return nunjucks.render(templateFile, tVar);
+  }
+}
