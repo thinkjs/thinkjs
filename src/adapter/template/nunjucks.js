cc06e9c42917fc067e7a4337463adef5bd602e07
lichengyin <welefen@gmail.com>
2015-10-16 09:59:16 +0800

    support nunjucks template

---
+++ src/adapter/template/nunjucks.js
@@ -0,0 +1,26 @@
+'use strict';
+/**
+ * nunjucks template
+ * @type {Class}
+ */
+export default class extends think.adapter.template {
+  /**
+   * run
+   * @param  {String} templateFile []
+   * @param  {Object} tVar         []
+   * @return {Promise}             []
+   */
+  async run(templateFile, tVar, config){
+    let nunjucks = await think.npm('nunjucks');
+    
+    let conf = think.extend({
+      autoescape: true,
+      watch: false,
+      noCache: false,
+      throwOnUndefined: false
+    }, think.config('view.options'), config && config.options);
+
+    nunjucks.configure('viewpath', conf);
+    return nunjucks.render(templateFile, tVar);
+  }
+}
--- src/config/package.js
+++ src/config/package.js
@@ -10,5 +10,6 @@ export default {
   mongodb: '2.0.36',
   memjs: '0.8.5',
   sockjs: '0.3.15',
+  nunjucks: '2.1.0',
   'socket.io': '1.3.6'
 };
