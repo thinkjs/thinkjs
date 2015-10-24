b3c688752e0aff8fa838afdb9806794bc30cbb1f
lichengyin <welefen@gmail.com>
2015-10-24 10:45:17 +0800

    reverse order in app_begin hook

--- src/config/hook.js
+++ src/config/hook.js
@@ -6,7 +6,7 @@ export default {
   resource_check: ['resource'],
   resource_output: ['output_resource'],
   route_parse: ['rewrite_pathname', 'subdomain_deploy', 'route'],
-  app_begin: ['check_csrf', 'read_html_cache'],
+  app_begin: ['read_html_cache', 'check_csrf'],
   view_init: [],
   view_template: ['locate_template'],
   view_parse: ['parse_template'],
