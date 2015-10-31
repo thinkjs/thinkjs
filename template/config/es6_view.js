5b84a93f54c5543867e552af92f390f717a7ed49
lichengyin <welefen@gmail.com>
2015-10-31 19:59:50 +0800

    move type to first in view config template

--- template/config/es6_view.js
+++ template/config/es6_view.js
@@ -3,10 +3,10 @@
  * template config
  */
 export default {
+  type: 'ejs',
   content_type: 'text/html',
   file_ext: '.html',
   file_depr: '_',
   root_path: think.ROOT_PATH + '/view',
-  type: 'ejs',
   options: {}
 };
--- template/config/view.js
+++ template/config/view.js
@@ -4,10 +4,10 @@
  * @type {Object}
  */
 module.exports = {
+  type: 'ejs',
   content_type: 'text/html',
   file_ext: '.html',
   file_depr: '_',
   root_path: think.ROOT_PATH + '/view',
-  type: 'ejs',
   options: {}
 };
