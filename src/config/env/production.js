95434584a69b0e903f5e1ab09bfb9e3fe87a7a90
lichengyin <welefen@gmail.com>
2015-11-02 09:22:30 +0800

    fix can not auto exit process when in cli mode, fix #187

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -2,6 +2,19 @@
 /**
  * configs in production enviroment
  */
-export default {
 
-};
+let configs = {};
+
+if(think.cli){
+  configs = think.extend(configs, {
+    gc: {
+      on: false
+    },
+    auto_reload: false,
+    log_pid: false,
+    process_timeout: 0
+  });
+}
+
+
+export default configs;
--- src/config/env/testing.js
+++ src/config/env/testing.js
@@ -2,6 +2,19 @@
 /**
  * configs in testing enviroment
  */
-export default {
 
-};
+let configs = {};
+
+if(think.cli){
+  configs = think.extend(configs, {
+    gc: {
+      on: false
+    },
+    auto_reload: false,
+    log_pid: false,
+    process_timeout: 0
+  });
+}
+
+
+export default configs;
