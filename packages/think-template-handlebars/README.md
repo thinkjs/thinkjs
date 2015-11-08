818fffc9e5babbc51b6d3a614d9aeea9eb8daceb
lichengyin <welefen@gmail.com>
2015-11-08 14:41:40 +0800

    release 1.0.0

--- packages/think-template-handlebars/README.md
+++ packages/think-template-handlebars/README.md
@@ -1,2 +1,39 @@
 # think-template-handlebars
-handlebars template adapter for ThinkJS 2.0
+
+handlebars template adapter for ThinkJS
+
+## Install
+
+```sh
+npm install think-template-handlebars
+```
+
+## How to use
+
+### register adapter
+
+```js
+import HandlebarseAdapter from 'think-template-handlebars';
+think.adapter('template', 'handlebars', HandlebarseAdapter);
+```
+
+add above code in bootstrap file, like `src/common/boostrap/adapter.js`.
+
+### change view type
+
+change viwe type in file `src/common/config/view.js`,
+
+```js
+export default {
+  type: 'handlebars',
+  adapter: {
+    handlebars: { //handlebars options
+
+    }
+  }
+}
+```
+
+### precompiled
+
+you can precompile handlebars template before deployed. it will auto identified when render template.
---
+++ packages/think-template-handlebars/src/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+import handlebars from 'handlebars';
+
+let Base = think.adapter('template', 'base');
+
+/**
+ * handlebars template adapter
+ */
+export default class extends Base {
+  /**
+   * run
+   * @param  {String} templateFile []
+   * @param  {Object} tVar         []
+   * @param  {Object} config       []
+   * @return {Promise}              []
+   */
+  async run(templateFile, tVar, config){
+    let options = this.mergeConfig(config);
+
+    this.prerender(options, handlebars);
+
+    let content = await this.getContent(templateFile);
+
+    if(options.compiled || content.slice(0, 13) === '{"compiler":['){
+      let data = (new Function('', `return ${content}`))();
+      return handlebars.template(data)(tVar);
+    }
+
+    return handlebars.compile(content, options)(tVar);
+  }
+}
