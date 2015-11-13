8f4bbf8cfc12df754b2cac1b42201859c47ca3e7
lichengyin <welefen@gmail.com>
2015-11-13 17:00:51 +0800

    update dependency package version

--- src/config/package.js
+++ src/config/package.js
@@ -4,12 +4,12 @@
  */
 export default {
   redis: '2.3.0',
-  sqlite3: '3.0.8',
-  ejs: '2.3.2',
+  sqlite3: '3.1.1',
+  ejs: '2.3.4',
   jade: '1.11.0',
-  mongodb: '2.0.36',
-  memjs: '0.8.5',
+  mongodb: '2.0.48',
+  memjs: '0.8.7',
   sockjs: '0.3.15',
   nunjucks: '2.1.0',
-  'socket.io': '1.3.6'
+  'socket.io': '1.3.7'
 };
