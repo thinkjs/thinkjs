c23f054ef1305f98ec3c5642f681e9f1d08a69a8
lichengyin <welefen@gmail.com>
2015-12-02 10:37:08 +0800

    use path.sep replacement /

--- template/www/production.js
+++ template/www/production.js
@@ -4,7 +4,7 @@ var path = require('path');
 var rootPath = path.dirname(__dirname);
 
 var instance = new thinkjs({
-  APP_PATH: rootPath + '/app',
+  APP_PATH: rootPath + path.sep + 'app',
   ROOT_PATH: rootPath,
   RESOURCE_PATH: __dirname,
   env: 'production'
