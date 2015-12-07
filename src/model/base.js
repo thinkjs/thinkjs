344b91271adcf069bc9ccb7712009842c2929c98
lichengyin <welefen@gmail.com>
2015-12-07 10:36:30 +0800

    fix error when use alias in countSelect method

--- src/model/base.js
+++ src/model/base.js
@@ -397,9 +397,10 @@ export default class extends Base {
     let pk = this.pk;
     let table = options.alias || this.getTableName();
 
+    //delete table options avoid error when has alias
+    delete options.table;
+
     if(!count){
-      //get count
-      delete options.alias;
       count = await this.options(options).count(`${table}.${pk}`);
     }
 
