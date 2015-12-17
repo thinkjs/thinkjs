da4754508c46dfc626565292df3d986ea8fee282
lichengyin <welefen@gmail.com>
2015-12-17 15:50:11 +0800

    fix windows test error

--- test/core/think.js
+++ test/core/think.js
@@ -21,7 +21,7 @@ think.APP_PATH = path.dirname(__dirname) + think.sep + 'testApp';
 var _http = require('../_http.js');
 
 function getHttp(config, options){
-  think.APP_PATH = path.dirname(__dirname) + '/testApp';
+  think.APP_PATH = path.dirname(__dirname) + think.sep + 'testApp';
   var req = think.extend({}, _http.req);
   var res = think.extend({}, _http.res);
   return think.http(req, res).then(function(http){
