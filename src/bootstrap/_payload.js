e12cdd13227caaf6a51ba7ce39f1438f01955a9d
lichengyin <welefen@gmail.com>
2015-12-17 16:16:21 +0800

    fix windows test error

--- src/bootstrap/_payload.js
+++ src/bootstrap/_payload.js
@@ -22,7 +22,7 @@ think.middleware('parse_form_payload', http => {
   }
 
   let deferred = think.defer();
-  let uploadDir = think.config('post.file_upload_path') || (os.tmpdir() + '/thinkjs_upload');
+  let uploadDir = think.config('post.file_upload_path') || (os.tmpdir() + think.sep + 'thinkjs_upload');
   think.mkdir(uploadDir);
   
   let postConfig = think.config('post');
@@ -76,7 +76,7 @@ think.middleware('parse_single_file_payload', http => {
   }
   
   let deferred = think.defer();
-  let uploadDir = think.config('post.file_upload_path') || (os.tmpdir() + '/thinkjs_upload');
+  let uploadDir = think.config('post.file_upload_path') || (os.tmpdir() + think.sep + 'thinkjs_upload');
   think.mkdir(uploadDir);
 
   let name = think.uuid(20);
