11d9da66485a837729afeb2f68be0abad9d87b59
lichengyin <welefen@gmail.com>
2015-12-24 09:15:29 +0800

    optimise for invoke __before & __after method

--- src/core/base.js
+++ src/core/base.js
@@ -25,35 +25,18 @@ export default class {
     
   }
   /**
-   * before magic method
-   * @return {} []
-   */
-  __before(){
-
-  }
-  /**
-   * after magic method
-   * @return {} []
-   */
-  __after(){
-
-  }
-  /**
    * invoke method, support __before & __after magic methods
    * @param  {String} method []
    * @param  {mixed} data []
    * @return {Promise}    []
    */
   async invoke(method, ...data){
-    if (think.isFunction(this.__before)) {
+    if (this.__before) {
       await think.co(this.__before(this));
-      //await think.co.wrap(this.__before).bind(this)(this);
     }
     let result = await think.co(this[method](...data));
-    //let result = await think.co.wrap(this[method]).apply(this, data);
-    if (think.isFunction(this.__after)) {
+    if (this.__after) {
       await think.co(this.__after(this));
-      //await think.co.wrap(this.__after).bind(this)(this);
     }
     return result;
   }
