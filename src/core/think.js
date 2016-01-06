244813fb972fd0dfce4cba4e9d371f03b46635c0
lichengyin <welefen@gmail.com>
2016-01-06 17:46:30 +0800

    separate route to single file

--- src/core/think.js
+++ src/core/think.js
@@ -19,6 +19,7 @@ import Await from '../util/await.js';
 import Validate from './think_validate.js';
 import Middleware from './think_middleware.js';
 import Hook from './think_hook.js';
+import Route from './think_route.js';
 
 import './think_cache.js';
 import './think_data.js';
@@ -159,6 +160,12 @@ think.middleware = Middleware;
  * @type {Function}
  */
 think.hook = Hook;
+
+/**
+ * route
+ * @type {Function}
+ */
+think.route = Route;
 
 /**
  * alias co module to think.co
@@ -747,84 +754,6 @@ think.alias = (type, paths, slash) => {
     });
   });
 };
-
-
-
-/**
- * load route
- * route detail config
- *
- * module.exports = {
- *   admin: {
- *     reg: /^admin/, //module reg
- *     children: [
- *       /^admin\/reg/, 'admin/index/reg'
- *     ]
- *   },
- *   home: {
- *     children: [
- *       
- *     ]
- *   }
- * }
- * 
- * @return {} []
- */
-let _getDynamicRoute = fn => {
-  return think.co(fn()).then((route = []) => {
-    thinkData.route = route;
-    return route;
-  });
-};
-
-let _getModuleRoute = config => {
-  for(let module in config){
-    let filepath = think.getPath(module, think.dirname.config) + '/route.js';
-    let moduleConfig = think.safeRequire(filepath);
-    config[module].children = moduleConfig || [];
-  }
-  thinkData.route = config;
-  return config;
-};
-/**
- * get route
- * @param  {} key []
- * @return {}     []
- */
-let _getRoute = () => {
-  let file = think.getPath(undefined, think.dirname.config) + '/route.js';
-  let config = think.safeRequire(file) || [];
-
-  //route config is funciton, may be is dynamic save in db
-  if (think.isFunction(config)) {
-    return _getDynamicRoute(config);
-  }
-  //get module route config
-  if(think.isObject(config) && think.mode === think.mode_module){
-    return _getModuleRoute(config);
-  }
-  thinkData.route = config;
-  return config;
-};
-
-think.route = routes => {
-  //remove route
-  if(routes === null){
-    thinkData.route = null;
-    return;
-  }
-  //set route
-  if (think.isArray(routes) || think.isObject(routes)) {
-    thinkData.route = routes;
-    return;
-  }
-  //get route
-  if (thinkData.route) {
-    return thinkData.route;
-  }
-  return _getRoute();
-};
-
 
 
 /**
