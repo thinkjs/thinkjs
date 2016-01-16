50d2773c9743bed65ed717e9e34880e82723bea9
lichengyin <welefen@gmail.com>
2016-01-16 15:09:12 +0800

    featurei: add model method for base service

--- src/service/base.js
+++ src/service/base.js
@@ -4,5 +4,24 @@
  * @type {Class}
  */
 export default class extends think.base {
-  
+  /**
+   * get model instance
+   * @return {} []
+   */
+  model(name, options, module){
+    if(think.isString(options) && think.module.indexOf(options) > -1){
+      module = options;
+      options = {};
+    }
+    if(!module){
+      let filename = this.__filename || __filename;
+      let seps = filename.split(think.sep).reverse();
+      module = seps[2];
+    }
+    if(think.isString(options)){
+      options = {type: options};
+    }
+    options = think.extend({}, think.config('db', undefined, module), options);
+    return think.model(name, options, module);
+  }
 }
