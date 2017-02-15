55e8f7fa709c4727d36813144dd24f4b06391582
lushijie <lushijie1218@126.com>
2017-02-15 18:46:39 +0800

    fix dir

--- packages/think-babel/test/index.js
+++ packages/think-babel/test/index.js
@@ -2,14 +2,12 @@
 * @Author: lushijie
 * @Date:   2017-02-14 10:56:08
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-02-15 18:42:58
+* @Last Modified time: 2017-02-15 18:45:50
 */
 
 const compileFileByBabel = require('../index');
-let out = compileFileByBabel('./test/src', './test/out', 'es6.es', '.js2', {
+let out = compileFileByBabel('./test/src/a', './test/out', 'b/es6.es', '.js', {
   presets: [['es2015', {'loose': true}], 'stage-1'],
   plugins: ['transform-runtime'],
   sourceMaps: true
 });
-
-console.log(out);
