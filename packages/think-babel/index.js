165b56a00f8f3b4d6163f30f418039fa5ac9acd3
lushijie <lushijie1218@126.com>
2017-02-15 20:43:43 +0800

    modify

--- packages/think-babel/index.js
+++ packages/think-babel/index.js
@@ -1,79 +1,55 @@
-const babel = require('babel-core');
-const helper = require('think-helper');
-const fs = require('fs');
-const path = require('path');
-
-/**
- * get relative path
- * @param  {String} file []
- * @return {String}      []
- */
-function getRelationPath(srcPath, outPath, file) {
-  //use dirname to resolve file path in source-map-support
-  //so must use dirname in here
-  let pPath = path.dirname(outPath + path.sep + file);
-  return path.relative(pPath, srcPath + path.sep + file);
-}
-
-/**
- * replace filepath extname
- * @param  {String} filepath []
- * @param  {String} extname  []
- * @return {String}          []
- */
-function replaceExtName(filepath, extname = '') {
-  return filepath.replace(/\.\w+$/, extname);
-}
+const babelCore = require('babel-core')
+const helper = require('think-helper')
+const fs = require('fs')
+const path = require('path')
 
 /**
  * compile es6+ file to es5
- * @param  {String} srcPath file src path
- * @param  {String} outPath file out path
- * @param  {[type]} file    file path without src
- * @param  {String} newExt  the new file ext,eg: '.js'
- * @param  {Object} options babel options
+ * @param  {Object} options
  * @return {Boolean}
  */
-function compileFileByBabel(srcPath, outPath, file, newExt, options = {}){
+function compileFileByBabel(options = {}){
+  let {srcPath, outPath, file, ext = '.js', babelOptions} = options;
+  srcPath = path.normalize(srcPath);
+  outPath = path.normalize(outPath);
+  let filePath = path.join(srcPath, file);
+  let pPath = path.dirname(outPath + path.sep + file);
+  let content = fs.readFileSync(filePath, 'utf8');
+  let relativePath = path.relative(pPath, srcPath + path.sep + file);
+
+  // babel options
+  babelOptions = Object.assign({
+    filename: file,
+    sourceFileName: relativePath
+  }, babelOptions);
+
+  // babel transform
+  let data;
   try {
-    if(helper.isObject(newExt)) {
-      options = newExt;
-      newExt = '.js';
-    }
-    newExt = newExt || '.js';
-
-    srcPath = path.normalize(srcPath);
-    outPath = path.normalize(outPath);
-    let filePath = `${srcPath}${path.sep}${file}`;
-    let content = fs.readFileSync(filePath, 'utf8');
-    let relativePath = getRelationPath(srcPath, outPath, file);
-    options = Object.assign({
-      filename: file,
-      sourceFileName: relativePath
-    }, options);
-
-    let data = babel.transform(content, options);
-    let outFile = `${outPath}${path.sep}${file}`;
-    outFile = replaceExtName(outFile, newExt);
-    helper.mkdir(path.dirname(outFile));
-
-    let basename = path.basename(file);
-    let prefix = '//# sourceMappingURL=';
-    if(data.code.indexOf(prefix) === -1 && options.sourceMaps){
-      data.code = data.code + '\n' + prefix + basename + '.map';
-    }
-    fs.writeFileSync(outFile, data.code);
-
-    if(options.sourceMaps) {
-      let sourceMap = data.map;
-      sourceMap.file = sourceMap.sources[0];
-      fs.writeFileSync(`${outFile}.map`, JSON.stringify(sourceMap, undefined, 4));
-    }
-    return true;
+    data = babelCore.transform(content, babelOptions);
   }
   catch(e) {
     throw new Error(e.message);
   }
+
+  // write es5 code file
+  let outFile = path.join(outPath, file);
+  outFile = outFile.replace(/\.\w+$/, ext);
+  helper.mkdir(path.dirname(outFile));
+  let basename = path.basename(file);
+  let prefix = '//# sourceMappingURL=';
+  if(data.code.indexOf(prefix) === -1 && babelOptions.sourceMaps){
+    data.code = data.code + '\n' + prefix + basename + '.map';
+  }
+  fs.writeFileSync(outFile, data.code);
+
+  // write souremap file
+  if(babelOptions.sourceMaps) {
+    let sourceMap = data.map;
+    sourceMap.file = sourceMap.sources[0];
+    fs.writeFileSync(outFile + '.map', JSON.stringify(sourceMap, undefined, 4));
+  }
+  return true;
 }
 
 module.exports = compileFileByBabel;
--- packages/think-babel/test/index.js
+++ packages/think-babel/test/index.js
@@ -2,7 +2,7 @@
 * @Author: lushijie
 * @Date:   2017-02-14 10:56:08
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-02-15 19:13:02
+* @Last Modified time: 2017-02-15 20:39:36
 */
 import test from 'ava'
 import helper from 'think-helper'
@@ -10,19 +10,44 @@ import compileFileByBabel from '../index'
 import path from 'path'
 import fs from 'fs'
 
-test.before(t => {
-  console.log('  Remove test output directory');
+test.serial.cb.beforeEach(t => {
   let outPath = path.join(__dirname, 'out');
-  helper.rmdir(outPath, false);
+  helper.rmdir(outPath, false).then(() => {
+    t.end();
+  });
 });
 
-test('compileFileByBabel', t => {
-  let out = compileFileByBabel('./test/src/a', './test/out', 'b/test.es', '.js', {
-    presets: [['es2015', {'loose': true}], 'stage-1'],
-    plugins: ['transform-runtime'],
-    sourceMaps: true
+test.serial('compileFileByBabel sourceMaps=true', t => {
+  let out = compileFileByBabel({
+    srcPath: './test/src/a',
+    outPath: './test/out',
+    file: 'b/test.es',
+    ext: '.js',
+    babelOptions: {
+      presets: ['es2015', 'stage-1'],
+      plugins: ['transform-runtime'],
+      sourceMaps: true
+    }
   });
   let outFile = helper.isFile(path.join(__dirname, 'out/b/test.js'));
-  let outMapFile = helper.isFile(path.join(__dirname, 'out/b/test.js.map'))
+  let outMapFile = helper.isFile(path.join(__dirname, 'out/b/test.js.map'));
   t.true(out && outFile && outMapFile);
+});
+
+
+test.serial('compileFileByBabel ext=.js2', t => {
+  let out = compileFileByBabel({
+    srcPath: './test/src/a',
+    outPath: './test/out',
+    file: 'b/test.es',
+    ext: '.js2',
+    babelOptions: {
+      presets: ['es2015', 'stage-1'],
+      plugins: ['transform-runtime'],
+      sourceMaps: false
+    }
+  });
+  let outFile = helper.isFile(path.join(__dirname, 'out/b/test.js2'));
+  let outMapFile = helper.isFile(path.join(__dirname, 'out/b/test.js2.map'));
+  t.true(out && outFile && !outMapFile);
 });
