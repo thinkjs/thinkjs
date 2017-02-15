ca2e23186720838b6247a070f681873923e8dd3d
Cathy Huang <lu.huang90@gmail.com>
2017-02-15 12:00:59 +0800

    finish README and add test cases

--- packages/think-debounce/README.md
+++ packages/think-debounce/README.md
@@ -1,2 +1,43 @@
 # think-await
-think await
+
+`think-await` runs a time-consuming operation. The operation may be called several times concurrently, but within `think-await`, it will only be run once before it's finished.
+
+In particular, You can use `think-await` to avoid duplicate requests for a remote API.
+
+## Syntax
+
+```
+think.await(key, callback)
+```
+
+- key {String} the identity of the operation.
+- callback {Function} the function which contains the operation and returns a Promise object.
+
+## Usage
+
+Take reading a local file for an example:
+
+```
+import thinkAwait from 'think-await';
+import fs from 'fs';
+
+let readTimes = 0;
+let awaitKey = 'readMyFile';
+let filePath = '../my/file/path';
+let readMyFileCallback = () => {
+  return new Promise((resolve, reject) => {
+    // even if `readMyFile` operation is called several times,
+    // `readMyFileCallback` will only be run once before it's finished.
+    readTimes ++;
+    fs.readFile(filePath, {encoding: 'utf8'}, (err, data) => {
+      if(err) reject(err);
+      resolve(data);
+    });
+  });
+}
+
+let promise1 = thinkAwait(awaitKey, readMyFileCallback);
+let promise2 = thinkAwait(awaitKey, readMyFileCallback);
+
+console.log(readTimes); // 1
+```
--- packages/think-debounce/test/index.js
+++ packages/think-debounce/test/index.js
@@ -1,1 +1,57 @@
 import test from 'ava';
+import thinkAwait from '../index.js';
+import fs from 'fs';
+
+test('await', t => {
+  let readTimes = 0;
+
+  const filePath = '../README.md';
+  const awaitKey = 'readMyFile';
+  const readFileCallback = () => {
+    return new Promise((resolve, reject) => {
+      readTimes ++;
+      fs.readFile(filePath, {encoding: 'utf8'}, (err, data) => {
+        if(err) reject(err);
+        resolve(data);
+      });
+    });
+  }
+
+  let promise1 = thinkAwait(awaitKey, readFileCallback);
+  let promise2 = thinkAwait(awaitKey, readFileCallback);
+
+
+  return Promise.all([promise1, promise2]).then(values => {
+    t.is(readTimes, 1);
+    t.is(values[0], values[1]);
+  }, reason => {
+    t.is(readTimes, 1);
+  });
+});
+
+test('await 1', t => {
+
+  let readTimes = 0;
+
+  const filePath = '../nonexistence'; // read a nonexistent file
+  const awaitKey = 'readMyFile1'; // Don't use the same key in different tests!
+  const readFileCallback = () => {
+    return new Promise((resolve, reject) => {
+      readTimes ++;
+      fs.readFile(filePath, {encoding: 'utf8'}, (err, data) => {
+        if(err) reject(err);
+        resolve(data);
+      });
+    });
+  }
+
+  let promise1 = thinkAwait(awaitKey, readFileCallback);
+  let promise2 = thinkAwait(awaitKey, readFileCallback);
+
+  return Promise.all([promise1, promise2]).then(values => {
+    t.is(readTimes, 1);
+    t.is(values[0], values[1]);
+  }, reason => {
+    t.is(readTimes, 1);
+  });
+});
