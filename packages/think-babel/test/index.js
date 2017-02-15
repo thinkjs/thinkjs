d4c1fd8375b8dc4dcaf0050dbfe76e73989f4460
lushijie <lushijie1218@126.com>
2017-02-15 15:39:59 +0800

    update package.json

--- packages/think-babel/test/index.js
+++ packages/think-babel/test/index.js
@@ -2,11 +2,8 @@
 * @Author: lushijie
 * @Date:   2017-02-14 10:56:08
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-02-15 14:55:21
+* @Last Modified time: 2017-02-15 15:39:28
 */
-// import test from 'ava';
-import {babelCompile} from '../index';
 
-//console.log(babelCompile);
-
+var babelCompile = require('../index').babelCompile;
 babelCompile('./test/src', './test/out', {shouldLog: true})
