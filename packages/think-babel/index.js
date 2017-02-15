a564fc7c8832fb43a59ad6b4fd0e6db1d117e57f
lushijie <lushijie1218@126.com>
2017-02-15 18:43:24 +0800

    think-babel

--- packages/think-babel/index.js
+++ packages/think-babel/index.js
@@ -1,68 +1,7 @@
 const babel = require('babel-core');
-// const helper = require('think-helper');
+const helper = require('think-helper');
 const fs = require('fs');
 const path = require('path');
-const {SourceMapGenerator, SourceMapConsumer} = require('source-map');
-const helper = {
-  isFile: function(p) {
-    try{
-      return fs.statSync(p).isFile();
-    }catch(e){}
-    return false;
-  },
-  mkdir: function(p, mode) {
-    mode = mode || '0777';
-    if (fs.existsSync(p)) {
-      chmod(p, mode);
-      return true;
-    }
-    let pp = path.dirname(p);
-    if (fs.existsSync(pp)) {
-      fs.mkdirSync(p, mode);
-    }else{
-      mkdir(pp, mode);
-      mkdir(p, mode);
-    }
-    return true;
-  },
-  isString: function(obj) {
-    return toString.call(obj) === '[object String]';
-  },
-  getFiles: function(dir, prefix, filter) {
-    dir = path.normalize(dir);
-
-    if (!fs.existsSync(dir)) {
-      console.log('dd', dir);
-      return [];
-    }
-    if(!helper.isString(prefix)){
-      filter = prefix;
-      prefix = '';
-    }
-    if(filter === true){
-      filter = item => {
-        return item[0] !== '.';
-      };
-    }
-    prefix = prefix || '';
-    let files = fs.readdirSync(dir);
-    let result = [];
-    files.forEach(item => {
-      let stat = fs.statSync(dir + path.sep + item);
-      if (stat.isFile()) {
-        if(!filter || filter(item)){
-          result.push(prefix + item);
-        }
-      }else if(stat.isDirectory()){
-        if(!filter || filter(item, true)){
-          let cFiles = getFiles(dir + sep + item, prefix + item + sep, filter);
-          result = result.concat(cFiles);
-        }
-      }
-    });
-    return result;
-  }
-}
 
 /**
  * get relative path
@@ -77,113 +16,63 @@ function getRelationPath(srcPath, outPath, file) {
 }
 
 /**
- * merge source map
- * @param  {String} content        []
- * @param  {Object} orginSourceMap []
- * @param  {Object} sourceMap      []
- * @return {}                []
+ * replace filepath extname
+ * @param  {String} filepath []
+ * @param  {String} extname  []
+ * @return {String}          []
  */
-function mergeSourceMap(orginSourceMap, sourceMap) {
-  sourceMap.file = sourceMap.file.replace(/\\/g, '/');
-  sourceMap.sources = sourceMap.sources.map(filePath => {
-    return filePath.replace(/\\/g, '/');
-  });
-  var generator = SourceMapGenerator.fromSourceMap(new SourceMapConsumer(sourceMap));
-  generator.applySourceMap(new SourceMapConsumer(orginSourceMap));
-  sourceMap = JSON.parse(generator.toString());
-  return sourceMap;
-}
-
-function compileByBabel(srcPath, outPath, content, file, options = {}){
-  let startTime = Date.now();
-  let {
-        presets,
-        plugins,
-        alreadyLogged,
-        shouldLog,
-        orginSourceMap
-      } = options;
-  let relativePath = getRelationPath(srcPath, outPath, file);
-  let data = babel.transform(content, {
-    filename: file,
-    presets: [].concat(presets || [['es2015', {'loose': true}], 'stage-1']),
-    plugins: [].concat(plugins || ['transform-runtime']),
-    sourceMaps: true,
-    sourceFileName: relativePath
-  });
-  if(!alreadyLogged && shouldLog){
-    //think.log(`Compile file ${file}`, 'Babel', startTime);
-    console.log(`Compile file ${file}`, 'Babel', startTime);
-  }
-  helper.mkdir(path.dirname(`${outPath}${path.sep}${file}`));
-  let basename = path.basename(file);
-  let prefix = '//# sourceMappingURL=';
-  if(data.code.indexOf(prefix) === -1){
-    data.code = data.code + '\n' + prefix + basename + '.map';
-  }
-  fs.writeFileSync(`${outPath}${path.sep}${file}`, data.code);
-  let sourceMap = data.map;
-  sourceMap.file = sourceMap.sources[0];
-  if(orginSourceMap){
-    sourceMap = mergeSourceMap(orginSourceMap, sourceMap);
-  }
-  fs.writeFileSync(`${outPath}${path.sep}${file}.map`, JSON.stringify(sourceMap, undefined, 4));
+function replaceExtName(filepath, extname = '') {
+  return filepath.replace(/\.\w+$/, extname);
 }
 
 /**
- * compile es6+ file to es5 by Babel
- * @param  {[type]} srcPath []
- * @param  {[type]} outPath []
- * @param  {[type]} options []
- * @return {}               []
+ * compile es6+ file to es5
+ * @param  {String} srcPath file src path
+ * @param  {String} outPath file out path
+ * @param  {[type]} file    file path without src
+ * @param  {String} newExt  the new file ext,eg: '.js'
+ * @param  {Object} options babel options
+ * @return {Boolean}
  */
-function babelCompile(srcPath, outPath, options) {
-  let compiledMtime = {};
-  let compiledErrorFiles = [];
-  let changedFiles = [];
-  srcPath = path.normalize(srcPath);
-  outPath = path.normalize(outPath);
-  let files = helper.getFiles(srcPath, true);
+function compileFileByBabel(srcPath, outPath, file, newExt, options = {}){
+  try {
+    if(helper.isObject(newExt)) {
+      options = newExt;
+      newExt = '.js';
+    }
 
-  files.forEach(file => {
+    srcPath = path.normalize(srcPath);
+    outPath = path.normalize(outPath);
     let filePath = `${srcPath}${path.sep}${file}`;
     let content = fs.readFileSync(filePath, 'utf8');
-    let extname = path.extname(file);
-    let mTime = fs.statSync(`${srcPath}${path.sep}${file}`).mtime.getTime();
+    let relativePath = getRelationPath(srcPath, outPath, file);
+    options = Object.assign({
+      filename: file,
+      sourceFileName: relativePath
+    }, options);
+
+    let data = babel.transform(content, options);
     let outFile = `${outPath}${path.sep}${file}`;
-    if(helper.isFile(outFile)){
-      let outmTime = fs.statSync(outFile).mtime.getTime();
-      if(outmTime >= mTime){
-        return;
-      }
+    outFile = replaceExtName(outFile, newExt);
+    helper.mkdir(path.dirname(outFile));
+
+    let basename = path.basename(file);
+    let prefix = '//# sourceMappingURL=';
+    if(data.code.indexOf(prefix) === -1 && options.sourceMaps){
+      data.code = data.code + '\n' + prefix + basename + '.map';
     }
-    if(!compiledMtime[file] || mTime > compiledMtime[file]){
-      let index = compiledErrorFiles.indexOf(file);
-      try{
-        compileByBabel(srcPath, outPath, content, file, options);
-        if(index > -1){
-          compiledErrorFiles.splice(index, 1);
-        }
-        changedFiles.push(outFile);
-        compiledMtime[file] = mTime;
-      }catch(e){
-        // think.log(colors => {
-        //   return colors.red(`compile file ${file} error`);
-        // }, 'COMPILE');
-        // think.log(e);
-        console.log(colors => {
-          return colors.red(`compile file ${file} error`);
-        }, 'BABELCOMPILE');
-        console.log(e);
-        e.message = 'Compile Error: ' + e.message;
-        if(index === -1){
-          compiledErrorFiles.push(file);
-        }
-      }
+    fs.writeFileSync(outFile, data.code);
+
+    if(options.sourceMaps) {
+      let sourceMap = data.map;
+      sourceMap.file = sourceMap.sources[0];
+      fs.writeFileSync(`${outFile}.map`, JSON.stringify(sourceMap, undefined, 4));
     }
-  })
+    return true;
+  }
+  catch(e) {
+    throw new Error(e.message);
+  }
 }
 
-module.exports = {
-  babelCompile: babelCompile
-}
+module.exports = compileFileByBabel;
--- packages/think-babel/test/index.js
+++ packages/think-babel/test/index.js
@@ -2,8 +2,14 @@
 * @Author: lushijie
 * @Date:   2017-02-14 10:56:08
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-02-15 15:39:28
+* @Last Modified time: 2017-02-15 18:42:58
 */
 
-var babelCompile = require('../index').babelCompile;
-babelCompile('./test/src', './test/out', {shouldLog: true})
+const compileFileByBabel = require('../index');
+let out = compileFileByBabel('./test/src', './test/out', 'es6.es', '.js2', {
+  presets: [['es2015', {'loose': true}], 'stage-1'],
+  plugins: ['transform-runtime'],
+  sourceMaps: true
+});
+
+console.log(out);
