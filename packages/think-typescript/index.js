5c722c75d2f12e798d66fb22baea1ca3ec2b21d1
lushijie <lushijie1218@126.com>
2017-02-16 17:12:39 +0800

    init struct

---
+++ packages/think-typescript/index.js
@@ -0,0 +1,55 @@
+const babelCore = require('babel-core')
+const helper = require('think-helper')
+const fs = require('fs')
+const path = require('path')
+
+/**
+ * compile es6+ file to es5
+ * @param  {Object} options
+ * @return {Boolean}
+ */
+function compileFileByTypescript(options = {}){
+  let {srcPath, outPath, file, ext = '.js', babelOptions} = options;
+  srcPath = path.normalize(srcPath);
+  outPath = path.normalize(outPath);
+  let filePath = path.join(srcPath, file);
+  let pPath = path.dirname(outPath + path.sep + file);
+  let content = fs.readFileSync(filePath, 'utf8');
+  let relativePath = path.relative(pPath, srcPath + path.sep + file);
+
+  // babel options
+  babelOptions = Object.assign({
+    filename: file,
+    sourceFileName: relativePath
+  }, babelOptions);
+
+  // babel transform
+  let data;
+  try {
+    data = babelCore.transform(content, babelOptions);
+  }
+  catch(e) {
+    throw new Error(e.message);
+  }
+
+  // write es5 code file
+  let outFile = path.join(outPath, file);
+  outFile = outFile.replace(/\.\w+$/, ext);
+  helper.mkdir(path.dirname(outFile));
+  let basename = path.basename(file);
+  let prefix = '//# sourceMappingURL=';
+  if(data.code.indexOf(prefix) === -1 && babelOptions.sourceMaps){
+    data.code = data.code + '\n' + prefix + basename + '.map';
+  }
+  fs.writeFileSync(outFile, data.code);
+
+  // write souremap file
+  if(babelOptions.sourceMaps) {
+    let sourceMap = data.map;
+    sourceMap.file = sourceMap.sources[0];
+    fs.writeFileSync(outFile + '.map', JSON.stringify(sourceMap, undefined, 4));
+  }
+  return true;
+}
+
+module.exports = compileFileByTypescript;
---
+++ packages/think-typescript/test/src/es6.js
@@ -0,0 +1,8 @@
+/*
+* @Author: lushijie
+* @Date:   2017-02-15 10:22:00
+* @Last Modified by:   lushijie
+* @Last Modified time: 2017-02-15 10:22:21
+*/
+
+[1,2,3].map(n => n + 1);
