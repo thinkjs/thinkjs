378dd507109b9382a5c975a82c62019bd0034c5b
lushijie <lushijie1218@126.com>
2017-02-16 18:15:53 +0800

    fix test

--- packages/think-typescript/index.js
+++ packages/think-typescript/index.js
@@ -4,60 +4,13 @@ const fs = require('fs')
 const path = require('path')
 const ts = require('typescript')
 
-/**
- * compile es6+ file to es5
- * @param  {Object} options
- * @return {Boolean}
- */
-// function compileFileByTypescript(options = {}){
-//   let {srcPath, outPath, file, ext = '.js', babelOptions} = options;
-//   srcPath = path.normalize(srcPath);
-//   outPath = path.normalize(outPath);
-//   let filePath = path.join(srcPath, file);
-//   let pPath = path.dirname(outPath + path.sep + file);
-//   let content = fs.readFileSync(filePath, 'utf8');
-//   let relativePath = path.relative(pPath, srcPath + path.sep + file);
-
-//   // babel options
-//   babelOptions = Object.assign({
-//     filename: file,
-//     sourceFileName: relativePath
-//   }, babelOptions);
-
-//   // babel transform
-//   let data;
-//   try {
-//     data = babelCore.transform(content, babelOptions);
-//   }
-//   catch(e) {
-//     throw new Error(e.message);
-//   }
-
-//   // write es5 code file
-//   let outFile = path.join(outPath, file);
-//   outFile = outFile.replace(/\.\w+$/, ext);
-//   helper.mkdir(path.dirname(outFile));
-//   let basename = path.basename(file);
-//   let prefix = '//# sourceMappingURL=';
-//   if(data.code.indexOf(prefix) === -1 && babelOptions.sourceMaps){
-//     data.code = data.code + '\n' + prefix + basename + '.map';
-//   }
-//   fs.writeFileSync(outFile, data.code);
-
-//   // write souremap file
-//   if(babelOptions.sourceMaps) {
-//     let sourceMap = data.map;
-//     sourceMap.file = sourceMap.sources[0];
-//     fs.writeFileSync(outFile + '.map', JSON.stringify(sourceMap, undefined, 4));
-//   }
-//   return true;
-// }
-
 function compileFileByTypescript(options) {
   let {srcPath, outPath, file, typescriptOptions, ext = '.js'} = options;
   typescriptOptions.fileName = file;
   let filePath = path.join(srcPath, file);
   let content = fs.readFileSync(filePath, 'utf8');
+  let pPath = path.dirname(outPath + path.sep + file);
+  let relativePath = path.relative(pPath, srcPath + path.sep + file);
 
   let data;
   try {
@@ -76,26 +29,10 @@ function compileFileByTypescript(options) {
   // write map file
   if(typescriptOptions.compilerOptions.sourceMap) {
     let sourceMap = JSON.parse(data.sourceMapText);
-    sourceMap.file = sourceMap.sources[0];
+    sourceMap.file = sourceMap.sources[0] = relativePath;
     fs.writeFileSync(outFile + '.map', JSON.stringify(sourceMap, undefined, 4));
   }
+  return true;
 }
-
-compileFileByTypescript({
-    srcPath: './test/src/a',
-    outPath: './test/out',
-    file: 'b/test.ts',
-    typescriptOptions: {
-      compilerOptions:{
-        module: ts.ModuleKind.CommonJS,
-        target: ts.ScriptTarget.ES6,
-        experimentalDecorators: true,
-        emitDecoratorMetadata: true,
-        allowSyntheticDefaultImports: true,
-        sourceMap: true
-      }
-    },
-    ext: '.js'
-})
 
 module.exports = compileFileByTypescript;
--- packages/think-typescript/test/index.js
+++ packages/think-typescript/test/index.js
@@ -2,10 +2,84 @@
 * @Author: lushijie
 * @Date:   2017-02-14 10:56:08
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-02-16 17:15:37
+* @Last Modified time: 2017-02-16 18:15:37
 */
+import test from 'ava'
+import helper from 'think-helper'
+import compileFileByTypescript from '../index'
+import path from 'path'
+import fs from 'fs'
+import ts from 'typescript'
 
-var compileFileByTypescript = require('../index').compileFileByTypescript;
-compileFileByTypescript('./test/src', './test/out', {
+test.serial.cb.beforeEach(t => {
+  let outPath = path.join(__dirname, 'out');
+  helper.rmdir(outPath, false).then(() => {
+    t.end();
+  });
+});
 
-})
+test.serial('compileFileByTypescript-1', t => {
+  let out = compileFileByTypescript({
+    srcPath: './test/src/a',
+    outPath: './test/out',
+    file: 'b/test.ts',
+    typescriptOptions: {
+      compilerOptions:{
+        module: ts.ModuleKind.CommonJS,
+        target: ts.ScriptTarget.ES6,
+        experimentalDecorators: true,
+        emitDecoratorMetadata: true,
+        allowSyntheticDefaultImports: true,
+        sourceMap: true
+      }
+    },
+    ext: '.js'
+  });
+  let outFile = helper.isFile(path.join(__dirname, 'out/b/test.js'));
+  let outMapFile = helper.isFile(path.join(__dirname, 'out/b/test.js.map'));
+  t.true(out && outFile && outMapFile);
+});
+
+test.serial('compileFileByTypescript-2', t => {
+  let out = compileFileByTypescript({
+    srcPath: './test/src/a',
+    outPath: './test/out',
+    file: 'b/test.ts',
+    typescriptOptions: {
+      compilerOptions:{
+        module: ts.ModuleKind.CommonJS,
+        target: ts.ScriptTarget.ES6,
+        experimentalDecorators: true,
+        emitDecoratorMetadata: true,
+        allowSyntheticDefaultImports: true,
+        sourceMap: false
+      }
+    }
+  });
+  let outFile = helper.isFile(path.join(__dirname, 'out/b/test.js'));
+  let outMapFile = helper.isFile(path.join(__dirname, 'out/b/test.js.map'));
+  t.true(out && outFile && !outMapFile);
+});
+
+test.serial('compileFileByTypescript-3', t => {
+  let out = compileFileByTypescript({
+   srcPath: './test/src/a',
+   outPath: './test/out',
+   file: 'b/test.ts',
+   typescriptOptions: {
+     compilerOptions:{
+       module: ts.ModuleKind.CommonJS,
+       target: ts.ScriptTarget.ES6,
+       experimentalDecorators: true,
+       emitDecoratorMetadata: true,
+       allowSyntheticDefaultImports: true,
+       sourceMap: true
+     }
+   }
+  });
+  let outFile = helper.isFile(path.join(__dirname, 'out/b/test.js'));
+  let outMapFile = helper.isFile(path.join(__dirname, 'out/b/test.js.map'));
+  t.true(out && outFile && outMapFile);
+});
+
+
