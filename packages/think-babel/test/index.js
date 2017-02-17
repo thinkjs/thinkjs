5b3693e806fe7193ab10a278a122b8345fd99538
lushijie <lushijie1218@126.com>
2017-02-17 10:40:38 +0800

    100% test case

--- packages/think-babel/test/index.js
+++ packages/think-babel/test/index.js
@@ -2,7 +2,7 @@
 * @Author: lushijie
 * @Date:   2017-02-14 10:56:08
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-02-17 10:18:34
+* @Last Modified time: 2017-02-17 10:39:04
 */
 import test from 'ava'
 import helper from 'think-helper'
@@ -61,4 +61,24 @@ test.serial('compileFileByBabel-3', t => {
   let outFile = helper.isFile(path.join(__dirname, 'out/b/test.js'));
   let outMapFile = helper.isFile(path.join(__dirname, 'out/b/test.js.map'));
   t.true(out && outFile && !outMapFile);
+});
+
+let wrongSyntax = 'wrong syntax';
+test.serial('compileFileByBabel-4', t => {
+  let testFilePath = path.join(__dirname, './src/a/b/test.es');
+  fs.appendFileSync(testFilePath, wrongSyntax);
+  let out = compileFileByBabel({
+    srcPath: './test/src/a',
+    outPath: './test/out',
+    file: 'b/test.es',
+    babelOptions: {
+      presets: ['es2015'],
+      sourceMaps: false
+    }
+  });
+
+  var originData = fs.readFileSync(testFilePath, 'utf8').replace(new RegExp(wrongSyntax), '');
+  fs.writeFileSync(testFilePath, originData);
+
+  t.true(helper.isError(out));
 });
