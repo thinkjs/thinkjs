eab5a1ff7fe903d0479f7644373fe98226db1d12
lushijie <lushijie1218@126.com>
2017-02-24 14:35:14 +0800

    fix

--- packages/think-validator/test/index.js
+++ packages/think-validator/test/index.js
@@ -2,7 +2,7 @@
 * @Author: lushijie
 * @Date:   2017-02-14 10:56:08
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-02-24 09:59:56
+* @Last Modified time: 2017-02-24 12:00:50
 */
 import test from 'ava';
 import thinkHelper from 'think-helper';
@@ -139,13 +139,224 @@ test('_requiredWith', t => {
   t.true((out1[0] === 123) && (out1[1] === ''));
 });
 
+// requiredWithAll
+test('requiredWithAll', t => {
+  let out1 = validatorRule.requiredWithAll('', '', '', '');
+  let out2 = validatorRule.requiredWithAll('', 'test', '', '');
+  let out3 = validatorRule.requiredWithAll('value', 'test', 'test1', 'test2');
+  let out4 = validatorRule.requiredWithAll('', 'test', 'test1', 'test2');
+  t.true(out1 && out2 && out3 && !out4);
+});
+
+// _requiredWithAll
+test('_requiredWithAll', t => {
+  let out1 = validatorRule._requiredWithAll(['a', 'b'], {a: {value: 123}});
+  t.true((out1[0] === 123) && (out1[1] === ''));
+});
+
+// requiredWithout
+test('requiredWithout', t => {
+  let out1 = validatorRule.requiredWithout('', '', '', '');
+  let out2 = validatorRule.requiredWithout('', 'test', '', '');
+  let out3 = validatorRule.requiredWithout('value', 'test', 'test1', 'test2');
+  let out4 = validatorRule.requiredWithout('', 'test', 'test1', 'test2');
+  t.true(!out1 && !out2 && out3 && out4);
+});
+
+// _requiredWithout
+test('_requiredWithout', t => {
+  let out1 = validatorRule._requiredWithout(['a', 'b'], {a: {value: 123}});
+  t.true((out1[0] === 123) && (out1[1] === ''));
+});
+
+// requiredWithoutAll
+test('requiredWithoutAll', t => {
+  let out1 = validatorRule.requiredWithoutAll('', '', '', '');
+  let out2 = validatorRule.requiredWithoutAll('', 'test', '', '');
+  let out3 = validatorRule.requiredWithoutAll('value', 'test', 'test1', 'test2');
+  let out4 = validatorRule.requiredWithoutAll('', 'test', 'test1', 'test2');
+  t.true(!out1 && out2 && out3 && out4);
+});
+
+// _requiredWithoutAll
+test('_requiredWithoutAll', t => {
+  let out1 = validatorRule._requiredWithoutAll(['a', 'b'], {a: {value: 123}});
+  t.true((out1[0] === 123) && (out1[1] === ''));
+});
+
+
 // contains
 test('contains', t => {
-  let out1 = validatorRule.contains('', 'lu');
+  let out1 = validatorRule.contains('');
   let out2 = validatorRule.contains('lushijie', 'lu');
   t.true(out1 && out2);
 });
 
+// equals
+test('equals', t => {
+  let out1 = validatorRule.equals('');
+  let out2 = validatorRule.equals('lushijie', 'lushijie');
+  t.true(out1 && out2);
+});
+
+// _equals
+test('_equals', t => {
+  let out1 = validatorRule._equals(['a', 'b'], {a: {value: 123}});
+  let out2 = validatorRule._equals(['b'], {a: {value: 123}});
+  t.true((out1[0] === 123) && out2[0] === '');
+});
+
+// equalsValue
+test('equalsValue', t => {
+  let out0 = validatorRule.equalsValue('');
+  let out1 = validatorRule.equalsValue('lushijie', 'lushijie');
+  t.true(out0 && out1);
+});
+
+// different
+test('different', t => {
+  let out1 = validatorRule.different('');
+  let out2 = validatorRule.different('lushijie1218', 'lushijie');
+  t.true(out1 && out2);
+});
+
+// _different
+test('_different', t => {
+  let out1 = validatorRule._different(['a', 'b'], {a: {value: 123}});
+  let out2 = validatorRule._different(['b'], {a: {value: 123}});
+  t.true((out1[0] === 123) && out2[0] === '');
+});
+
+// after
+test('after', t => {
+  let out1 = validatorRule.after('');
+  let out2 = validatorRule.after('1955-09-11 13:22:00');
+  t.true(out1 && !out2);
+});
+
+// _after
+test('_after', t => {
+  let out1 = validatorRule._after(['a', 'b'], {a: {value: 123}});
+  let out2 = validatorRule._after(['b'], {a: {value: 123}});
+  t.true((out1[0] === 123) && out2[0] === 'b');
+});
+
+
+// before
+test('before', t => {
+  let out1 = validatorRule.before('');
+  let out2 = validatorRule.before('1970-09-11 13:22:00');
+  t.true(out1 && out2);
+});
+
+// _before
+test('_before', t => {
+  let out1 = validatorRule._before(['a', 'b'], {a: {value: 123}});
+  let out2 = validatorRule._before(['b'], {a: {value: 123}});
+  t.true((out1[0] === 123) && out2[0] === 'b');
+});
+
+// alpha
+test('alpha', t => {
+  let out1 = validatorRule.alpha('');
+  let out2 = validatorRule.alpha('lushijie');
+  t.true(out1 && out2);
+});
+
+// alphaDash
+test('alphaDash', t => {
+  let out1 = validatorRule.alphaDash('');
+  let out2 = validatorRule.alphaDash('lushijie');
+  t.true(out1 && out2);
+});
+
+// alphaNumeric
+test('alphaNumeric', t => {
+  let out1 = validatorRule.alphaNumeric('');
+  let out2 = validatorRule.alphaNumeric('lushijie');
+  t.true(out1 && out2);
+});
+
+// alphaNumericDash
+test('alphaNumericDash', t => {
+  let out1 = validatorRule.alphaNumericDash('');
+  let out2 = validatorRule.alphaNumericDash('lushijie_212');
+  t.true(out1 && out2);
+});
+
+// ascii
+test('ascii', t => {
+  let out1 = validatorRule.ascii('');
+  let out2 = validatorRule.ascii('welefNnwer_we1$212');
+  t.true(out1 && out2);
+});
+
+// base64
+test('base64', t => {
+  let out1 = validatorRule.base64('');
+  let out2 = validatorRule.base64((new Buffer('lushijie')).toString('base64'));
+  t.true(out1 && out2);
+});
+
+// byteLength
+test('byteLength', t => {
+  let out1 = validatorRule.byteLength('');
+  let out2 = validatorRule.byteLength('lushijie', 2, 10);
+  t.true(out1 && out2);
+});
+
+// creditcard
+test('creditcard', t => {
+  let out1 = validatorRule.creditcard('');
+  let out2 = validatorRule.creditcard('6226090109493516');
+  t.true(out1 && out2);
+});
+
+// currency
+test('currency', t => {
+  let out1 = validatorRule.currency('');
+  let out2 = validatorRule.currency('$128');
+  t.true(out1 && out2);
+});
+
+// date
+test('date', t => {
+  let out1 = validatorRule.date('');
+  let out2 = validatorRule.date('2012-12-12');
+  t.true(out1 && out2);
+});
+
+// decimal
+test('decimal', t => {
+  let out1 = validatorRule.decimal('');
+  let out2 = validatorRule.decimal('0.1');
+  let out3 = validatorRule.decimal('.1');
+  t.true(out1 && out2 && out3);
+});
+
+// divisibleBy
+test('divisibleBy', t => {
+  let out1 = validatorRule.divisibleBy('');
+  let out2 = validatorRule.divisibleBy('10', 2);
+  t.true(out1 && out2);
+});
+
+// email
+test('email', t => {
+  let out1 = validatorRule.email('');
+  let out2 = validatorRule.email('lushijie1218@126.com');
+  let out3 = validatorRule.email('lushijie');
+  t.true(out1 && out2 && !out3);
+});
+
+//fqdn
+test('fqdn', t => {
+  let out1 = validatorRule.fqdn('');
+  let out2 = validatorRule.fqdn('gmail.com');
+  let out3 = validatorRule.fqdn('www.gmail.com');
+  let out4 = validatorRule.fqdn('ww-w.gmail.com');
+  t.true(out1 && out2 && out3 && out4);
+});
 
 //float
 test('float', t => {
@@ -210,6 +421,20 @@ test('ip6', t => {
   let out1 = validatorRule.ip6('2031:0000:1F1F:0000:0000:0100:11A0:ADD');
   let out2 = validatorRule.ip6('127.0.0.1');
   t.true(out0 && out1 && !out2);
+});
+
+// isbn
+test('isbn', t => {
+  let out0 = validatorRule.isbn('');
+  let out1 = validatorRule.isbn('9787540471644');
+  t.true(out0 && out1);
+});
+
+// isin
+test('isin', t => {
+  let out0 = validatorRule.isin('');
+  let out1 = validatorRule.isin('9787540471644');
+  t.true(out0 && !out1);
 });
 
 //iso8601
@@ -347,6 +572,33 @@ test('variableWidth', t => {
   let out1 = validatorRule.variableWidth('');
   let out2 = validatorRule.variableWidth('lushijielushijielushijielushijie');
   t.true(out1 && !out2);
+});
+
+// order
+test('order', t => {
+  let out1 = validatorRule.order('');
+  let out2 = validatorRule.order('name ASC, id DESC');
+  let out3 = validatorRule.order('AA|ww');
+  t.true(out1 && out2 && !out3);
+});
+
+
+// field
+test('field', t => {
+  let out1 = validatorRule.field('');
+  let out2 = validatorRule.field('name,*');
+  let out3 = validatorRule.field('name-23,*');
+  t.true(out1 && out2 && !out3);
+});
+
+// image
+test('image', t => {
+  let out1 = validatorRule.image('');
+  let out2 = validatorRule.image({
+      originalFilename: 'test.jpg'
+  });
+  let out3 = validatorRule.image('test.gif');
+  t.true(out1 && out2 && out3);
 });
 
 // startWith
