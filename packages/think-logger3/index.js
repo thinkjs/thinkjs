9bcda89f8bd618a077167c4249ddafa41766d590
lizheming <secretlzm007@gmail.com>
2017-02-24 19:16:24 +0800

    add logger and adapter

---
+++ packages/think-logger3/index.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+
+class Logger {
+  constructor(config) {
+    if( !this instanceof Logger ) {
+      return new Logger(config);
+    }
+    
+    let handle = config.handle;
+    delete config.handle;
+    
+    this._logger = new handle(config);
+    ['debug', 'info', 'warn', 'error'].forEach(level => {
+      assert(this._logger[level], `adapter function ${level} not exist!`);
+      this[level] = (...args) => {
+        this._beforeLog(...args);
+        this._logger[level](...args);
+        this._afterLog(...args);
+      }
+    });
+  }
+
+  _beforeLog() {
+
+  }
+
+  _afterLog() {
+
+  }
+}
+
+module.exports = Logger;
