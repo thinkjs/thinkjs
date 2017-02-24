e27b8f3fd6632391f1d74edbcaac186d10cda6eb
lushijie <lushijie1218@126.com>
2017-02-24 10:00:08 +0800

    add test caser

--- packages/think-validator/test/index.js
+++ packages/think-validator/test/index.js
@@ -2,7 +2,7 @@
 * @Author: lushijie
 * @Date:   2017-02-14 10:56:08
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-02-22 17:35:10
+* @Last Modified time: 2017-02-24 09:59:56
 */
 import test from 'ava';
 import thinkHelper from 'think-helper';
@@ -145,6 +145,110 @@ test('contains', t => {
   let out2 = validatorRule.contains('lushijie', 'lu');
   t.true(out1 && out2);
 });
+
+
+//float
+test('float', t => {
+  let out1 = validatorRule.float('');
+  let out2 = validatorRule.float('65M');
+  let out3 = validatorRule.float('34.00', 23, 56);
+  let out4 = validatorRule.float('34.00', 23, 33);
+  let out5 = validatorRule.float('34.00', 35, 56);
+  let out6 = validatorRule.float('34.00', 23);
+  let out7 = validatorRule.float('34.00', 35);
+  t.true(out1 && !out2 && out3 && !out4 && !out5 && out6 && !out7);
+});
+
+// fullWidth
+test('fullWidth', t => {
+  let out0 = validatorRule.fullWidth('');
+  let out1 = validatorRule.fullWidth('￥$$$');
+  t.true(out0 && out1);
+});
+
+// halfWidth
+test('halfWidth', t => {
+  let out0 = validatorRule.halfWidth('');
+  let out1 = validatorRule.halfWidth('￥$$');
+  t.true(out0 && out1);
+});
+
+// hexColor
+test('hexColor', t => {
+  let out0 = validatorRule.hexColor('');
+  let out1 = validatorRule.hexColor('#000000');
+  let out2 = validatorRule.hexColor('#000');
+  t.true(out0 && out1 && out2);
+});
+
+// hex
+test('hex', t => {
+  let out0 = validatorRule.hex('');
+  let out1 = validatorRule.hex('a0a');
+  t.true(out0 && out1);
+});
+
+// ip
+test('ip', t => {
+  let out0 = validatorRule.ip('');
+  let out1 = validatorRule.ip('127.0.0.1');
+  let out2 = validatorRule.ip('2031:0000:1F1F:0000:0000:0100:11A0:ADD');
+  t.true(out0 && out1 && out2);
+});
+
+// ip4
+test('ip4', t => {
+  let out0 = validatorRule.ip4('');
+  let out1 = validatorRule.ip4('127.0.0.1');
+  let out2 = validatorRule.ip4('256.0.0.0');
+  t.true(out0 && out1 && !out2);
+});
+
+// ip6
+test('ip6', t => {
+  let out0 = validatorRule.ip6('');
+  let out1 = validatorRule.ip6('2031:0000:1F1F:0000:0000:0100:11A0:ADD');
+  let out2 = validatorRule.ip6('127.0.0.1');
+  t.true(out0 && out1 && !out2);
+});
+
+//iso8601
+test('iso8601', t => {
+  let out0 = validatorRule.iso8601('');
+  let out1 = validatorRule.iso8601('2011-09-11');
+  let out2 = validatorRule.iso8601('2011-09-11-123');
+  t.true(out0 && out1 && !out2);
+});
+
+//in
+test('in', t => {
+  let out0 = validatorRule.in('');
+  let out1 = validatorRule.in('1.2', '1.2', '2.3');
+  let out2 = validatorRule.in('1.2', '1.3', '1.4');
+  let out3 = validatorRule.in('lu', 'lushijie', 'shijie');
+  t.true(out0 && out1 && !out2 && !out3);
+});
+
+//notIn
+test('notIn', t => {
+  let out0 = validatorRule.notIn('')
+  let out1 = validatorRule.notIn('1.2', '1.2', '2.3');
+  let out2 = validatorRule.notIn('1.2', '1.3', '1.4');
+  let out3 = validatorRule.notIn('lu', 'lushijie', 'shijie');
+  t.true(out0 && !out1 && out2 && out3);
+});
+
+//int
+test('int', t => {
+  let out1 = validatorRule.int('');
+  let out2 = validatorRule.int('65M');
+  let out3 = validatorRule.int('34', 23, 56);
+  let out4 = validatorRule.int('34', 23, 33);
+  let out5 = validatorRule.int('34', 35, 56);
+  let out6 = validatorRule.int('34', 23);
+  let out7 = validatorRule.int('34', 35);
+  t.true(out1 && !out2 && out3 && !out4 && !out5 && out6 && !out7);
+})
 
 // min
 test('min', t => {
