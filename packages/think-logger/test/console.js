b168afb3688e75f01f23c4a007043bfd17a6c4f3
lizheming <secretlzm007@gmail.com>
2017-02-25 09:53:57 +0800

    add consoler logger coverage test

---
+++ packages/think-logger/test/console.js
@@ -0,0 +1,25 @@
+const test = require('ava');
+const Logger = require('../lib');
+const process = require('process');
+const Adapter = require('../src/adapter/console');
+
+test.before('console logger', () => {
+  global.server_log = '';
+  process.stdout.write = (write => (string, ...args) => {
+    global.server_log += string;
+    write.apply(process.stdout, [string, ...args]);
+  })(process.stdout.write);
+});
+
+test('console logger', t => {
+  let func_names = ['debug', 'info', 'warn', 'error'];
+  let logger = new Logger({handle: Adapter});
+
+  t.plan(func_names.length);
+  for(let func_name of func_names) {
+    let func = logger[func_name];
+    func('Hello World');
+    t.true(global.server_log.includes('Hello World'));
+    global.server_log = '';
+  }
+});
