ea2a99a48ee18a33bd51c7e06be33c122f3e5c71
lizheming <secretlzm007@gmail.com>
2017-02-28 20:09:27 +0800

    add test for file

--- packages/think-logger3/test/file.js
+++ packages/think-logger3/test/file.js
@@ -1,2 +1,36 @@
+const fs = require('fs');
 const test = require('ava');
-test.todo('file logger');
+const log4js = require('log4js');
+const Logger = require('../src');
+const process = require('process');
+const Adapter = require('../src/adapter/file');
+
+for(let func of ['debug', 'info', 'warn', 'error']) {
+  const filename = `${__dirname}/test-${func}.log`;
+  test.before('file logger '+func, () => {
+    try {
+      fs.statSync(filename);
+      fs.unlinkSync(filename);
+    } catch(e) {
+
+    } finally {
+      fs.writeFileSync(filename, '', {encoding: 'utf-8'});
+    }
+  });
+
+  test('file logger '+func, t => {
+    let logger = new Logger({
+      handle: Adapter,
+      filename: filename
+    });
+
+    // log4js.shutdown(() => {
+    //   console.log(filename);
+    //   let text = fs.readFileSync(filename, {encoding: 'utf-8'});
+    //   t.true(text.includes('Hello World'));
+    //   fs.unlinkSync(filename);
+    // });
+
+    logger[func]('Hello World');
+  });
+}
