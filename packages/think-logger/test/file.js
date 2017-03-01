28225bc8a5d975b89020b7ca8aad9a2e0784295a
lizheming <secretlzm007@gmail.com>
2017-03-01 22:23:07 +0800

    update file logger test

--- packages/think-logger/test/file.js
+++ packages/think-logger/test/file.js
@@ -5,32 +5,33 @@ const Logger = require('../lib');
 const process = require('process');
 const Adapter = Logger.File;
 
-for(let func of ['debug', 'info', 'warn', 'error']) {
-  const filename = `${__dirname}/test-${func}.log`;
-  test.before('file logger '+func, () => {
-    try {
-      fs.statSync(filename);
-      fs.unlinkSync(filename);
-    } catch(e) {
+const sleep = time => new Promise(resolve => setTimeout(resolve, time));
+const filename = `${__dirname}/test.log`;
 
-    } finally {
-      fs.writeFileSync(filename, '', {encoding: 'utf-8'});
-    }
+test.before('file logger', () => {
+  try {
+    fs.statSync(filename);
+    fs.unlinkSync(filename);
+  } catch(e) {
+
+  } finally {
+    fs.writeFileSync(filename, '', {encoding: 'utf-8'});
+  }  
+});
+
+test('file logger', async t => {
+  let logger = new Logger({
+    handle: Adapter,
+    filename
   });
 
-  test('file logger '+func, t => {
-    let logger = new Logger({
-      handle: Adapter,
-      filename: filename
-    });
+  let funcs = ['debug', 'info', 'warn', 'error'];
+  funcs.forEach(func => logger[func]('Hello World'));
 
-    // log4js.shutdown(() => {
-    //   console.log(filename);
-    //   let text = fs.readFileSync(filename, {encoding: 'utf-8'});
-    //   t.true(text.includes('Hello World'));
-    //   fs.unlinkSync(filename);
-    // });
+  await sleep(500);
 
-    logger[func]('Hello World');
-  });
-}
+  let text = fs.readFileSync(filename, {encoding: 'utf-8'});
+  t.true(text.split('Hello World').length === funcs.length+1 );
+  
+  fs.unlinkSync(filename);
+});
