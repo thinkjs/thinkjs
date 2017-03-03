39895b6716de65b262d1d9ebb7d9506789313c89
lushijie <lushijie1218@126.com>
2017-03-03 10:44:07 +0800

    rewrite with class

---
+++ packages/think-validator/index.bak.js
@@ -0,0 +1,222 @@
+/*
+* @Author: lushijie
+* @Date:   2017-03-03 09:16:23
+* @Last Modified by:   lushijie
+* @Last Modified time: 2017-03-03 09:17:55
+*/
+const Validator = require('./rules.js');
+const thinkHelper = require('think-helper');
+
+let thinkValidate = {};
+
+// the method names for required
+const requiredRuleNames = [
+  'required',
+  'requiredIf',
+  'requiredNotIf',
+  'requiredWith',
+  'requiredWithAll',
+  'requiredWithOut',
+  'requiredWithOutAll'
+];
+
+/**
+ * get error message for rule
+ * @param  {String} rule            [description]
+ * @param  {String} pname        [description]
+ * @param  {Array|Object|Number|String|Boolean} parsedRuleOptions [description]
+ * @param  {String} errmsg          [description]
+ * @return {String}                 [description]
+ */
+function _getErrorMessage(rname, pname, parsedRuleOptions, pitem) {
+  if(pitem.errmsg) {
+    return pitem.errmsg;
+  }
+  let ruleOptions = pitem[rname];
+  let errmsg = Validator.errors['validate_' + rname];
+  return errmsg.replace('{name}', pname)
+               .replace('{parsedOptions}', JSON.stringify(parsedRuleOptions))
+               .replace('{options}', JSON.stringify(ruleOptions));
+};
+
+/**
+ * parse the rule's arguments
+ * @param  {String} rname        []
+ * @param  {Array|Boolean|Object|String|Int} roptions []
+ * @param  {Object} requestData []
+ * @return {Array|Boolean|Object|String|Int}             []
+ */
+function _parseRuleOptions(rname, roptions, requestData) {
+  let parseFn = Validator[`_${rname}`];
+  if(thinkHelper.isFunction(parseFn)){
+    roptions = parseFn(roptions, requestData);
+  }
+  return roptions;
+};
+
+/**
+ * convert the param output style for int,float
+ * @param  {Object} pitem [description]
+ * @return {[type]}      [description]
+ */
+function _convertParamItemValue(pitem) {
+  pitem = thinkHelper.extend({}, pitem);
+  if(pitem.int) {
+    return parseInt(pitem.value);
+  }else if(pitem.float || pitem.numeric) {
+    return parseFloat(pitem.value);
+  }
+  return pitem.value;
+}
+
+/**
+ * check if the param's value is required
+ * @param  {[type]}  pitem        [description]
+ * @param  {Object}  requestData [description]
+ * @return {Boolean}             [description]
+ */
+function _isValueRequired(pitem, requestData) {
+  let isRequired = false;
+  let vtype;
+  for(var i = 0; i <= requiredRuleNames.length; i++) {
+    vtype = requiredRuleNames[i];
+    if(pitem[vtype]) {
+      let fn = Validator[vtype];
+      let afterParsedArgs = _parseRuleOptions(vtype, pitem[vtype], requestData);
+      if(fn(pitem.value, afterParsedArgs)) {
+        isRequired = true;
+        break;
+      };
+    }
+  }
+  return isRequired;
+}
+
+
+//let thinkValidate = (rname, callback, errmsg) => {
+  // // register validate callback
+  // if (thinkHelper.isString(rname)) {
+  //   if (thinkHelper.isFunction(callback)) {
+  //     Validator[rname] = callback;
+  //     Validator.ERR['validate_' + rname] = errmsg || (rname + 'rule valid failed');
+  //     return;
+  //   }
+  //   // get validator callback
+  //   return Validator[rname];
+  // }
+
+  // if(thinkHelper.isObject(rname)) {
+  //   let pitems = rname, requestData = callback;
+  //   return thinkValidate.exec(pitems, requestData);
+  // };
+//};
+
+/**
+ * add Validate method
+ * @param  {String}   rname    [description]
+ * @param  {Function} callback [description]
+ * @param  {String}   errmsg   [description]
+ */
+thinkValidate.add = (rname, callback, errmsg) => {
+  Validator[rname] = callback;
+  Validator.ERR['validate_' + rname] = errmsg || (rname + 'rule valid failed');
+}
+
+/**
+ * get Validate method
+ * @param  {String} rname [description]
+ * @return {Function}       [description]
+ */
+// thinkValidate.get = rname => {
+//   return Validator[rname];
+// }
+
+/**
+ * exec validate
+ * @param  {Object} pitems []
+ * @return {Object}            []
+ */
+thinkValidate.exec = (pitems, requestData = {}) => {
+  let ret = {};
+
+  outerLoop:
+  for(let pname in pitems){
+
+    let pitem = pitems[pname];
+    pitem.value = requestData[pname];
+
+    // set default
+    if(typeof(pitem.value) === 'undefined' && !thinkHelper.isTrueEmpty(pitem.default)){
+      pitem.value = pitem.default;
+    }
+
+    // trim pitem value if trim is true
+    if(pitem.trim && pitem.value && pitem.value.trim){
+      pitem.value = pitem.value.trim();
+    }
+
+    // set defalut ret for response
+    if(typeof pitem.value !== 'undefined') {
+      ret[pname] = pitem.value;
+    }
+
+    let isRequired = _isValueRequired(pitem, requestData);
+    if(isRequired && thinkHelper.isTrueEmpty(pitem.value)) {
+      let rname = 'required';
+      let parsedOptions = _parseRuleOptions(rname, pitem[rname], requestData);
+      let errmsg = _getErrorMessage(rname, pname, parsedOptions, pitem);
+      ret = {
+        _valid: false,
+        name: pname,
+        rule: rname,
+        value: pitem.value,
+        errmsg: errmsg
+      }
+      return ret;
+    }else {
+      if(thinkHelper.isTrueEmpty(pitem.value)) {
+        continue outerLoop;
+      }
+    }
+
+    for(let rname in pitem){
+      // the skipedRule types for the item
+      const skipedRuleNames = ['value', 'default', 'trim', 'errmsg'].concat(requiredRuleNames);
+
+      // skip the attr don't need valid
+      if(skipedRuleNames.indexOf(rname) >= 0) {
+        continue;
+      }
+
+      // check if the valid method is exsit
+      let fn = Validator[rname];
+      if (!thinkHelper.isFunction(fn)) {
+        return new Error(rname + ' valid method is not been configed');
+      }
+
+      // parse rule's options
+      let parsedOptions = _parseRuleOptions(rname, pitem[rname], requestData);
+
+      let result = fn(pitem.value, parsedOptions);
+      if(!result){
+        let errmsg = _getErrorMessage(rname, pname, parsedOptions, pitem);
+        ret = {
+          _valid: false,
+          name: pname,
+          rule: rname,
+          value: pitem.value,
+          errmsg: errmsg
+        }
+        break outerLoop;
+      }else {
+        let convertValue = _convertParamItemValue(pitem);
+        ret[pname] = convertValue;
+      }
+    } // end inner for
+
+  } // end outer for
+  return ret;
+};
+
+
+module.exports = thinkValidate;
--- packages/think-validator/index.js
+++ packages/think-validator/index.js
@@ -2,221 +2,154 @@
 * @Author: lushijie
 * @Date:   2017-02-21 18:50:26
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-03-02 19:18:57
+* @Last Modified time: 2017-03-03 10:43:30
 */
-const Validator = require('./rules.js');
-const thinkHelper = require('think-helper');
+const _validator = require('./rules.js');
+const helper = require('think-helper');
 
-let thinkValidate = {};
 
-// the method names for required
-const requiredRuleNames = [
-  'required',
-  'requiredIf',
-  'requiredNotIf',
-  'requiredWith',
-  'requiredWithAll',
-  'requiredWithOut',
-  'requiredWithOutAll'
-];
-
-/**
- * get error message for rule
- * @param  {String} rule            [description]
- * @param  {String} pname        [description]
- * @param  {Array|Object|Number|String|Boolean} parsedRuleOptions [description]
- * @param  {String} errmsg          [description]
- * @return {String}                 [description]
- */
-function _getErrorMessage(rname, pname, parsedRuleOptions, pitem) {
-  if(pitem.errmsg) {
-    return pitem.errmsg;
+class Validator {
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.requiredValidNames = [
+      'required',
+      'requiredIf',
+      'requiredNotIf',
+      'requiredWith',
+      'requiredWithAll',
+      'requiredWithOut',
+      'requiredWithOutAll'
+    ];
+    this.skipedValidNames = ['value', 'default', 'trim', 'errmsg'].concat(this.requiredValidNames);
   }
-  let ruleOptions = pitem[rname];
-  let errmsg = Validator.errors['validate_' + rname];
-  return errmsg.replace('{name}', pname)
-               .replace('{parsedOptions}', JSON.stringify(parsedRuleOptions))
-               .replace('{options}', JSON.stringify(ruleOptions));
-};
 
-/**
- * parse the rule's arguments
- * @param  {String} rname        []
- * @param  {Array|Boolean|Object|String|Int} roptions []
- * @param  {Object} requestData []
- * @return {Array|Boolean|Object|String|Int}             []
- */
-function _parseRuleOptions(rname, roptions, requestData) {
-  let parseFn = Validator[`_${rname}`];
-  if(thinkHelper.isFunction(parseFn)){
-    roptions = parseFn(roptions, requestData);
+  _getErrorMessage(rule, ruleName, validName,  parsedValidArgs) {
+    // if(rule.errmsg) {
+    //   return rule.errmsg;
+    // }
+    //let ruleName = Object.keys(rule)[0];
+    let validArgs = rule[validName];
+    let errmsg = _validator.errors['validate_' + validName];
+    return errmsg.replace('{name}', ruleName)
+                 .replace('{parsedOptions}', JSON.stringify(parsedValidArgs))
+                 .replace('{options}', JSON.stringify(validArgs));
+
   }
-  return roptions;
-};
 
-/**
- * convert the param output style for int,float
- * @param  {Object} pitem [description]
- * @return {[type]}      [description]
- */
-function _convertParamItemValue(pitem) {
-  pitem = thinkHelper.extend({}, pitem);
-  if(pitem.int) {
-    return parseInt(pitem.value);
-  }else if(pitem.float || pitem.numeric) {
-    return parseFloat(pitem.value);
+  _parseRuleArgs(validName, ruleArgs) {
+    let pfn = _validator['_' + validName];
+    if(helper.isFunction(pfn)){
+      ruleArgs = pfn(ruleArgs, this.ctx);
+    }
+    return ruleArgs;
   }
-  return pitem.value;
-}
 
-/**
- * check if the param's value is required
- * @param  {[type]}  pitem        [description]
- * @param  {Object}  requestData [description]
- * @return {Boolean}             [description]
- */
-function _isValueRequired(pitem, requestData) {
-  let isRequired = false;
-  let vtype;
-  for(var i = 0; i <= requiredRuleNames.length; i++) {
-    vtype = requiredRuleNames[i];
-    if(pitem[vtype]) {
-      let fn = Validator[vtype];
-      let afterParsedArgs = _parseRuleOptions(vtype, pitem[vtype], requestData);
-      if(fn(pitem.value, afterParsedArgs)) {
-        isRequired = true;
-        break;
-      };
+  _convertParamItemValue(rule, ruleName) {
+    if(rule.int || rule.float || rule.numeric) {
+       this.ctx[ruleName] = parseFloat(rule.value);
     }
   }
-  return isRequired;
-}
 
-
-//let thinkValidate = (rname, callback, errmsg) => {
-  // // register validate callback
-  // if (thinkHelper.isString(rname)) {
-  //   if (thinkHelper.isFunction(callback)) {
-  //     Validator[rname] = callback;
-  //     Validator.ERR['validate_' + rname] = errmsg || (rname + 'rule valid failed');
-  //     return;
-  //   }
-  //   // get validator callback
-  //   return Validator[rname];
-  // }
-
-  // if(thinkHelper.isObject(rname)) {
-  //   let pitems = rname, requestData = callback;
-  //   return thinkValidate.exec(pitems, requestData);
-  // };
-//};
-
-/**
- * add Validate method
- * @param  {String}   rname    [description]
- * @param  {Function} callback [description]
- * @param  {String}   errmsg   [description]
- */
-thinkValidate.add = (rname, callback, errmsg) => {
-  Validator[rname] = callback;
-  Validator.ERR['validate_' + rname] = errmsg || (rname + 'rule valid failed');
-}
-
-/**
- * get Validate method
- * @param  {String} rname [description]
- * @return {Function}       [description]
- */
-// thinkValidate.get = rname => {
-//   return Validator[rname];
-// }
-
-/**
- * exec validate
- * @param  {Object} pitems []
- * @return {Object}            []
- */
-thinkValidate.exec = (pitems, requestData = {}) => {
-  let ret = {};
-
-  outerLoop:
-  for(let pname in pitems){
-
-    let pitem = pitems[pname];
-    pitem.value = requestData[pname];
-
-    // set default
-    if(typeof(pitem.value) === 'undefined' && !thinkHelper.isTrueEmpty(pitem.default)){
-      pitem.value = pitem.default;
-    }
-
-    // trim pitem value if trim is true
-    if(pitem.trim && pitem.value && pitem.value.trim){
-      pitem.value = pitem.value.trim();
-    }
-
-    // set defalut ret for response
-    if(typeof pitem.value !== 'undefined') {
-      ret[pname] = pitem.value;
-    }
-
-    let isRequired = _isValueRequired(pitem, requestData);
-    if(isRequired && thinkHelper.isTrueEmpty(pitem.value)) {
-      let rname = 'required';
-      let parsedOptions = _parseRuleOptions(rname, pitem[rname], requestData);
-      let errmsg = _getErrorMessage(rname, pname, parsedOptions, pitem);
-      ret = {
-        _valid: false,
-        name: pname,
-        rule: rname,
-        value: pitem.value,
-        errmsg: errmsg
+  _checkValueRequired(rule) {
+    let isRequired = false;
+    //let validName;
+    for(var i = 0; i <= this.requiredValidNames.length; i++) {
+      let validName = this.requiredValidNames[i];
+      if(rule[validName]) {
+        let fn = _validator[validName];
+        let parsedValidArgs = this._parseRuleArgs(validName, rule[validName], this.ctx);
+        if(fn(rule.value, parsedValidArgs)) {
+          isRequired = true;
+          break;
+        };
       }
-      return ret;
-    }else {
-      if(thinkHelper.isTrueEmpty(pitem.value)) {
+    }
+    return isRequired;
+  }
+
+  validate(rules, msgs) {
+    let ret = {};
+
+    outerLoop:
+    for(let ruleName in rules){
+
+      let rule = rules[ruleName];
+      rule.value = this.ctx[ruleName];
+
+      // set default
+      if(typeof(rule.value) === 'undefined' && !helper.isTrueEmpty(rule.default)){
+        rule.value = rule.default;
+      }
+
+      // trim rule value if trim is true
+      if(rule.trim && rule.value && rule.value.trim){
+        rule.value = rule.value.trim();
+      }
+
+      // 数组
+      if(rule.array && !helper.isArray(rule.value)) {
+        rule.value = [rule.value]
+      }
+
+      // required check
+      let isRequired = this._checkValueRequired(rule);
+      if(isRequired && helper.isTrueEmpty(rule.value)) {
+        let validName = 'required';
+        let parsedValidArgs = this._parseRuleArgs(validName, rule[validName]);
+        let errMsg = this._getErrorMessage(rule, ruleName, validName, parsedValidArgs);
+        ret[ruleName] = errMsg;
+        continue outerLoop;
+      }else if(!isRequired && helper.isTrueEmpty(rule.value)){
         continue outerLoop;
       }
-    }
 
-    for(let rname in pitem){
-      // the skipedRule types for the item
-      const skipedRuleNames = ['value', 'default', 'trim', 'errmsg'].concat(requiredRuleNames);
-
-      // skip the attr don't need valid
-      if(skipedRuleNames.indexOf(rname) >= 0) {
-        continue;
-      }
-
-      // check if the valid method is exsit
-      let fn = Validator[rname];
-      if (!thinkHelper.isFunction(fn)) {
-        return new Error(rname + ' valid method is not been configed');
-      }
-
-      // parse rule's options
-      let parsedOptions = _parseRuleOptions(rname, pitem[rname], requestData);
-
-      let result = fn(pitem.value, parsedOptions);
-      if(!result){
-        let errmsg = _getErrorMessage(rname, pname, parsedOptions, pitem);
-        ret = {
-          _valid: false,
-          name: pname,
-          rule: rname,
-          value: pitem.value,
-          errmsg: errmsg
+      for(let validName in rule){
+        // skip the attr don't need valid
+        if(this.skipedValidNames.indexOf(validName) >= 0) {
+          continue;
         }
-        break outerLoop;
-      }else {
-        let convertValue = _convertParamItemValue(pitem);
-        ret[pname] = convertValue;
-      }
-    } // end inner for
 
-  } // end outer for
-  return ret;
+        // check if the valid method is exsit
+        let fn = _validator[validName];
+        if (!helper.isFunction(fn)) {
+          throw new Error(validName + ' valid method is not been configed');
+        }
+
+        let parsedValidArgs = this._parseRuleArgs(validName, rule[validName]);
+        let result = fn(rule.value, parsedValidArgs);
+
+        if(!result){
+          let errMsg = this._getErrorMessage(rule, ruleName, validName, parsedValidArgs);
+          ret[ruleName] = errMsg;
+          break outerLoop;
+        }else {
+          this._convertParamItemValue(rule, ruleName);
+        }
+      } // end inner for
+
+    } // end outer for
+    console.log('resp-->', ret);
+    console.log('ctx-->', this.ctx)
+    return ret;
+  }
+}
+
+let ctx = {
+  name: '123.00'
 };
 
+let rules2 = {
+  name: {
+    int: true,
+    required: true
+  }
+}
 
-module.exports = thinkValidate;
+let msgs2 = {
+  int: 'int valid failed'
+}
+const instance = new Validator(ctx);
+let resp = instance.validate(rules2, msgs2);
+
+
+module.exports = Validator;
