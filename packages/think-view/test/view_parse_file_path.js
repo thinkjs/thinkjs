453ad1582e1c0aa2e4f4a3c8fd098cfd032eb380
zhengqingxin <zhengqingxin@360.cn>
2017-03-07 20:57:39 +0800

    add test cases

--- packages/think-view/test/view_parse_file_path.js
+++ packages/think-view/test/view_parse_file_path.js
@@ -18,3 +18,28 @@ test('parseFilePath function -- normal scene', t=> {
   let file = view.parseFilePath('index.html', config);
   t.is(file, 'www/static/index.html');
 });
+
+// test('parseFilePath function -- Object path', t=> {
+//   const view = new View(ctx);
+//   let file = view.parseFilePath({}, config);
+//   console.log(file);
+// });
+
+test('parseFilePath function -- path without extension name', t=> {
+  const view = new View(ctx);
+  let file = view.parseFilePath('index', config);
+  t.is(file, 'www/static/index.html');
+});
+
+
+test('parseFilePath function -- undefined path', t=> {
+  const view = new View(ctx);
+  let file = view.parseFilePath(undefined, config);
+  t.is(file, 'www/static/admin/user/index.html');
+});
+
+test('parseFilePath function -- absolute path', t=> {
+  const view = new View(ctx);
+  let file = view.parseFilePath('/index.html', config);
+  t.is(file, '/index.html');
+});
