9d42722c87a9a31083190a7f84d50f1691059262
Cathy Huang <lu.huang90@gmail.com>
2017-03-09 16:40:19 +0800

    use mock in test

--- packages/think-inspect/index.js
+++ packages/think-inspect/index.js
@@ -1,92 +1,93 @@
 const Helper = require('think-helper');
 const fs = require('fs');
+const path = require('path');
 
-/**
- * check if node version meets the requirement of ThinkJS.
- * @param {Object} config: {THINK_PATH: 'XXX'}.
- * `THINK_PATH` specifies the path of ThinkJS module.
- * @param {Function} callback: callback when error occurs.
- */
-function checkNodeVersion(config, callback) {
-  let packageFile = `${config.THINK_PATH}/package.json`;
-  if(!Helper.isFile(packageFile)){
-    return;
+let logger = null;
+const Inspector = class {
+  constructor(config) {
+    this.config = config;
+    logger = config.logger;
   }
-  let {engines} = JSON.parse(fs.readFileSync(packageFile, 'utf-8'));
-  let needVersion = engines.node.substr(2);
+  /**
+   * check if node version meets the requirement of ThinkJS.
+   */
+  checkNodeVersion() {
+    const config = this.config;
+    let packageFile = path.join(config.THINK_PATH, 'package.json');
 
-  let nodeVersion = process.version;
-  if(nodeVersion[0] === 'v'){
-    nodeVersion = nodeVersion.slice(1);
-  }
-  if(needVersion > nodeVersion){
-    callback(`ThinkJS need node version >= ${needVersion}, current version is ${nodeVersion}, please upgrade it.`, 'EXIT');
-  }
-}
-exports.checkNodeVersion = checkNodeVersion;
 
-/**
- * check if filenames in application are in lowercase;
- * include: js, html, tpl;
- * exclude: filenames with `/locale/`.
- * @param {Object} config: {APP_PATH: 'app', sep: '/', locale: 'locale'}.
- * `APP_PATH` specifies the absolute path of application.
- * @param {Function} callback: callback when error occurs.
- * @return {} []
- */
-function checkFileName(config, callback) {
-  let files = Helper.getdirFiles(config.APP_PATH);
-  const excludePath = `${config.sep}${config.locale}${config.sep}`;
-  const excludeReg = new RegExp(excludePath);
-  const includeReg = /\.(js|html|tpl)$/;
-  const uppercaseReg = /[A-Z]/;
-  files.forEach((item) => {
-    // inspect filename
-    if(!includeReg.test(item) || excludeReg.test(item)) return;
-    if(uppercaseReg.test(item)) {
-      callback(`filename \`${item}\` has uppercase chars.`, 'WARNING');  
+    if(!Helper.isFile(packageFile)){
+      return;
     }
-  });
-}
-exports.checkFileName = checkFileName;
- 
- /**
-  * check dependencies are installed before server starts
-  * @param {Object} config: {ROOT_PATH: 'XXX', sep: '/', env: 'development'}.
-  * `APP_PATH` specifies the root path of project.
-  * @param {Function} callback: callback when error occurs.
-  * @return {} []
-  */
-function checkDependencies(config, callback) {
-  let packageFile = `${config.ROOT_PATH}/package.json`;
-  if(!Helper.isFile(packageFile)){
-    return;
-  }
+    let {engines} = JSON.parse(fs.readFileSync(packageFile, 'utf-8'));
+    let needVersion = engines.node.substr(2);
 
-  let data = JSON.parse(fs.readFileSync(packageFile, 'utf-8'));
-  let dependencies = Helper.extend({}, data.dependencies);
-
-  // merge devDependencies in development env
-  if(config.env === 'development') {
-    dependencies = Helper.extend(dependencies, data.devDependencies);
-  }
-
-  // package alias
-  let pkgAlias = {
-    'babel-runtime': 'babel-runtime/helpers/inherits'
-  };
-  let pkgPath = `${config.ROOT_PATH}${config.sep}node_modules${config.sep}`;
-  for(let pkg in dependencies) {
-    pkg = pkgAlias[pkg] || pkg;
-    if(Helper.isDirectory(`${pkgPath}${pkg}`)) {
-      continue;
+    let nodeVersion = process.version;
+    if(nodeVersion[0] === 'v'){
+      nodeVersion = nodeVersion.slice(1);
     }
-  
-    try {
-      require(pkg);
-    } catch(e) {
-      callback(`package \`${pkg}\` is not installed. please run \`npm install\` command before server starts.`, 'EXIT');
+   
+    if(needVersion > nodeVersion){
+      logger.error(`ThinkJS need node version >= ${needVersion}, current version is ${nodeVersion}, please upgrade it.`);
     }
   }
+
+  /**
+   * check if filenames in application are in lowercase;
+   * include: js, html, tpl;
+   * exclude: filenames with `/locale/`.
+   */
+  checkFileName() {
+
+    const config = this.config;
+    let files = Helper.getdirFiles(config.APP_PATH);
+    const excludePath = `${path.sep}${config.locale}${path.sep}`;
+    const excludeReg = new RegExp(excludePath);
+    const includeReg = /\.(js|html|tpl)$/;
+    const uppercaseReg = /[A-Z]/;
+    files.forEach((item) => {
+      // inspect filename
+      if(!includeReg.test(item) || excludeReg.test(item)) return;
+      if(uppercaseReg.test(item)) {
+        logger.warn(`filename \`${item}\` has uppercase chars.`);  
+      }
+    });
+  }
+  /**
+    * check dependencies are installed before server starts
+    */
+  checkDependencies() {
+    const config = this.config;
+    let packageFile = path.join(config.ROOT_PATH, 'package.json');
+    if(!Helper.isFile(packageFile)){
+      return;
+    }
+
+    let data = JSON.parse(fs.readFileSync(packageFile, 'utf-8'));
+    let dependencies = Helper.extend({}, data.dependencies);
+    // merge devDependencies in development env
+    if(config.env === 'development') {
+      dependencies = Helper.extend(dependencies, data.devDependencies);
+    }
+
+    // package alias
+    let pkgAlias = {
+      'babel-runtime': 'babel-runtime/helpers/inherits'
+    };
+    let pkgPath = path.join(config.ROOT_PATH, 'node_modules/');
+    for(let pkg in dependencies) {
+      pkg = pkgAlias[pkg] || pkg;
+      let pkgFilename = path.join(pkgPath, pkg);
+      if(Helper.isDirectory(pkgFilename)) {
+        continue;
+      }
+    
+      try {
+        require(pkg);
+      } catch(e) {
+        logger.error(`package \`${pkg}\` is not installed. please run \`npm install\` command before server starts.`);
+      }
+    }
+  }
 }
-exports.checkDependencies = checkDependencies;
+ module.exports = Inspector;
--- packages/think-inspect/test/index.js
+++ packages/think-inspect/test/index.js
@@ -1,83 +1,200 @@
 import test from 'ava';
-import {
-  checkNodeVersion,
-  checkFileName,
-  checkDependencies
-} from '../index.js';
+import Inspector from '../index.js';
+import mock from 'mock-require';
 
-test('checkNodeVersion', t => {
-  let n = 0;
-  let result = checkNodeVersion({
-    THINK_PATH: 'test/demo/checkNodeVersion'
-  }, (errmsg, type)=> {
-    n ++;
-  });
-  t.is(n, 0);
-});
-test('checkNodeVersion1', t => {
-  let n = 0;
-  let result = checkNodeVersion({
-    THINK_PATH: 'test/demo/checkNodeVersion1'
-  },  (errmsg, type) => {
-    n ++;
-  });
-  t.is(n, 1);
-});
-test('checkNodeVersion2', t => {
-  let n = 0;
-   //not callback, return directly
-  let result = checkNodeVersion({
-    THINK_PATH: 'test/demo/checkNodeVersion2'
-  },  (errmsg, type) => {
-      n ++;
-  });
-  t.is(n, 0);
+const THINK_PATH = 'think_path';
+const APP_PATH = 'app_path';
+const ROOT_PATH = 'root_path';
+const pkgName = 'package.json';
+const locale = 'locale';
+const needVersion = ">=6.0.0";
+const realNodeVersion = process.version;
+
+test.beforeEach(t => {
+  const fs = require('fs');
+  const Helper = require('think-helper');
+    fs.___readdirSync = fs.readdirSync;
+    fs.___statSync = fs.statSync;
+    Helper.___isFile = Helper.isFile;
+    Helper.___isDirectory = Helper.isDirectory;
+    Helper.___getdirFiles = Helper.getdirFiles;
 });
 
+function mockHelperIsFile(res) {
+  const Helper = require('think-helper');
+  Helper.isFile = function() {return res;};
+}
 
+function mockReadPkgEnginesFileSync(t) {
+  const fs = require('fs');
+  fs.readFileSync = function(pkgPath) {
+    return JSON.stringify({
+      "engines": {
+        "node": needVersion
+      }
+    });
+  };
+}
+
+function mockNodeVersion(nodeVersion) {
+  Object.defineProperty(process, 'version', {  
+    value: nodeVersion
+  });
+}
+
+function testCheckNodeVersion(t, isFile, nodeVersion) {
+  mockHelperIsFile(isFile);
+  mockReadPkgEnginesFileSync(t);
+  mockNodeVersion(nodeVersion);
+  let checkNodeVersion_error = false;
+  const logger = {
+    error(msg) { 
+      console.log(msg);
+      checkNodeVersion_error = true;
+    }
+  };
+   
+  const inspector = new Inspector({
+    THINK_PATH,
+    APP_PATH,
+    ROOT_PATH,
+    logger: logger
+  });
+ 
+  inspector.checkNodeVersion();
+  return checkNodeVersion_error;
+}
+test('checkNodeVersion when it meets the requirement of package.json', t => {
+  const isFile = true;
+  const nodeVersion = "v6.0.0";
+  const checkNodeVersion_error = testCheckNodeVersion(t, isFile, nodeVersion);
+  t.is(checkNodeVersion_error, false);
+});
+
+test('checkNodeVersion when it is lower than the requirement of package.json', t => {
+  const isFile = true;
+  const nodeVersion = "5.0.0";
+  const checkNodeVersion_error = testCheckNodeVersion(t, isFile, nodeVersion);
+  t.is(checkNodeVersion_error, true);
+});
+
+test('checkNodeVersion when package.json is not a file', t => {
+  const isFile = false;
+  const nodeVersion = "6.0.0";
+  const checkNodeVersion_error = testCheckNodeVersion(t, isFile, nodeVersion);
+  t.is(checkNodeVersion_error, false);
+});
+
+function mockHelperGetDirFiles() {
+  const helper = require('think-helper');
+  helper.getdirFiles = function() {
+    return ['UPPER.js', 'a.js', 'b.css', 'c.html', 'd.tpl', '/locale/EN.js'];
+  }
+}
 test('checkFileName', t => {
-  checkFileName({
-    APP_PATH: 'test/demo/checkFileName',
-    sep: '/',
-    locale: 'locale'
-  }, (errmsg, type)=>{
-    t.is(type, 'WARNING');
+  let checkFileNameWran = false;
+  const logger = {
+    warn(msg) { 
+      console.log(msg);
+      checkFileNameWran = true;
+    }
+  };
+  mockHelperGetDirFiles();
+  const inspector = new Inspector({
+    APP_PATH,
+    locale,
+    logger
   });
+  inspector.checkFileName();
+  t.is(checkFileNameWran, true);
 });
 
-test('checkDependencies', t => {
-  let n = 0;
-  checkDependencies({
-    env: 'development',
-    ROOT_PATH: 'test/demo/checkDependencies',
-    sep: '/'
-  }, (errmsg, type) => {
-    n ++;
+
+function mockReadPkgDepSync(t) {
+  const fs = require('fs');
+  fs.readFileSync = function() {
+    return JSON.stringify({
+      "dependencies": {
+        "dep1": "",
+        "depNotExisted": "",
+        "depIsDir": ""
+      },
+      "devDependencies": {
+        "devDep1": "",
+        "babel-runtime": ""
+      }
+    });
+  };
+}
+
+function mockHelperIsDir() {
+  const Helper = require('think-helper');
+  Helper.isDirectory = function(filename) {
+    if(filename.indexOf('depIsDir') > -1) {
+      return true;
+    }
+  }
+}
+
+function mockRequire() {
+  // Don't mock "depNotExisted"
+  // and don't have a "depNotExisted" module in your project 
+  // when you do the test :)
+  mock("dep1", {___name: "dep1"});
+  mock("devDep1", {___name: "devDep1"});
+  mock("babel-runtime/helpers/inherits", {___name: "babel-runtime"});
+}
+
+function testCheckDependencies(t, isPkgFile, env) {
+  let checkDependencies_error = false;
+  const logger = {
+    error(msg) { 
+      console.log(msg);
+      checkDependencies_error = true;
+    }
+  };
+  mockHelperIsDir();
+  mockHelperIsFile(isPkgFile);
+  mockReadPkgDepSync(t);
+  mockRequire();
+  
+  const inspector = new Inspector({
+    env,
+    ROOT_PATH,
+    logger
   });
-  t.is(n, 0);
+
+  inspector.checkDependencies();
+  return checkDependencies_error;
+}
+test('checkDependencies when env is development', t => {
+  const env = 'development';
+  const isPkgFile = true;
+  const checkDependencies_error = testCheckDependencies(t, isPkgFile, env);
+  t.is(checkDependencies_error, true);
 });
-test('checkDependencies1', t => {
-  let n = 0;
-  //contains think-helper1 think-helper2 uninstalled
-  checkDependencies({
-    env: 'production',
-    ROOT_PATH: 'test/demo/checkDependencies1',
-    sep: '/'
-  }, (errmsg, type) => {
-    t.is(type, 'EXIT');
-    n ++;
-  });
-   t.true(n >= 1);
+test('checkDependencies when env is production', t => {
+  const env = 'production';
+  const isPkgFile = true;
+  const checkDependencies_error = testCheckDependencies(t, isPkgFile, env);
+  t.is(checkDependencies_error, true);
 });
-test('checkDependencies2', t => {
-  let n = 0;
-  //package.json is directory, return directly
-  checkDependencies({
-    env: 'production',
-    ROOT_PATH: 'test/demo/checkDependencies2',
-    sep: '/'
-  }, (errmsg, type) => {
-    n ++;
+test('checkDependencies when package.json is not file', t => {
+  const env = 'production';
+  const isPkgFile = false;
+  const checkDependencies_error = testCheckDependencies(t, isPkgFile, env);
+  t.is(checkDependencies_error, false);
+});
+
+test.afterEach.always(t => {
+  const fs = require('fs');
+  const Helper = require('think-helper');
+  fs.readdirSync =fs.___readdirSync;
+  fs.statSync = fs.___statSync;
+  Helper.isFile = Helper.___isFile;
+  Helper.isDirectory = Helper.___isDirectory;
+  Helper.getdirFiles = Helper.___getdirFiles;
+  Object.defineProperty(process, 'version', {  
+    value: realNodeVersion
   });
-  t.is(n, 0);
 });
