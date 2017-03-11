1c15f61407451bd8a83720ee1f611e7ab48fe8bb
lushijie <lushijie1218@126.com>
2017-03-11 12:28:19 +0800

    del unuse file

--- packages/think-view-handlebars/test.js
+++
@@ -1,9 +0,0 @@
-/*
-* @Author: lushijie
-* @Date:   2017-03-11 10:25:52
-* @Last Modified by:   lushijie
-* @Last Modified time: 2017-03-11 10:29:55
-*/
-const handlebars = require('handlebars');
-const viewPath = path.join(__dirname, 'test/views');
-handlebars.compile(source)(viewData);
