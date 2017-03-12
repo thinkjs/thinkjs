053017cfeb8bedd3a6419936c540934c75f7e191
lizheming <secretlzm007@gmail.com>
2017-03-12 18:08:55 +0800

    rm object.rest

--- packages/think-logger3/src/adapter/console.js
+++ packages/think-logger3/src/adapter/console.js
@@ -4,7 +4,8 @@ module.exports = class ConsoleLogger extends Base {
   constructor(config) {
     super(config);
 
-    let {level, ...lConfig} = config;
+    let lConfig = Object.assign({}, config);
+    let {level} = lConfig;
     level = level ? level.toUpperCase() : 'ALL';
 
     config = Object.assign({
