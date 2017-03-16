a750b1175a5016e18ffe9e2e5be6b2098f40c86a
lushijie <lushijie1218@126.com>
2017-03-16 15:51:37 +0800

    add test cache

--- packages/think-cache-file/test/index.js
+++ packages/think-cache-file/test/index.js
@@ -2,7 +2,7 @@
 * @Author: lushijie
 * @Date:   2017-03-16 09:23:29
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-03-16 15:02:17
+* @Last Modified time: 2017-03-16 15:50:43
 */
 import test from 'ava';
 import helper from 'think-helper';
@@ -17,21 +17,54 @@ let myOptions = {
   path_depth: 1
 };
 
+test.serial.cb.beforeEach(t => {
+  let cachePath = path.join(__dirname, 'cache');
+  helper.rmdir(cachePath, false).then(() => {
+    t.end();
+  });
+});
+
 test.serial('set key & get key & del key', async t => {
   let config = helper.extend({}, myOptions);
-  let cacheInst = new FileCache(myOptions);
-
-  //set
+  let cacheInst = new FileCache(config);
   await cacheInst.set('name', 'thinkjs');
-
-  // get1
   let ret1 = await cacheInst.get('name');
-
-  //del
   await cacheInst.delete('name');
-
-  // get2
   let ret2 = await cacheInst.get('name');
 
   t.true(ret1 === 'thinkjs' && ret2 === undefined);
+});
+
+test.serial('set key & get key & del key without path_depth', async t => {
+  let config = helper.extend({}, myOptions, {path_depth: null});
+  let cacheInst = new FileCache(config);
+  await cacheInst.set('name', 'thinkjs');
+  let ret1 = await cacheInst.get('name');
+  t.true(ret1 === 'thinkjs');
+});
+
+
+test.serial('get expired key', async t => {
+  let config = helper.extend({}, myOptions);
+  let cacheInst = new FileCache(config);
+  await cacheInst.set('name1', 'thinkjs', -1);
+  let ret = await cacheInst.get('name1');
+
+  t.true(ret === undefined);
+});
+
+
+test.serial('gc', async t => {
+  let config1 = helper.extend({}, myOptions);
+  let cacheInst1 = new FileCache(config1);
+  await cacheInst1.set('name1', 'thinkjs', -1);
+
+  let config2 = helper.extend({}, myOptions);
+  let cacheInst2 = new FileCache(config2);
+  await cacheInst2.set('name2', 'thinkjs');
+
+  // cacl name1's cache path
+  let cacheExpiredPath = path.join(__dirname, 'cache', helper.md5('name1').slice(0, config1.path_depth).split('').join(path.sep));
+
+  t.true(helper.getdirFiles(cacheExpiredPath).length === 0);
 });
