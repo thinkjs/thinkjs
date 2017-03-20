bd6c689ec9a264910445b490f7b06bdaeaadcfe2
lizheming <secretlzm007@gmail.com>
2017-03-20 21:27:50 +0800

    if 404 error

--- packages/think-trace/src/index.js
+++ packages/think-trace/src/index.js
@@ -5,13 +5,19 @@ module.exports = function(opts) {
   let tracer = new Tracer(opts);
 
   // source map support for compiled file
-  if( opts.sourceMap !== false ) {
+  if( opts && opts.sourceMap !== false ) {
     sourceMapSupport.install();
   }
 
   return (ctx, next) => 
     tracer.getTemplateContent()
     .then(next)
-    .then(() => tracer.is404(ctx))
+    .then(() => {
+      if( ctx.res.statusCode !== 404 ) {
+        return true;
+      }
+
+      return ctx.throw(404, `url \`${ctx.path}\` not found.`);
+    })
     .catch(err => tracer.run(ctx, err));
 }
