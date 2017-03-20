018e423c29ed00e7d052326f598c9d00e47f2fc4
lizheming <secretlzm007@gmail.com>
2017-03-20 21:31:52 +0800

    add errMsg and 404 page

--- packages/think-trace/src/tracer.js
+++ packages/think-trace/src/tracer.js
@@ -8,14 +8,10 @@ const DEFAULT_404_TEMPLATE = path.join(__dirname, 'template/404.html');
 const DEFAULT_500_TEMPLATE = path.join(__dirname, 'template/500.html');
 
 module.exports = class Tracer {
-  constructor(opts = {
-    ctxLineNumbers: 10, 
-    err404Template: DEFAULT_404_TEMPLATE,
-    err500Template: DEFAULT_500_TEMPLATE
-  }) {
-    this.ctxLineNumbers = opts.ctxLineNumbers;
-    this.err404Template = opts.err404Template;
-    this.err500Template = opts.err500Template;
+  constructor({ctxLineNumbers, err404Template, err500Template}) {
+    this.ctxLineNumbers = ctxLineNumbers || 10;
+    this.err404Template = err404Template || DEFAULT_404_TEMPLATE;
+    this.err500Template = err500Template || DEFAULT_500_TEMPLATE;
   }
 
   /**
@@ -25,14 +21,14 @@ module.exports = class Tracer {
     return Promise.all([
       /** 404 */
       readFileAsync(this.err404Template, {encoding: 'utf-8'})
-        .catch(() => readFileAsync(DEFAULT_404_TEMPLATE, {encoding: 'utf-8'}))
-        .then(templateContent => this.err404TemplateContent = templateContent),
-        
+        .catch(() => readFileAsync(DEFAULT_404_TEMPLATE, {encoding: 'utf-8'})),
       /** 500 */
       readFileAsync(this.err500Template, {encoding: 'utf-8'})
         .catch(() => readFileAsync(DEFAULT_500_TEMPLATE, {encoding: 'utf-8'}))
-        .then(templateContent => this.err500TemplateContent = temlateContent)
-    ]);
+    ]).then(([template404Content, template500Content]) => {
+      this.err404TemplateContent = template404Content;
+      this.err500TemplateContent = template500Content;
+    });
   }
 
   /**
@@ -46,9 +42,9 @@ module.exports = class Tracer {
     return readFileAsync(filename, {encoding: 'utf-8'}).then(data => {
       let startLineNumber = lineNumber - this.ctxLineNumbers;
       let endLineNumber = lineNumber + this.ctxLineNumbers;
-      let content = data.split(/[\r\n]+/).slice(startLineNumber, endLineNumber);
-
-      line.content = content;
+      let content = data.split('\n').slice(startLineNumber, endLineNumber);
+      
+      line.content = content.join('\n');
       line.getContent = function() { return this.content; };
       line.startLineNumber = startLineNumber < 0 ? 1 : startLineNumber + 1;
       line.getStartLineNumber = function() { return this.startLineNumber; };
@@ -61,9 +57,22 @@ module.exports = class Tracer {
    * render error page
    * @param {*Array} stacks stacke tracer array
    */
-  render(stacks) {
+  render500(stacks, err) {
     let error = JSON.stringify(stacks, null, '\t');
-    this.ctx.body = this.templateContent.replace('{{error}}', error);
+    return this.err500TemplateContent
+      .replace('{{error}}', error)
+      .replace('{{errMsg}}', err.toString());
+  }
+
+  /**
+   * render 404 not found page
+   * @param {*Error} err Error instance
+   */
+  render404(err) {
+    error = 'controller not found';
+    return this.err404TemplateContent
+      .replace('{{error}}', error)
+      .replace('{{errMsg}}', err.toString());
   }
 
   /**
@@ -77,12 +86,16 @@ module.exports = class Tracer {
       err = new Error(err);
     }
 
-    let stack = stackTrace.parse(err);
-    stack._rawErr = err;
+    // 404 not found error
+    if( err.status === 404 ) {
+      ctx.body = this.render404(ctx, err);
+      return true;
+    }
     
+    let stack = stackTrace.parse(err);
     return Promise.all( 
       stack.map(this.getFile.bind(this)) 
     ).then(stacks => stacks.filter(stack => stack))
-     .then(this.render.bind(this));
+     .then(stacks => ctx.body = this.render500(stacks, err));
   }
 }
