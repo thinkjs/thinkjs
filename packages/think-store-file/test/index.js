cd9c288e5e0ebe1225d45f6b9a27ec572902caf8
lushijie <lushijie1218@126.com>
2017-03-21 11:31:12 +0800

    add test case

--- packages/think-store-file/test/index.js
+++ packages/think-store-file/test/index.js
@@ -2,7 +2,7 @@
 * @Author: lushijie
 * @Date:   2017-03-16 09:23:29
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-03-19 14:51:52
+* @Last Modified time: 2017-03-21 11:30:32
 */
 import test from 'ava';
 import helper from 'think-helper';
@@ -30,6 +30,16 @@ test.serial('set file & get file & del file', async t => {
 
   t.true(ret1 === content && !ret2)
 });
+
+test.serial('set file & get file empty', async t => {
+  let content = '';
+  let relativePath = 'abc/a.js';
+  let storeInst = new FileStore(path.join(__dirname, 'data'));
+  await storeInst.set(relativePath, content);
+  let ret1 = await storeInst.get(relativePath);
+  t.true(ret1 === '')
+});
+
 
 test.serial('use exist dir', async t => {
   let content = 'Welcome to Thinkjs\'s World';
