b72c8069ff9ccc6528c0f3efbd01284811737de0
welefen <welefen@gmail.com>
2017-03-22 10:19:07 +0800

    Add gc method

--- packages/think-session-file/index.js
+++ packages/think-session-file/index.js
@@ -4,9 +4,14 @@ const helper = require('think-helper');
 const path = require('path');
 const debug = require('debug')('think-session-file');
 const gc = require('think-gc');
+const fs = require('fs');
+
+const readFile = helper.promisify(fs.readFile, fs);
+const unlink = helper.promisify(fs.unlink, fs);
 
 const initSessionData = Symbol('think-session-file-init');
 const autoSave = Symbol('think-session-save');
+
 /**
  * use file to store session
  */
@@ -17,13 +22,15 @@ class FileSession {
    * @param {Object} ctx 
    */
   constructor(options, ctx){
-    assert(options.sessionPath && path.isAbsolutePath(options.sessionPath), '.sessionPath required');
+    assert(options.sessionPath && path.isAbsolute(options.sessionPath), '.sessionPath required');
     assert(options.cookie, '.cookie required');
+
     this.options = options;
     this.ctx = ctx;
     this.fileStore = new FileStore(options.sessionPath);
     this.data = {};
     this.status = 0;
+
     this.gcType = `session_${options.sessionPath}`;
     gc(this, this.options.gcInterval);
   }
@@ -34,7 +41,7 @@ class FileSession {
     if(this.initPromise){
       return this.initPromise;
     }
-    if(this.options.fresh){
+    if(this.options.fresh || this.status === -1){
       return this.initPromise = Promise.resolve();
     }
     this.initPromise = this.fileStore.get(this.options.cookie).then(content => {
@@ -55,9 +62,9 @@ class FileSession {
   [autoSave](){
     this.ctx.res.once('finish', () => {
       if(this.status === -1){
-        return this.fileStore.delete(this.cookie);
+        return this.fileStore.delete(this.options.cookie);
       }else if(this.status === 1){
-        return this.fileStore.set(this.cookie, JSON.stringify({
+        return this.fileStore.set(this.options.cookie, JSON.stringify({
           expires: Date.now() + this.options.maxAge,
           data: this.data
         }));
@@ -69,21 +76,21 @@ class FileSession {
    * @param {String} name 
    */
   get(name){
-    if(this.options.autoUpdate){
-      this.status = 1;
-    }
-    return this.initPromise().then(() => {
+    return this[initSessionData]().then(() => {
+      if(this.options.autoUpdate){
+        this.status = 1;
+      }
       return name ? this.data[name] : this.data;
     });
   }
   /**
-   * 
+   * set session data
    * @param {String} name 
    * @param {Mixed} value 
    */
   set(name, value){
-    this.status = 1;
-    return this.initPromise().then(() => {
+    return this[initSessionData]().then(() => {
+      this.status = 1;
       this.data[name] = value;
     });
   }
@@ -93,12 +100,25 @@ class FileSession {
   delete(){
     this.status = -1;
     this.data = {};
+    return Promise.resolve();
   }
   /**
    * gc
    */
   gc(){
-
+    let files = helper.getdirFiles(this.options.sessionPath);
+    files.forEach(file => {
+      let filePath = path.join(this.options.sessionPath, file);
+      return readFile(filePath, 'utf8').then(content => {
+        if(!content) return Promise.reject(new Error('content empty'));
+        content = JSON.parse(content);
+        if(Date.now() > content.expires){
+          return Promise.reject(new Error('session file expired'));
+        }
+      }).catch(() => {
+        return unlink(filePath);
+      });
+    })
   }
 }
 
