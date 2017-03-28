3f27f9a3f7b83bb61a2751f9fa8e9c5d69afd598
welefen <welefen@gmail.com>
2017-03-28 14:19:17 +0800

    Fixed test errors

--- packages/think-loader/test/extend_allowextends.js
+++ packages/think-loader/test/extend_allowextends.js
@@ -2,5 +2,5 @@ const test = require('ava');
 
 test('extend.allowExtends', t=>{
   var allowExtends = require('../loader/extend').allowExtends;
-  t.deepEqual(allowExtends, ['think', 'context', 'request', 'response', 'controller', 'logic']);
+  t.deepEqual(allowExtends, ['think', 'application', 'context', 'request', 'response', 'controller', 'logic']);
 });
