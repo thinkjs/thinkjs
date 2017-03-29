17a9c25c5fe0d4dd723d3732bffca4ad553328d5
zhengqingxin <362965772@qq.com>
2017-03-29 14:25:02 +0800

    add test cases

--- packages/think-session-cookie/test/index.js
+++ packages/think-session-cookie/test/index.js
@@ -1,32 +1,47 @@
 const test = require('ava');
 const mock = require('mock-require');
 
-function getSessionCookie() {
-  return mock.reRequire('../index');
-}
-
 function mockAssert(assertCallParams = []) {
   mock('assert', (type, desc) => {
     assertCallParams.push(type, desc);
-    throw new Error("this is an assert failed signal");
+    if(!type){
+      throw new Error("this is an assert failed signal");
+    }
   });
+}
+
+function getSessionCookie() {
+  return mock.reRequire('../index');
 }
 
 const defaultCtx = {
   cookieStore: {
     test: JSON.stringify({id: 1, value: 'test'})
   },
-  cookie(name){
-    return this.cookieStore[name];
+  cookie(name,data){
+    if(!data){
+      return this.cookieStore[name];
+    }
+    this.cookieStore[name] = data;
+  },
+  clear(){
+    this.cookieStore = {
+      test: JSON.stringify({id: 1, value: 'test'})
+    };
   }
 };
+
+test.afterEach.always(_ => {
+  mock.stop('assert');
+  defaultCtx.clear();
+});
 
 test('constructor function -- option.encrypt with empty keys', t => {
   let assertCallParams = [];
   mockAssert(assertCallParams);
   const options = {
     encrypt: 'test',
-    name:'cookie_name'
+    name: 'cookie_name'
   };
   const SessionCookie = getSessionCookie();
 
@@ -47,7 +62,7 @@ test('constructor function -- option.encrypt with not array keys', t => {
   mockAssert(assertCallParams);
   const options = {
     encrypt: 'test',
-    name:'cookie_name'
+    name: 'cookie_name'
   };
   const SessionCookie = getSessionCookie();
 
@@ -93,7 +108,7 @@ test('set/get function -- normal scene', async t => {
 
   const SessionCookie = getSessionCookie();
   const sc = new SessionCookie(options, defaultCtx);
-  await sc.set('username','thinkjs');
+  await sc.set('username', 'thinkjs');
   let value = await sc.get('username');
 
   t.deepEqual(value, 'thinkjs');
@@ -106,17 +121,42 @@ test('set/get function -- encrypt', async t => {
     encrypt: 'test',
     keys: ['a', 'b']
   };
-
   const SessionCookie = getSessionCookie();
   const sc = new SessionCookie(options, defaultCtx);
-  await sc.set('username','thinkjs');
+  await sc.set('username', 'thinkjs');
   let value = await sc.get('username');
 
   t.deepEqual(value, 'thinkjs');
 });
 
+test('get function -- autoUpdate && maxAge name', async t => {
+  const options = {
+    name: 'test',
+    encrypt: 'test',
+    keys: ['a', 'b'],
+    autoUpdate: true,
+    maxAge: 30000
+  };
+  const ctx = {
+    cookieStore: {},
+    cookie(name,data){
+      if(!data){
+        return this.cookieStore[name];
+      }
+      if(options && options.autoUpdate && options.maxAge){
+        this.cookieStore = {};
+        return this.cookieStore;
+      }
+      this.cookieStore[name] = data;
+    }
+  };
+  const SessionCookie = getSessionCookie();
+  const sc = new SessionCookie(options, ctx);
 
-
+  sc.fresh = false;
+  const result = await sc.get();
+  t.deepEqual(result, {});
+});
 
 
 
