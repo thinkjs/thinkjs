b465233fa3e25f8f2e8ad5d2e883ca3b534e2c6b
夏明星 <xiamingxing@360.cn>
2017-04-10 18:00:07 +0800

    initial project

---
+++ packages/think-resource/index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const debug = require('debug')('think-resource');
+const resolve = require('path').resolve;
+const assert = require('assert');
+const send = require('koa-send');
+
+/**
+ * defaultOptions
+ * @type {{root: string, index: string, hidden: boolean, format: boolean, gzip: boolean, extensions: boolean, maxage: number, setHeaders: boolean}}
+ */
+const defaultOptions = {
+  root: '',
+  index: 'index.html',
+  hidden: false,
+  format: false,
+  gzip: false,
+  extensions: false,
+  maxage: 0,
+  setHeaders: false
+}
+
+/**
+ * serve wrapper by koa-send
+ * @param options
+ * @returns {serve}
+ */
+module.exports = function (options) {
+  options = Object.assign(defaultOptions, options);
+
+  const root = options.root
+  assert(options.root, 'root directory is required to serve files');
+  debug('static "%s" %j', root, options);
+  options.root = resolve(root);
+
+  /**
+   * serve
+   */
+  return function serve(ctx, next){
+    if (ctx.method == 'HEAD' || ctx.method == 'GET') {
+      return send(ctx, ctx.path, options).then(done => {
+        if (!done) {
+          return next();
+        }
+      });
+    }
+    return next();
+  };
+}
