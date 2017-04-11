ca3575b330f2ae5f3adfb3b20a677affb85e3cc5
夏明星 <xiamingxing@360.cn>
2017-04-11 16:15:27 +0800

    test

--- packages/think-resource/index.js
+++ packages/think-resource/index.js
@@ -1,4 +1,9 @@
 'use strict';
+require('babel-polyfill');
+require('babel-register')({
+  ignore: false,
+  cache: true
+});
 
 const debug = require('debug')('think-resource');
 const resolve = require('path').resolve;
