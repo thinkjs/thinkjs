981f58152ddac039796a1ada3b4761d5c83cd928
zhengqingxin <362965772@qq.com>
2017-05-02 17:34:40 +0800

    add test case

--- packages/think-crontab/test/index.js
+++ packages/think-crontab/test/index.js
@@ -1,6 +1,6 @@
 const test = require('ava');
 const mock = require('mock-require');
-
+const helper = require('think-helper');
 function getCrontab() {
   return mock.reRequire('../index');
 }
@@ -12,4 +12,47 @@ function mockAssert(assertCallParams = []) {
 }
 
 test('constructor function', t => {
+  let Crontab = getCrontab();
+  let option = 'crontab/test';
+  let cron = new Crontab(option);
+  t.deepEqual(helper.isFunction(cron.options[0].handle), true);
+  t.is(cron.options[0].worker, 'one');
+});
+
+test('constructor function', t => {
+  let Crontab = getCrontab();
+  let option = {
+    handle:'crontab/test',
+    worker:'one'
+  };
+  let cron = new Crontab(option);
+  t.deepEqual(helper.isFunction(cron.options[0].handle), true);
+  t.is(cron.options[0].worker, 'one');
+});
+
+test('constructor function', t => {
+  let Crontab = getCrontab();
+  let option = {
+    handle(){
+      console.log('test');
+    },
+    worker:'one',
+    enable:true
+  };
+  let cron = new Crontab(option);
+  t.deepEqual(helper.isFunction(cron.options[0].handle), true);
+  t.is(cron.options[0].worker, 'one');
+});
+
+test('constructor function', t => {
+  let Crontab = getCrontab();
+  let option = {
+    handle(){
+      console.log('test');
+    },
+    worker:'one',
+    enable:false
+  };
+  let cron = new Crontab(option);
+  t.deepEqual(cron.options, []);
 });
