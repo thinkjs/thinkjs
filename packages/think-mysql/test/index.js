67577b789484e58410cb4f2b0e7903cd82c0ec38
zhengqingxin <362965772@qq.com>
2017-05-05 14:07:20 +0800

    add test case

--- packages/think-mysql/test/index.js
+++ packages/think-mysql/test/index.js
@@ -1,20 +1,49 @@
 import test from 'ava';
 const helper = require('think-helper');
+const mock = require('mock-require');
 const mysql = require('../index.js');
 
+
+function getMysql() {
+  return mock.reRequire('../index');
+}
 const config = {
   user: 'root',
-  password: '',
+  password: 'Hello@123',
   database: 'think_test',
   logConnect: true,
   logSql: true,
   connectionLimit: 1
 };
-const instance = mysql.getInstance(config);
+
+test.afterEach.always(_ => {
+  // instance.close();
+});
 
 test('constructor function', async t => {
+  const mysql = getMysql();
+  let instance = mysql.getInstance(config);
   let conn = await instance.getConnection();
   t.is(helper.isEmpty(instance.config),false);
   t.is(helper.isEmpty(instance.pool),false);
   t.is(helper.isEmpty(conn),false);
+});
+
+test('query function', async t => {
+  const mysql = getMysql();
+  let instance = mysql.getInstance(config);
+
+  await instance.execute({
+    sql:"insert into `think_test`.`books` (`name`, `author`) values ('thinkjs best practice', ?)",
+    timeout: 5000,
+    values: ['David']
+  });
+
+  let books = await instance.query({
+    sql:'SELECT * FROM `books` WHERE `author` = ?',
+    timeout: 5000,
+    values: ['David']
+  });
+
+  t.is(books[0].name,'thinkjs best practice');
 });
