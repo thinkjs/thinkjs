b07d8a39c3bf1e07f0d0f94f9d92c2663712d1f2
berwin <berwin1995@qq.com>
2017-05-06 18:18:05 +0800

    update

--- packages/think-payload/lib/text.js
+++ packages/think-payload/lib/text.js
@@ -5,8 +5,8 @@ exports.before = function (ctx, opts = {}) {
   const req = ctx.req;
 
   // defaults
-  var len = req.headers['content-length'];
-  var encoding = req.headers['content-encoding'] || 'identity';
+  let len = req.headers['content-length'];
+  const encoding = req.headers['content-encoding'] || 'identity';
   if (len && encoding === 'identity') opts.length = ~~len;
   opts.encoding = opts.encoding || 'utf8';
   opts.limit = opts.limit || '1mb';
