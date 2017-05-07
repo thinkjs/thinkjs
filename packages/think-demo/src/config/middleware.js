365daa4fc2295f0f10a9dffe09b0c96e8098ae6e
berwin <berwin1995@qq.com>
2017-05-07 17:23:29 +0800

    add think-payload

--- packages/think-demo/src/config/middleware.js
+++ packages/think-demo/src/config/middleware.js
@@ -2,6 +2,7 @@ const router = require('think-router');
 const trace = require('think-trace');
 const meta = require('think-meta');
 const logic = require('think-logic');
+const payload = require('think-payload');
 const controller = require('think-controller');
 
 module.exports = [{
@@ -17,6 +18,9 @@ module.exports = [{
     options: {}
   },{
     handle: logic
+  },{
+    handle: payload,
+    options: {}
   },{
     handle: controller
   }
