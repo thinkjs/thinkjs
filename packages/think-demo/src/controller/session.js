a6c8ec2bfa4e15da03c1cdffb1766a770e7c1108
welefen <welefen@gmail.com>
2017-05-14 14:09:24 +0800

    add session

---
+++ packages/think-demo/src/controller/session.js
@@ -0,0 +1,29 @@
+module.exports = class SessionController extends think.Controller {
+  indexAction(){
+    this.success(231);
+  }
+  /**
+   * set session
+   */
+  async setAction(){
+    await this.session('name', 'value');
+    let value = await this.session('num') | 0;
+    await this.session('num', value + 1);
+    await this.session('test', {name: 'test'})
+    return this.success();
+  }
+  /**
+   * get session
+   */
+  async getAction(){
+    let data = await this.session();
+    return this.success(data);
+  }
+  /**
+   * remove session
+   */
+  async rmAction(){
+    await this.session(null);
+    return this.success();
+  }
+}
