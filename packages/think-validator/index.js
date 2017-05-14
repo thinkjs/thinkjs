1f817b321d685cebe9d30cb6454cfd606f71413d
lushijie <lushijie1218@126.com>
2017-05-14 15:13:05 +0800

    update ruleName to argName

--- packages/think-validator/index.js
+++ packages/think-validator/index.js
@@ -2,11 +2,11 @@
 * @Author: lushijie
 * @Date:   2017-02-21 18:50:26
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-05-14 14:23:11
+* @Last Modified time: 2017-05-14 15:10:55
 */
 const validator = require('./rules.js');
 const helper = require('think-helper');
-const ARRAY_SP = '_array_', OBJECT_SP = '_object_';
+const ARRAY_SP = '__array__', OBJECT_SP = '__object__';
 const METHOD_MAP = {
   get: 'param',
   post: 'post',
@@ -31,12 +31,12 @@ class Validator {
   }
 
   /**
-   * format ruleName nested array and object
-   * @param  {String} ruleName [description]
+   * format argName nested array and object
+   * @param  {String} argName [description]
    * @return {String}          [description]
    */
-  _formatNestedRuleName(ruleName) {
-    let newRuleName = ruleName;
+  _formatNestedRuleName(argName) {
+    let newRuleName = argName;
     if(newRuleName.indexOf(ARRAY_SP) > -1) {
       let tmpRuleName = newRuleName.split(ARRAY_SP);
       newRuleName = tmpRuleName[0] + '[' + tmpRuleName[1]+ ']';
@@ -50,20 +50,20 @@ class Validator {
 
   /**
    * get error message
-   * @param  {String} ruleName        [description]
+   * @param  {String} argName        [description]
    * @param  {Object} rule            [description]
    * @param  {String} validName       [description]
    * @param  {Mixed} parsedValidArgs [description]
    * @param  {Object} msgs            [description]
    * @return {String}                 [description]
    */
-  _getErrorMessage(ruleName, rule, validName, parsedValidArgs, msgs) {
+  _getErrorMessage(argName, rule, validName, parsedValidArgs, msgs) {
     let errMsg = validator.errors[validName];
     if(helper.isObject(msgs)) {
       // eg int: 'error message'
       errMsg = msgs[validName];
 
-      let msgs_RuleName = msgs[ruleName];
+      let msgs_RuleName = msgs[argName];
       // eg name: 'error message'
       if(msgs_RuleName && helper.isString(msgs_RuleName)) {
         errMsg = msgs_RuleName;
@@ -76,10 +76,10 @@ class Validator {
 
       // eg name: {name1,name2: 'error message'}
       // eg name: {name1,name2: {int: 'error message'}}
-      if(ruleName.indexOf(OBJECT_SP) > -1) {
-        let parsedResult = ruleName.split(OBJECT_SP);
-        ruleName = parsedResult[0];
-        let msgs_RuleName = msgs[ruleName];
+      if(argName.indexOf(OBJECT_SP) > -1) {
+        let parsedResult = argName.split(OBJECT_SP);
+        argName = parsedResult[0];
+        let msgs_RuleName = msgs[argName];
         let subRuleName = parsedResult[1];
         if(msgs_RuleName) {
           for(let i in msgs_RuleName) {
@@ -96,18 +96,19 @@ class Validator {
     }
 
     // format error message rule name
-    let originRuleName = this._formatNestedRuleName(ruleName);
+    let originRuleName = this._formatNestedRuleName(argName);
 
     // set defalut error message
     if(!errMsg) {
       return originRuleName + ' valid failed';
     }
 
-    // ruleName validName validArgs parsedValidArgs
-    let validArgs = rule[ruleName];
-    return errMsg.replace('{name}', originRuleName)
-                 .replace('{args}', JSON.stringify(validArgs))
-                 .replace('{pargs}', JSON.stringify(parsedValidArgs));
+    // argName validName validArgs parsedValidArgs
+    let validArgs = rule[argName];
+    let lastMsg = errMsg.replace('{name}', originRuleName)
+      .replace('{args}', JSON.stringify(validArgs))
+      .replace('{pargs}', JSON.stringify(parsedValidArgs));
+    return lastMsg;
   }
 
   /**
@@ -120,7 +121,7 @@ class Validator {
     let ruleArgs = rule[validName];
     let pfn = validator['_' + validName];
     if(helper.isFunction(pfn)){
-      // support rewrite back
+      // support rewrite back, so just pass this.ctxQuery without clone
       ruleArgs = pfn(ruleArgs, this.ctxQuery);
     }
     return ruleArgs;
@@ -128,23 +129,23 @@ class Validator {
 
   /**
    * convert value by value type
-   * @param  {String} ruleName [description]
+   * @param  {String} argName [description]
    * @param  {Object} rule     [description]
    * @return {Mixed}          [description]
    */
-  _convertParamValue(ruleName, rule) {
+  _convertParamValue(argName, rule) {
     let queryMethod = this._getQueryMethod(rule);
     if((rule.int || rule.float || rule.numeric) && queryMethod) {
-      if(ruleName.indexOf(ARRAY_SP) > -1) {
-        let parsedRuleName = ruleName.split(ARRAY_SP);
+      if(argName.indexOf(ARRAY_SP) > -1) {
+        let parsedRuleName = argName.split(ARRAY_SP);
         this.ctxQuery[parsedRuleName[0]][parsedRuleName[1]] = parseFloat(rule.value);
       }
-      else if (ruleName.indexOf(OBJECT_SP) > -1) {
-        let parsedRuleName = ruleName.split(OBJECT_SP);
+      else if (argName.indexOf(OBJECT_SP) > -1) {
+        let parsedRuleName = argName.split(OBJECT_SP);
         this.ctxQuery[parsedRuleName[0]][parsedRuleName[1]] = parseFloat(rule.value);
       }
       else {
-        this.ctxQuery[ruleName] = parseFloat(rule.value);
+        this.ctxQuery[argName] = parseFloat(rule.value);
       }
     }
   }
@@ -197,8 +198,8 @@ class Validator {
     // to keep the nested rules split from the array or object
     let childRules = {};
 
-    for(let ruleName in rules) {
-      let rule = rules[ruleName];
+    for(let argName in rules) {
+      let rule = rules[argName];
 
       // basic type check, only one basic type is legal(ok)
       let containTypeNum = this.basicType.reduce((acc, val) => {
@@ -207,7 +208,7 @@ class Validator {
       }, 0);
 
       if(containTypeNum > 1) {
-        throw new Error('Any rule can\'t contains one more basic type, the param you are validing is ' + ruleName);
+        throw new Error('Any rule can\'t contains one more basic type, the param you are validing is ' + argName);
       }
 
       // set this.ctxQuery
@@ -216,7 +217,7 @@ class Validator {
 
       // set related value on ctx to rule.value first
       if(!rule.value) {
-        rule.value = this.ctxQuery[ruleName];
+        rule.value = this.ctxQuery[argName];
       }
 
       // set default, when rule.value is undefined
@@ -240,25 +241,25 @@ class Validator {
       }
 
       // write back rule.value to ctx, nested children wait for next round to handle
-      if(typeof rule.value !== 'undefined' && ruleName.indexOf(ARRAY_SP) === -1 & ruleName.indexOf(OBJECT_SP) === -1 && queryMethod){
-        this.ctxQuery[ruleName] = rule.value;
+      if(typeof rule.value !== 'undefined' && argName.indexOf(ARRAY_SP) === -1 & argName.indexOf(OBJECT_SP) === -1 && queryMethod){
+        this.ctxQuery[argName] = rule.value;
       }
 
       // array & object children split and set the value
       if(rule.children) {
         let ruleValue = rule.value;
-        let ruleChildren = rules[ruleName].children;
-        // delete the ruleName, like [array|object]: true
-        delete rules[ruleName];
+        let ruleChildren = rules[argName].children;
+        // delete the argName, like [array|object]: true
+        delete rules[argName];
 
         if(rule.array) {
           for(let i = 0; i < ruleValue.length; i++) {
-            let tmpRuleName = ruleName + ARRAY_SP + i;
+            let tmpRuleName = argName + ARRAY_SP + i;
             childRules[tmpRuleName] = helper.extend({}, ruleChildren, {value: ruleValue[i]});
           }
         }else {
           for(let key in ruleValue) {
-            let tmpRuleName = ruleName + OBJECT_SP + key;
+            let tmpRuleName = argName + OBJECT_SP + key;
             childRules[tmpRuleName] = helper.extend({}, ruleChildren, {value: ruleValue[key]});
           }
         }
@@ -286,23 +287,23 @@ class Validator {
    * validate rules
    * @param  {Object} rules [description]
    * @param  {Object} msgs  [custom errors]
-   * @return {Object}       {ruleName: errorMessage}
+   * @return {Object}       {argName: errorMessage}
    */
   validate(rules, msgs) {
     let ret = {};
     let parsedRules = this._preTreatRules(rules);
 
     outerLoop:
-    for(let ruleName in parsedRules){
-      let rule = parsedRules[ruleName];
+    for(let argName in parsedRules){
+      let rule = parsedRules[argName];
 
       // required check
       let isRequired = this._checkRequired(rule);
       if(isRequired && helper.isTrueEmpty(rule.value)) {
         let validName = 'required';
         let parsedValidArgs = this._parseValidArgs(validName, rule);
-        let errMsg = this._getErrorMessage(ruleName, rule, validName, parsedValidArgs, msgs);
-        ret[ruleName] = errMsg;
+        let errMsg = this._getErrorMessage(argName, rule, validName, parsedValidArgs, msgs);
+        ret[argName] = errMsg;
         continue;
       }else if(!isRequired && helper.isTrueEmpty(rule.value)){
         continue;
@@ -325,15 +326,15 @@ class Validator {
 
         let result = fn(rule.value, parsedValidArgs);
         if(!result){
-          let errMsg = this._getErrorMessage(ruleName, rule, validName, parsedValidArgs, msgs);
+          let errMsg = this._getErrorMessage(argName, rule, validName, parsedValidArgs, msgs);
 
           // format error message's rule name
-          let newRuleName = this._formatNestedRuleName(ruleName);
+          let newRuleName = this._formatNestedRuleName(argName);
           ret[newRuleName] = errMsg;
           continue outerLoop;
         }else {
           // if this is no error, convert the value
-          this._convertParamValue(ruleName, rule);
+          this._convertParamValue(argName, rule);
         }
       }
     }
