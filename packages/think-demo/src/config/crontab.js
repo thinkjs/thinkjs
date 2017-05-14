5377cc699b2a6de7bef19844c321e102931a4a01
welefen <welefen@gmail.com>
2017-05-14 14:15:20 +0800

    update config

--- packages/think-demo/src/config/crontab.js
+++ packages/think-demo/src/config/crontab.js
@@ -1,13 +1,15 @@
 module.exports = [{
   interval: '1s',
   type: 'all',
+  enable: false,
   handle: function(){
-    //console.log(`crontab all, pid: ${process.pid}`);
+    console.log(`crontab all, pid: ${process.pid}`);
   }
 }, {
   interval: '1s',
   type: 'one',
+  enable: false,
   handle: function(){
-    //console.log(`crontab one, pid: ${process.pid}`)
+    console.log(`crontab one, pid: ${process.pid}`)
   }
 }]
