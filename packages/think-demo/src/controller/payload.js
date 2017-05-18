ca4b4bed13d2608cc2955e9883405729779369ce
berwin <berwin1995@qq.com>
2017-05-18 19:05:41 +0800

    add payload demo

---
+++ packages/think-demo/src/controller/payload.js
@@ -0,0 +1,17 @@
+module.exports = class PayloadController extends think.Controller {
+  async indexAction() {
+    await this.display('payload/index.html');
+  }
+
+  async requestAction() {
+    await this.display('payload/request.html');
+  }
+
+  async uploadAction() {
+    await this.display('payload/upload.html');
+  }
+
+  async demoAction() {
+    this.ctx.body = this.ctx.request.body;
+  }
+}
