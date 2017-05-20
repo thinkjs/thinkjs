9d42fd681cff427c508349fe3df08d5c7ec413ff
lizheming <secretlzm007@gmail.com>
2017-05-20 16:40:21 +0800

    change index controller

--- packages/think-demo/src/controller/index.js
+++ packages/think-demo/src/controller/index.js
@@ -6,8 +6,8 @@ module.exports = class IndexController extends think.Controller {
     this.assign('jed', jed);
     this.assign('numeral', numeral);
     this.assign('moment', moment);
-    const result = await this.fetch('https://cnodejs.org/api/v1/topic/56e688a983cbb63b6d120300').then(res => res.json());
-    this.assign('title', result.data.title);
+    const result = await this.fetch('https://api.github.com/orgs/thinkjs').then(res => res.json());
+    this.assign('title', 'Welecome to' + result.name);
     await this.display('index_index');
   }
   __after(){
