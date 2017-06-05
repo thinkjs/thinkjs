4cfacb3ad4ae74d0a972fe4ec430522b47615dd2
Shijie Lu <lushijie1218@126.com>
2017-06-05 19:29:49 +0800

    Update index.js

--- packages/think-model/src/index.js
+++ packages/think-model/src/index.js
@@ -5,41 +5,41 @@ const MysqlRelation = require('./mysql/relation');
 Mysql.Relation = MysqlRelation;
 module.exports = app => {
 
-  const getClass = function(type, name, m){
+  const getClass = function (type, name, m) {
     let mcls = app[type];
     let cls = null;
-    if(app.modules.length){
-      if(mcls[m]){
+    if (app.modules.length) {
+      if (mcls[m]) {
         cls = mcls[m][name];
       }
-      if(!cls && m !== 'common' && mcls.common){
+      if (!cls && m !== 'common' && mcls.common) {
         cls = mcls.common[name];
       }
-    }else{
+    } else {
       cls = mcls[name];
     }
     return cls;
-  }
+  };
 
-  const getModels = function(m){
-    if(app.modules.length){
+  const getModels = function (m) {
+    if (app.modules.length) {
       return helper.extend({}, app.models.common, app.models[m]);
-    }else{
+    } else {
       return app.models;
     }
-  }
+  };
 
-  const model = function(name, config, m = 'common'){
+  const model = function (name, config, m = 'common') {
     const cls = getClass('models', name, m);
     let commonConfig = helper.parseAdapterConfig(this.config('model'));
     config = Object.assign({}, commonConfig, config);
     //add models in config, it's need in model when get relation model instance
     config.models = getModels(m) || {};
-    if(cls){
+    if (cls) {
       return new cls(name, config);
     }
     return new Mysql(name, config);
-  }
+  };
 
   return {
     think: {
@@ -47,17 +47,17 @@ module.exports = app => {
       model
     },
     controller: {
-      model: (name, config, m = this.module) => {
+      model: function(name, config, m = this.module) {
         return this.ctx.model(name, config, m);
       }
     },
     context: {
-      model: (name, config, m = this.module) => {
+      model: function(name, config, m = this.module) {
         config = helper.parseAdapterConfig(this.config('model'), config);
         //add adapter cache config
         config.cache = helper.parseAdapterConfig(this.config('cache'), config.cache);
-        return model(name, config, m);
+        return model.bind(this)(name, config, m);
       }
     }
   };
-}
+};
