b60785a797b320a1250a7397d0cc03e80e9864a8
huangxiaolu <lu.huang90@gmail.com>
2017-07-12 21:27:57 +0800

    update eslint

--- packages/think-session/index.js
+++ packages/think-session/index.js
@@ -2,13 +2,13 @@ const Session = require('./lib/session.js');
 
 module.exports = {
   context: {
-    session: function(name, value, options){
+    session: function(name, value, options) {
       const instance = new Session(this, options);
       return instance.run(name, value);
     }
   },
   controller: {
-    session: function(name, value, options){
+    session: function(name, value, options) {
       const instance = new Session(this.ctx, options);
       return instance.run(name, value);
     }
