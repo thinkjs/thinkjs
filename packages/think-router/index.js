d12adc084212e4df407eb94518c8f9695a02a2ed
welefen <welefen@gmail.com>
2017-07-13 16:26:11 +0800

    Fix eslint errors

--- packages/think-router/index.js
+++ packages/think-router/index.js
@@ -1,32 +1,31 @@
 const Router = require('./router.js');
 const helper = require('think-helper');
 const pathToRegexp = require('path-to-regexp');
-const debug = require('debug')('think-router');
 
 /**
  * default options
  */
 const defaultOptions = {
-  defaultModule: 'home', //default module name, is enable in multi module mode
-  defaultController: 'index', //default controller name
-  defaultAction: 'index', //default action name
+  defaultModule: 'home', // default module name, is enable in multi module mode
+  defaultController: 'index', // default controller name
+  defaultAction: 'index', // default action name
   prefix: [], // url prefix
   suffix: ['.html'], // url suffix
   enableDefaultRouter: true,
   subdomainOffset: 2,
-  subdomain: {}, //subdomain
-  denyModules: [] //deny module, enable in multi module mode
-}
+  subdomain: {}, // subdomain
+  denyModules: [] // deny module, enable in multi module mode
+};
 
 /**
  * format routers
  * @param {Array|Object} routers
  */
 const formatRouters = routers => {
-  if(helper.isArray(routers)){
+  if (helper.isArray(routers)) {
     return routers.map(item => {
-      let query = [];
-      let match = pathToRegexp(item[0], query);
+      const query = [];
+      const match = pathToRegexp(item[0], query);
       return {
         match,
         path: item[1],
@@ -36,40 +35,39 @@ const formatRouters = routers => {
       };
     });
   }
-  for(let m in routers){
-    if(routers[m].match){
+  for (const m in routers) {
+    if (routers[m].match) {
       routers[m].match = pathToRegexp(routers[m].match);
     }
     routers[m].rules = formatRouters(routers[m].rules);
   }
   return routers;
-}
+};
 
 /**
  * parse router
  */
-module.exports = function parseRouter(options, app){
+module.exports = function parseRouter(options, app) {
   options = Object.assign(defaultOptions, options);
-  //set subdomain offset
-  if(options.subdomainOffset){
+  // set subdomain offset
+  if (options.subdomainOffset) {
     app.subdomainOffset = options.subdomainOffset;
   }
-  //change subdomain array to object
-  //subdomain: ['admin', 'user'] => {admin: 'admin', user: 'ussr'}
-  if(helper.isArray(options.subdomain)){
-    let subdomain = {};
+  // change subdomain array to object
+  // subdomain: ['admin', 'user'] => {admin: 'admin', user: 'ussr'}
+  if (helper.isArray(options.subdomain)) {
+    const subdomain = {};
     options.subdomain.forEach(item => {
       subdomain[item] = item;
     });
     options.subdomain = subdomain;
   }
-  //format routers when app ready
+  // format routers when app ready
   app.once('appReady', () => {
-    debug('router listen appReady event');
     app.routers = formatRouters(app.routers);
   });
-  return (ctx, next) => {
-    let instance = new Router(ctx, next, options, app);
+  return function router(ctx, next) {
+    const instance = new Router(ctx, next, options, app);
     return instance.run();
-  }
+  };
 };
--- packages/think-router/router.js
+++ packages/think-router/router.js
@@ -25,7 +25,7 @@ class Router {
    * @param {Object} options middleware options
    * @param {Object} app  think.app
    */
-  constructor(ctx, next, options, app){
+  constructor(ctx, next, options, app) {
     this.ctx = ctx;
     this.next = next;
     this.options = options;
@@ -39,44 +39,44 @@ class Router {
   /**
    * get pathname, remove prefix & suffix
    */
-  getPathname(){
+  getPathname() {
     let pathname = this.ctx.path;
     const prefix = this.options.prefix;
-    //remove prefix in pathname
-    if(prefix && prefix.length){
+    // remove prefix in pathname
+    if (prefix && prefix.length) {
       prefix.some(item => {
-        if(helper.isString(item) && pathname.indexOf(item) === 0){
+        if (helper.isString(item) && pathname.indexOf(item) === 0) {
           pathname = pathname.slice(item.length);
           return true;
         }
-        if(helper.isRegExp(item) && item.test(pathname)){
+        if (helper.isRegExp(item) && item.test(pathname)) {
           pathname = pathname.replace(item, '');
           return true;
         }
       });
     }
-    //remove suffix in pathname
+    // remove suffix in pathname
     const suffix = this.options.suffix;
-    if(suffix && suffix.length){
+    if (suffix && suffix.length) {
       suffix.some(item => {
-        if(helper.isString(item) && pathname.endsWith(item)){
+        if (helper.isString(item) && pathname.endsWith(item)) {
           pathname = pathname.slice(0, pathname.length - item.length);
           return true;
         }
-        if(helper.isRegExp(item) && item.test(pathname)){
+        if (helper.isRegExp(item) && item.test(pathname)) {
           pathname = pathname.replace(item, '');
           return true;
         }
       });
     }
-    //deal subdomain
-    let subdomain = this.options.subdomain;
-    if(!helper.isEmpty(subdomain)){
-      let subdomainStr = this.ctx.subdomains.join(',');
-      if(subdomainStr && subdomain[subdomainStr]){
-        if(pathname[0] === '/'){
+    // deal subdomain
+    const subdomain = this.options.subdomain;
+    if (!helper.isEmpty(subdomain)) {
+      const subdomainStr = this.ctx.subdomains.join(',');
+      if (subdomainStr && subdomain[subdomainStr]) {
+        if (pathname[0] === '/') {
           pathname = '/' + subdomain[subdomainStr] + pathname;
-        }else{
+        } else {
           pathname = subdomain[subdomainStr] + '/' + pathname;
         }
       }
@@ -87,16 +87,16 @@ class Router {
   /**
    * get router rules
    */
-  getRules(){
-    let rules = this.rules;
-    if(this.modules.length === 0) {
+  getRules() {
+    const rules = this.rules;
+    if (this.modules.length === 0) {
       return this.rules;
-    }else if(this.modules.length && helper.isObject(rules)){
-      for(let m in rules){
-        let match = rules[m].match;
-        if(match){
+    } else if (this.modules.length && helper.isObject(rules)) {
+      for (const m in rules) {
+        const match = rules[m].match;
+        if (match) {
           assert(helper.isRegExp(match), 'router.match must be a RegExp');
-          if(match.test(this.pathname)){
+          if (match.test(this.pathname)) {
             this.ctx.module = m;
             return rules[m].rules || [];
           }
@@ -109,30 +109,29 @@ class Router {
   /**
    * detect rule match
    */
-  getMatchedRule(rules){
+  getMatchedRule(rules) {
     let rule;
-    let specialMethods = ['REDIRECT', 'REST'];
+    const specialMethods = ['REDIRECT', 'REST'];
     rules.some(item => {
-      let itemMethod = item.method;
-      if(itemMethod && specialMethods.indexOf(itemMethod) === -1){
-        //check method matched
-        if(itemMethod.indexOf(this.ctxMethod) === -1) return;
+      const itemMethod = item.method;
+      if (itemMethod && specialMethods.indexOf(itemMethod) === -1) {
+        // check method matched
+        if (itemMethod.indexOf(this.ctxMethod) === -1) return;
       }
       assert(helper.isRegExp(item.match), 'router.match must be a RegExp');
-      let match = item.match.exec(this.pathname);
-      if(!match) return;
+      const match = item.match.exec(this.pathname);
+      if (!match) return;
       assert(helper.isArray(item.query), 'router.query must be an array');
-      let query = {};
+      const query = {};
       let pathname = item.path;
 
       item.query.forEach((queryItem, index) => {
-        if(/^\d+$/.test(queryItem.name)){
-          let index = parseInt(queryItem.name) + 1;
+        if (/^\d+$/.test(queryItem.name)) {
+          const index = parseInt(queryItem.name) + 1;
           pathname = pathname.replace(`:${index}`, match[index] || '');
-        }else{
+        } else {
           query[queryItem.name] = match[index + 1];
         }
-
       });
       rule = Object.assign({}, item, {query, path: pathname});
       return true;
@@ -142,27 +141,27 @@ class Router {
   /**
    * parser item rule
    */
-  parseRule(rule){
-    let ruleMethod = rule.method;
+  parseRule(rule) {
+    const ruleMethod = rule.method;
     // redirect url
-    if(ruleMethod === 'REDIRECT'){
-      if(rule.options && rule.options.statusCode){
+    if (ruleMethod === 'REDIRECT') {
+      if (rule.options && rule.options.statusCode) {
         this.ctx.status = rule.options.statusCode;
       }
       return this.ctx.redirect(rule.path);
     }
-    //remove /
+    // remove /
     let pathname = rule.path.replace(/^\/|\/$/g, '').replace(/\/{2,}/g, '/');
     let query = rule.query || {};
-    let queryPos = pathname.indexOf('?');
-    //parse query in path
-    if(queryPos > -1){
+    const queryPos = pathname.indexOf('?');
+    // parse query in path
+    if (queryPos > -1) {
       query = Object.assign(query, querystring.parse(pathname.slice(queryPos + 1)));
       pathname = pathname.slice(0, queryPos);
     }
-    //remove when query value is undefined or empty string
-    for(let name in query){
-      if(query[name] === undefined || query[name].trim() === ''){
+    // remove when query value is undefined or empty string
+    for (const name in query) {
+      if (query[name] === undefined || query[name].trim() === '') {
         delete query[name];
       }
     }
@@ -170,20 +169,20 @@ class Router {
     let m = ''; // module
     // multi module application, parse module first
     let controllers = this.controllers;
-    if(this.modules.length){
-      let pos = pathname.indexOf('/');
+    if (this.modules.length) {
+      const pos = pathname.indexOf('/');
       m = pos === -1 ? pathname : pathname.slice(0, pos);
-      if(this.modules.indexOf(m) > -1 && m !== 'common' && this.options.denyModules.indexOf(m) === -1){
+      if (this.modules.indexOf(m) > -1 && m !== 'common' && this.options.denyModules.indexOf(m) === -1) {
         pathname = pos === -1 ? '' : pathname.slice(pos + 1);
-      }else{
+      } else {
         m = this.options.defaultModule;
       }
       controllers = controllers[m] || {};
     }
     let controller = '';
-    for(let name in controllers){
-      if(name.indexOf('/') === -1) break;
-      if(name === pathname || pathname.indexOf(`${name}/`) === 0){
+    for (const name in controllers) {
+      if (name.indexOf('/') === -1) break;
+      if (name === pathname || pathname.indexOf(`${name}/`) === 0) {
         controller = name;
         pathname = pathname.slice(name.length + 1);
         break;
@@ -191,16 +190,16 @@ class Router {
     }
     let action = '';
     pathname = pathname.split('/');
-    if(controller){
-      action = ruleMethod === 'REST' ? this.ctxMethod.toLowerCase()  : pathname[0];
-    }else{
+    if (controller) {
+      action = ruleMethod === 'REST' ? this.ctxMethod.toLowerCase() : pathname[0];
+    } else {
       controller = pathname[0];
       action = ruleMethod === 'REST' ? this.ctxMethod.toLowerCase() : pathname[1];
     }
     this.ctx.module = m;
     this.ctx.controller = controller || this.options.defaultController;
     this.ctx.action = action || this.options.defaultAction;
-    //add query to context
+    // add query to context
     this.ctx.param(query);
     debug(`RouterParser: path=${this.ctx.path}, module=${this.ctx.module}, controller=${this.ctx.controller}, action=${this.ctx.action}, query=${JSON.stringify(query)}`);
     return this.next();
@@ -208,14 +207,14 @@ class Router {
   /**
    * parse router
    */
-  run(){
+  run() {
     const rules = this.getRules();
     const matchedRule = this.getMatchedRule(rules);
-    if(matchedRule){
+    if (matchedRule) {
       debug(`matchedRule: ${JSON.stringify(matchedRule)}`);
       return this.parseRule(matchedRule);
     }
-    if(this.options.enableDefaultRouter){
+    if (this.options.enableDefaultRouter) {
       return this.parseRule({path: this.pathname});
     }
     return this.next();
