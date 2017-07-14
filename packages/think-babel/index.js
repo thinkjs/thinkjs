4b6abb98f2f68b280542be158c78bf160d304252
lushijie <lushijie1218@126.com>
2017-07-14 18:05:18 +0800

    eslint-think

--- packages/think-babel/index.js
+++ packages/think-babel/index.js
@@ -8,12 +8,13 @@ const path = require('path');
  * @param  {Object} options
  * @return {Boolean}
  */
-function babelTranspile(config){
-  let {srcPath, outPath, file, options, ext = '.js'} = config;
-  let filePath = path.join(srcPath, file);
-  let pPath = path.dirname(path.join(outPath, file));
-  let content = fs.readFileSync(filePath, 'utf8');
-  let relativePath = path.relative(pPath, path.join(srcPath, file));
+function babelTranspile(config) {
+  const {srcPath, outPath, file, ext = '.js'} = config;
+  let {options} = config;
+  const filePath = path.join(srcPath, file);
+  const pPath = path.dirname(path.join(outPath, file));
+  const content = fs.readFileSync(filePath, 'utf8');
+  const relativePath = path.relative(pPath, path.join(srcPath, file));
 
   // babel options
   options = Object.assign({
@@ -26,23 +27,23 @@ function babelTranspile(config){
   let data;
   try {
     data = babelCore.transform(content, options);
-  }catch(e) {
+  } catch (e) {
     return e;
   }
 
   // write es5 code file
-  let outFile = path.join(outPath, file).replace(/\.\w+$/, ext);
+  const outFile = path.join(outPath, file).replace(/\.\w+$/, ext);
   helper.mkdir(path.dirname(outFile));
-  let basename = path.basename(file).replace(/\.\w+$/, ext);
-  let prefix = '//# sourceMappingURL=';
-  if(data.code.indexOf(prefix) === -1 && options.sourceMaps){
+  const basename = path.basename(file).replace(/\.\w+$/, ext);
+  const prefix = '//# sourceMappingURL=';
+  if (data.code.indexOf(prefix) === -1 && options.sourceMaps) {
     data.code = data.code + '\n' + prefix + basename + '.map';
   }
   fs.writeFileSync(outFile, data.code);
 
   // write souremap file
-  if(options.sourceMaps) {
-    let sourceMap = data.map;
+  if (options.sourceMaps) {
+    const sourceMap = data.map;
     sourceMap.file = sourceMap.sources[0];
     fs.writeFileSync(outFile + '.map', JSON.stringify(sourceMap, undefined, 4));
   }
