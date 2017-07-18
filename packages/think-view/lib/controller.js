1e5853b7ea00d7868b550bb3293f53f8cc3dcd16
zhengqingxin <362965772@qq.com>
2017-07-18 12:30:48 +0800

    change event name

--- packages/think-view/lib/controller.js
+++ packages/think-view/lib/controller.js
@@ -17,7 +17,7 @@ module.exports = {
       instance.assign('controller', this); // controller
       instance.assign('config', this.config()); // config
       instance.assign('ctx', this.ctx); // context
-      this.ctx.app.emit('viewReady');
+      this.ctx.app.emit('viewInit');
       this[viewInstance] = instance;
     }
     return this[viewInstance];
