41d567de37c6118ece80412f54caf2c63c4b45bd
welefen <welefen@gmail.com>
2017-07-21 19:10:57 +0800

    update

--- packages/think-gc/index.js
+++ packages/think-gc/index.js
@@ -2,34 +2,34 @@ const assert = require('assert');
 const helper = require('think-helper');
 const debug = require('debug')('think-gc');
 
-//min interval, 1 hour
+// min interval, 1 hour
 const MIN_STEP = 3600 * 1000;
 let intervalTimes = 0;
-let gcTypes = {};
+const gcTypes = {};
 let timerStart = false;
 
-function gc(instance, interval = MIN_STEP, MIN_INTERVAL = MIN_STEP){
+function gc(instance, interval = MIN_STEP, MIN_INTERVAL = MIN_STEP) {
   assert(instance && helper.isFunction(instance.gc), 'instance.gc must be a function');
   assert(instance && helper.isString(instance.gcType), 'instance.gcType must be a string');
-  if(gcTypes[instance.gcType]) return;
+  if (gcTypes[instance.gcType]) return;
 
-  gcTypes[instance.gcType] = function(){
-    if(helper.isFunction(interval)){
-      if(!interval()) return;
-    }else{
+  gcTypes[instance.gcType] = function() {
+    if (helper.isFunction(interval)) {
+      if (!interval()) return;
+    } else {
       interval = helper.ms(interval);
-      let num = Math.floor(interval / MIN_INTERVAL);
-      if(intervalTimes % num !== 0) return;
+      const num = Math.floor(interval / MIN_INTERVAL);
+      if (intervalTimes % num !== 0) return;
     }
     debug(`run gc, type: ${instance.gcType}`);
     instance.gc();
-  }
+  };
 
-  if(!timerStart){
+  if (!timerStart) {
     timerStart = true;
     const timer = setInterval(() => {
       intervalTimes++;
-      for(let type in gcTypes){
+      for (const type in gcTypes) {
         gcTypes[type]();
       }
     }, MIN_INTERVAL);
