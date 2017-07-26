5332f0958969b2d62c1fb8a2c567d91b310c4371
welefen <welefen@gmail.com>
2017-07-26 17:40:03 +0800

    Optimize performance

--- packages/think-model/index.js
+++ packages/think-model/index.js
@@ -3,21 +3,13 @@ const path = require('path');
 const Model = require('./lib/model.js');
 
 module.exports = app => {
-  const getModels = function(m) {
-    if (app.modules.length) {
-      return helper.extend({}, app.models.common, app.models[m]);
-    } else {
-      return app.models;
-    }
-  };
-
   const model = function(name, config, m = 'common') {
-    const models = getModels(m) || {};
+    const models = app.models.length ? (app.models[m] || {}) : app.models;
     const Cls = models[name] || Model;
     const modelName = path.basename(name);
     const instance = new Cls(modelName, config);
     // add models in config, it's need in model when get relation model instance
-    instance.models = getModels(m) || {};
+    instance.models = models;
     return instance;
   };
 
@@ -25,8 +17,10 @@ module.exports = app => {
     think: {
       Model: Model,
       model(name, config, m) {
-        config = helper.parseAdapterConfig(app.think.config('model', undefined, m), config);
-        config.cache = helper.parseAdapterConfig(app.think.config('cache', undefined, m), config.cache);
+        const modelConfig = app.think.config('model', undefined, m);
+        const cacheConfig = app.think.config('cache', undefined, m);
+        config = helper.parseAdapterConfig(modelConfig, config);
+        config.cache = helper.parseAdapterConfig(cacheConfig, config.cache);
         return model(name, config, m);
       }
     },
