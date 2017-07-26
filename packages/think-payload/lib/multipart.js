0c11cec500bb4495494b914c3088c4948f6e2f3c
welefen <welefen@gmail.com>
2017-07-26 14:02:52 +0800

    Optimize files upload

--- packages/think-payload/lib/multipart.js
+++ packages/think-payload/lib/multipart.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const helper = require('think-helper');
 const formidable = require('formidable');
+const onFinish = require('on-finished');
+
 const fsUnlink = helper.promisify(fs.unlink, fs);
 const fsAccess = helper.promisify(fs.access, fs);
 
@@ -8,18 +10,21 @@ module.exports = (ctx, opts = {}) => {
   const req = ctx.req;
   const form = new formidable.IncomingForm(opts);
 
-  ctx.res.once('finish', () => {
-    const files = ctx.request.body.file;
-    const unlinks = Object.keys(files).map(key => {
-      return fsAccess(files[key].path).then(() => fsUnlink(files[key].path)).catch(() => {});
+  let uploadFiles = null;
+
+  onFinish(ctx.res, () => {
+    if (!uploadFiles) return;
+    Object.keys(uploadFiles).forEach(key => {
+      const filepath = uploadFiles[key].path;
+      fsAccess(filepath).then(() => fsUnlink(filepath)).catch(() => {});
     });
-    Promise.all(unlinks);
   });
 
   return new Promise((resolve, reject) => {
     form.parse(req, function(err, fields, files) {
       if (err) return reject(err);
 
+      uploadFiles = files;
       resolve({
         post: fields,
         file: files
