9c516ced01b2a2139b23330bdfb37b05a4fa5f04
welefen <welefen@gmail.com>
2017-07-28 17:34:48 +0800

    Fix eslint errors

--- packages/think-controller/index.js
+++ packages/think-controller/index.js
@@ -1,56 +1,55 @@
 const assert = require('assert');
 const helper = require('think-helper');
 
-function invokeController(options, app){
-  
+function invokeController(options, app) {
   return (ctx, next) => {
     const isMultiModule = app.modules.length;
     let controllers = app.controllers;
 
-    if(isMultiModule){
+    if (isMultiModule) {
       assert(ctx.module, 'ctx.module required in multi module');
     }
     assert(ctx.controller, 'ctx.controller required');
     assert(ctx.action, 'ctx.action required');
     // error avoiding
-    if(controllers && isMultiModule){
+    if (controllers && isMultiModule) {
       controllers = controllers[ctx.module];
     }
-    //controllers empty
-    if(helper.isEmpty(controllers)){
+    // controllers empty
+    if (helper.isEmpty(controllers)) {
       return next();
     }
-    const controller = controllers[ctx.controller];
+    const Controller = controllers[ctx.controller];
     // controller not exist
-    if(helper.isEmpty(controller)){
+    if (helper.isEmpty(Controller)) {
       return next();
     }
-    const instance = new controller(ctx);
+    const instance = new Controller(ctx);
     let promise = Promise.resolve();
-    if(instance.__before){
+    if (instance.__before) {
       promise = Promise.resolve(instance.__before());
     }
     // if return false, it will be prevent next process
     return promise.then(data => {
-      if(data === false) return false;
+      if (data === false) return false;
       let method = `${ctx.action}Action`;
-      if(!instance[method]){
+      if (!instance[method]) {
         method = '__call';
       }
-      if(instance[method]){
+      if (instance[method]) {
         return instance[method]();
       }
     }).then(data => {
-      if(data === false) return false;
-      if(instance.__after){
+      if (data === false) return false;
+      if (instance.__after) {
         return instance.__after();
       }
     }).then(data => {
-      if(data !== false){
+      if (data !== false) {
         return next();
       }
     });
-  }
+  };
 }
 
 module.exports = invokeController;
