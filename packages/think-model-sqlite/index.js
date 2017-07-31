2ca0bce1b9725dd86ccc28ec7c2f9fb09c44cf63
welefen <welefen@gmail.com>
2017-07-31 19:03:52 +0800

    Change class name

--- packages/think-model-sqlite/index.js
+++ packages/think-model-sqlite/index.js
@@ -5,7 +5,7 @@ const Schema = require('./lib/schema.js');
 const QUERY = Symbol('think-model-query');
 const SCHEMA = Symbol('think-model-schema');
 
-module.exports = class Mysql extends Abstract {
+module.exports = class SQLite extends Abstract {
   /**
    * get query instance
    */
