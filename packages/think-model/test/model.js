1436e3d66875200c191e74543143531aa5cfc03a
lizheming <secretlzm007@gmail.com>
2017-08-01 20:55:14 +0800

    add model test

--- packages/think-model/test/model.js
+++ packages/think-model/test/model.js
@@ -209,10 +209,231 @@ test('model set order alias having group lock auto explan distinct', t => {
   t.deepEqual(model.distinct({field: 'user'}).options.distinct, {field: 'user'});
 });
 
-test('model after find', t => {
-  t.pass();
+test('model parse options', async t => {
+  t.plan(4);
+  const adapter = function() {};
+  adapter.prototype.getReverseFields = function() {
+    return ['id', 'name', 'title'];
+  };
+
+  const model = new Model('post', {handle: adapter});
+
+  t.deepEqual(
+    await model.parseOptions(3),
+    {table: 'post', pk: 'id', tablePrefix: '', where: {id: '3'}}
+  );
+  t.deepEqual(
+    await model.parseOptions('3,4'),
+    {table: 'post', pk: 'id', tablePrefix: '', where: {id: {IN: '3,4'}}}
+  );
+  t.deepEqual(
+    await model.parseOptions({table: 'user', where: {id: 3}}),
+    {table: 'user', pk: 'id', tablePrefix: '', where: {id: 3}}
+  );
+
+  model.config.prefix = 'fk_';
+  model.options = {
+    field: 'title',
+    fieldReverse: true
+  };
+  t.deepEqual(
+    await model.parseOptions(),
+    {table: 'fk_post', pk: 'id', tablePrefix: 'fk_', field: ['id', 'name', 'title']}
+  );
 });
 
-test('model after select', t => {
-  t.pass();
+test('model add data', async t => {
+  t.plan(2);
+
+  const adapter = class {
+    add() {
+      return 3;
+    }
+    parseData(data) {
+      return data;
+    }
+  };
+
+  const model = new Model('post', {handle: adapter});
+  try {
+    await model.add();
+    t.fail();
+  } catch (e) {
+    t.pass();
+  };
+
+  const result = await model.add({title: 'hello', content: 'hello world'});
+  t.is(result, 3);
+});
+
+test('model then add data', async t => {
+  t.plan(3);
+
+  const model = new Model('post', {handle: class {
+    add(data) {
+      t.deepEqual(data, {title: 'hello2', content: 'hello world again!'});
+      return 2;
+    }
+    parseData(data) {
+      return data;
+    }
+  }});
+  model.find = function() {
+    if (this.options.where.id === 1) {
+      return {id: 1, title: 'hello', content: 'hello world'};
+    }
+    return {};
+  };
+
+  let result = await model.thenAdd({title: 'hello2', content: 'hello world again!'}, {id: 1});
+  t.deepEqual(result, {type: 'exist', id: 1});
+
+  result = await model.thenAdd({title: 'hello2', content: 'hello world again!'}, {id: 2});
+  t.deepEqual(result, {type: 'add', id: 2});
+});
+
+test('model then update data exist', async t => {
+  t.plan(2);
+
+  const model = new Model('post', {handle: new Function()});
+  model.find = function() {
+    return {id: 1, title: 'test', content: 'world'};
+  };
+  model.update = function(data) {
+    t.is(this.options.where.id, 1);
+    return {id: 1, title: 'hello', content: 'world'};
+  };
+  const result = await model.thenUpdate({title: 'hello'}, {id: 1});
+  t.deepEqual(result, 1);
+});
+
+test('model then update data not exist', async t => {
+  t.plan(2);
+
+  const model = new Model('post', {handle: new Function()});
+  model.find = function() {
+    t.is(this.options.where.id, 3);
+    return {};
+  };
+  model.add = function(data) {
+    return 4;
+  };
+  const result = await model.thenUpdate({title: 'hello', content: 'world'}, {id: 3});
+  t.is(result, 4);
+});
+
+test('model add many data error', async t => {
+  t.plan(2);
+
+  const model = new Model('post', {handle: class {
+    parseData(data) {
+      return data;
+    }
+    addMany() {
+      return [1, 2, 3, 4];
+    }
+  }});
+
+  try {
+    await model.addMany({title: 'hello'});
+  } catch (e) {
+    t.pass();
+  }
+
+  try {
+    await model.addMany([1, 2]);
+  } catch (e) {
+    t.pass();
+  }
+});
+
+test('model add many data', async t => {
+  t.plan(6);
+
+  const model = new Model('post', {handle: class {
+    parseData(data) {
+      return data;
+    }
+    addMany(data) {
+      t.true(data.every(d => d.user === 'lizheming'));
+      return [1, 2, 3, 4];
+    }
+  }});
+
+  const addData = [
+    {title: 'hello', content: 'world'},
+    {title: 'hello1', content: 'world1'},
+    {title: 'hello2', content: 'world2'},
+    {title: 'hello3', content: 'world3'}
+  ];
+  model.beforeAdd = function(data) {
+    data.user = 'lizheming';
+    return data;
+  };
+  model.afterAdd = function(data) {
+    t.true(data.hasOwnProperty('id'));
+  };
+  const result = await model.addMany(addData);
+  t.deepEqual(result, [1, 2, 3, 4]);
+});
+
+test('model delete data', async t => {
+  t.plan(3);
+  const model = new Model('post', {handle: class {
+    delete(options) {
+      return 1;
+    }
+  }});
+
+  const options = {id: 3};
+
+  model.beforeDelete = model.afterDelete = function(opt) {
+    t.is(opt.id, options.id);
+    return opt;
+  };
+
+  const result = await model.delete(options);
+  t.is(result, 1);
+});
+
+test('model update data', async t => {
+  t.plan(4);
+
+  const model = new Model('post', {handle: class {
+    parseData(data) {
+      return data;
+    }
+    update(data, options) {
+      t.deepEqual(options.where, {id: 3});
+      t.deepEqual(data, {title: 'hello'});
+      return 'update data';
+    }
+  }});
+
+  try {
+    await model.update({title: 'hello'});
+  } catch (e) {
+    t.pass();
+  }
+
+  const result = await model.update({title: 'hello', id: 3});
+  t.is(result, 'update data');
+});
+
+test('model updateMany data error', async t => {
+  const model = new Model('post', {handle: class {
+    parseData(data) {
+      return data;
+    }
+    update() {
+      return true;
+    }
+  }});
+
+  try {
+    await model.where({id: 3}).updateMany({title: 'hello'});
+    t.fail();
+  } catch (e) {
+    t.deepEqual(model.options, {});
+  }
 });
