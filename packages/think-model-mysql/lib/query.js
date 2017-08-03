2b2e07f32d76bfbccd44b6bb20c817fe4120c791
welefen <welefen@gmail.com>
2017-08-03 09:48:41 +0800

    Remove some methods, is defined in base class

--- packages/think-model-mysql/lib/query.js
+++ packages/think-model-mysql/lib/query.js
@@ -1,10 +1,8 @@
 const {Query} = require('think-model-abstract');
 const Mysql = require('think-mysql');
-const helper = require('think-helper');
 const Parser = require('./parser.js');
 
-const PARSER = Symbol('think-model-mysql-parser');
-const SOCKET = Symbol('think-model-mysql-socket');
+const PARSER = Symbol('think-model-parser');
 
 /**
  * mysql query
@@ -23,52 +21,17 @@ module.exports = class MysqlQuery extends Query {
    * @param {String|Object} sql 
    */
   socket(sql) {
-    if (sql && this.config.parser) {
-      const config = Object.assign({}, this.config, this.config.parser(sql));
-      return Mysql.getInstance(config);
-    }
-    if (this[SOCKET]) return this[SOCKET];
-    this[SOCKET] = Mysql.getInstance(this.config);
-    return this[SOCKET];
-  }
-  /**
-   * query sql
-   */
-  query(sqlOptions, connection) {
-    const sql = helper.isString(sqlOptions) ? sqlOptions : sqlOptions.sql;
-    this.lastSql = sql;
-    return this.socket(sql).query(sqlOptions, connection);
+    return super.socket(sql, Mysql);
   }
   /**
    * execute sql
    */
   execute(sqlOptions, connection) {
-    const sql = helper.isString(sqlOptions) ? sqlOptions : sqlOptions.sql;
-    this.lastSql = sql;
-    return this.socket(sql).execute(sqlOptions, connection).then(data => {
+    return super.execute(sqlOptions, connection).then(data => {
       if (data.insertId) {
         this.lastInsertId = data.insertId;
       }
       return data.affectedRows || 0;
     });
-  }
-
-  startTrans(connection) {
-    return this.socket().startTrans(connection).then(connection => {
-      this.connection = connection;
-      return connection;
-    });
-  }
-
-  commit(connection = this.connection) {
-    return this.socket().commit();
-  }
-
-  rollback(connection = this.connection) {
-    return this.socket().rollback(connection);
-  }
-
-  transaction(fn, connection) {
-    return this.socket().transaction(fn, connection);
   }
 };
