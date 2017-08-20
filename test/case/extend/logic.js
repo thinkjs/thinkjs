322dc7ee1db2f18dcd9b8cd2ec23b8a1f1f90f4d
lushijie <lushijie1218@126.com>
2017-08-20 19:30:22 +0800

    add scope for logic

--- test/case/extend/logic.js
+++ test/case/extend/logic.js
@@ -40,6 +40,30 @@ test.serial('validate pass', async t => {
   think.app.validators.rules = null;
 });
 
+
+test.serial('validate pass', async t => {
+  let rules = {
+    mockResult:false,
+    username: {
+      string: true,       // 字段类型为 String 类型
+      required: true,     // 字段必填
+      default: 'thinkjs', // 字段默认值为 'thinkjs'
+      trim: true,         // 字段需要trim处理
+      method: 'GET'       // 指定获取数据的方式
+    },
+    age: {
+      int: {min: 20, max: 60} // 20到60之间的整数
+    }
+  }
+  think.app.validators.rules = {
+    name:{string:true},
+    version:{string:true}
+  };
+  let flag = logic.validate(rules);
+  t.is(flag,true);
+  think.app.validators.rules = null;
+});
+
 test.serial('validate error', async t => {
   let rules = {
     mockResult:true,
@@ -100,6 +124,11 @@ test.serial('_after', async t => {
 
 test.serial('_after', async t => {
   logic = mock.reRequire('../../../lib/extend/logic');
+  logic.scope = {
+    id: {
+      required: true
+    }
+  }
   logic.rules = {
     mockResult:true,
     username: {
