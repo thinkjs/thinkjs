eddf42f2710e82fd1b0e6116bdaee8f2410a451f
lushijie <lushijie1218@126.com>
2017-08-27 09:23:44 +0800

    support freeze tableName

--- packages/think-sequelize/lib/model.js
+++ packages/think-sequelize/lib/model.js
@@ -2,11 +2,14 @@
 * @Author: lushijie
 * @Date:   2017-08-23 16:05:05
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-08-25 16:55:17
+* @Last Modified time: 2017-08-27 09:22:00
 */
+const sequelize = require('sequelize');
 const Socket = require('./socket.js');
 const {extendClassMethods} = require('./util.js');
+
 const models = {};
+const MODELS = Symbol('think-sequelize-models');
 
 class Sequelize {
   constructor(modelName, config, name) {
@@ -26,6 +29,8 @@ class Sequelize {
       };
     }
     schema.options = Object.assign({}, this.config.schema, schema.options);
+    this.schemaLast = schema;
+
     const model = sequelizeConn.define(this.tableName, schema.attributes, schema.options);
     const Class = class extends model {};
     extendClassMethods(Class, this);
@@ -43,8 +48,35 @@ class Sequelize {
    * get table name, with table prefix
    */
   get tableName() {
+    let schemaLast = this.schemaLast;
+    if(schemaLast.options.freezeTableName && schemaLast.options.tableName) return schemaLast.options.tableName;
     return this.tablePrefix + this.modelName;
   }
+
+  /**
+   * get all store models
+   */
+  get models() {
+    return this[MODELS] || {};
+  }
+  /**
+   * set models
+   */
+  set models(value) {
+    this[MODELS] = value;
+  }
+  /**
+   * get model instance
+   * @param {String} name
+   */
+  //sequelize(name) {
+    // const ModelClass = this.models[name] || Sequelize;
+    // const modelName = path.basename(name);
+    // const instance = new ModelClass(modelName, this.config, name);
+    // instance.models = this.models;
+    // return instance;
+  //}
 }
 
+// Sequelize.sequelize = sequelize;
 module.exports = Sequelize;
