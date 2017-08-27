1f90e68ed8093ba4d0fb20511d51eabaa2125bf2
lizheming <secretlzm007@gmail.com>
2017-08-27 18:11:06 +0800

    fix layouts param error

--- packages/think-logger/src/adapter/file.js
+++ packages/think-logger/src/adapter/file.js
@@ -6,13 +6,13 @@ module.exports = class FileLogger extends Base {
 
     const lConfig = Object.assign({}, config);
     // eslint-disable-next-line prefer-const
-    let {level, filename, maxLogSize, backups, absolute, layouts} = config;
+    let {level, filename, maxLogSize, backups, absolute, layout} = config;
     level = level ? level.toUpperCase() : 'ALL';
 
     // combine config for file appender, common config for log4js
     config = Object.assign({
       appenders: [
-        {type: 'file', level, filename, maxLogSize, backups, absolute, layouts}
+        {type: 'file', level, filename, maxLogSize, backups, absolute, layout}
       ]
     }, lConfig);
     // check cluster mode
