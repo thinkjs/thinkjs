0e547d1d191cf66cdaf74db6202e70298dd56f0e
welefen <welefen@gmail.com>
2017-09-05 19:12:20 +0800

    Remove agent code

--- packages/think-cluster/index.js
+++ packages/think-cluster/index.js
@@ -2,10 +2,7 @@ const util = require('./lib/util.js');
 const Messenger = require('./lib/messenger.js');
 
 exports.Worker = require('./lib/worker.js');
-exports.Agent = require('./lib/agent.js');
 exports.Master = require('./lib/master.js');
-exports.delegate = require('./lib/delegate.js');
 
 exports.messenger = new Messenger();
-exports.isAgent = util.isAgent;
 exports.isFirstWorker = util.isFirstWorker;
