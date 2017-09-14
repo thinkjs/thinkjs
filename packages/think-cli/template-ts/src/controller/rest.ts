160683aa4d90ab83bc44a1165f592a75cacf256b
SijieCai <sijie.cai@qq.com>
2017-09-14 10:17:52 +0800

    update rest base for ts

--- packages/think-cli/template-ts/src/controller/rest.ts
+++ packages/think-cli/template-ts/src/controller/rest.ts
@@ -1,24 +1,27 @@
 const path = require('path');
 const assert = require('assert');
+import * as Think from 'thinkjs';
 import { think } from 'thinkjs';
 
 export default class extends think.Controller {
-  constructor(ctx) {
+  resource: string;
+  id: string;
+  modelInstance: Think.Model;
+  constructor(ctx: Think.Context) {
     super(ctx);
     this.resource = this.getResource();
     this.id = this.getId();
+
     assert(think.isFunction(this.model), 'this.model must be a function');
     this.modelInstance = this.model(this.resource);
   }
-  __before() {}
+  __before() { }
   /**
    * get resource
    * @return {String} [resource name]
    */
   getResource() {
-    const filename = this.__filename || __filename;
-    const last = filename.lastIndexOf(path.sep);
-    return filename.substr(last + 1, filename.length - last - 4);
+    return this.ctx.controller;
   }
   getId() {
     const id = this.get('id');
@@ -34,8 +37,8 @@ export default class extends think.Controller {
   async getAction() {
     let data;
     if (this.id) {
-      const pk = await this.modelInstance.getPk();
-      data = await this.modelInstance.where({[pk]: this.id}).find();
+      const pk = this.modelInstance.pk;
+      data = await this.modelInstance.where({ [pk]: this.id }).find();
       return this.success(data);
     }
     data = await this.modelInstance.select();
@@ -46,14 +49,15 @@ export default class extends think.Controller {
    * @return {Promise} []
    */
   async postAction() {
-    const pk = await this.modelInstance.getPk();
+    const pk = this.modelInstance.pk;
     const data = this.post();
-    delete data[pk];
+    if(data[pk])
+      delete data[pk];
     if (think.isEmpty(data)) {
       return this.fail('data is empty');
     }
     const insertId = await this.modelInstance.add(data);
-    return this.success({id: insertId});
+    return this.success({ id: insertId });
   }
   /**
    * delete resource
@@ -63,9 +67,9 @@ export default class extends think.Controller {
     if (!this.id) {
       return this.fail('params error');
     }
-    const pk = await this.modelInstance.getPk();
-    const rows = await this.modelInstance.where({[pk]: this.id}).delete();
-    return this.success({affectedRows: rows});
+    const pk = this.modelInstance.pk;
+    const rows = await this.modelInstance.where({ [pk]: this.id }).delete();
+    return this.success({ affectedRows: rows });
   }
   /**
    * update resource
@@ -75,14 +79,14 @@ export default class extends think.Controller {
     if (!this.id) {
       return this.fail('params error');
     }
-    const pk = await this.modelInstance.getPk();
+    const pk = this.modelInstance.pk;
     const data = this.post();
     delete data[pk];
     if (think.isEmpty(data)) {
       return this.fail('data is empty');
     }
-    const rows = await this.modelInstance.where({[pk]: this.id}).update(data);
-    return this.success({affectedRows: rows});
+    const rows = await this.modelInstance.where({ [pk]: this.id }).update(data);
+    return this.success({ affectedRows: rows });
   }
   __call() {
 
