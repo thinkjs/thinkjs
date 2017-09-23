b9803995078632a546b2bfbaf9dd845e834791b2
lizheming <secretlzm007@gmail.com>
2017-09-23 15:23:19 +0800

    first blood :smile:

--- packages/think-router-rest/README.md
+++ packages/think-router-rest/README.md
@@ -1,2 +1,33 @@
 # think-router-rest
-Let think-router recognize REST router easily without custom router.
+
+Let think-router recognize REST router easily without custom router for ThinkJS 3.x.
+
+## Installation
+
+```sh
+npm install think-router-rest --save
+```
+
+## How To Use
+
+append this middleware in `src/config/middleware.js`:
+
+```
+const router = require('think-router');
+const routerREST = require('think-router-rest');
+
+module.exports = [
+  {handle: router, options: {}},
+  routerREST
+];
+```
+
+## Contributing
+
+Contributions welcome!
+
+## License
+
+[MIT](https://github.com/thinkjs/think-router-rest/blob/master/LICENSE)
+
+
---
+++ packages/think-router-rest/index.js
@@ -0,0 +1,25 @@
+const debug = require('debug')('think-router-rest');
+
+module.exports = options => {
+  const _REST = options.restConstName || '_REST';
+  const _method = options.methodConstName || '_method';
+
+  return (ctx, next) => {
+    const controller = ctx.app.controllers[ctx.controller];
+    if (!controller || !controller[_REST]) {
+      debug(`_REST: ${ctx.controller} is not rest controller`);
+      return next();
+    }
+
+    let actionName = ctx.method;
+    const methodName = controller[_method];
+    /* just allow custom method in post request */
+    debug(`_method: methd=${actionName}, methodName = ${methodName}`);
+    if (actionName === 'POST' && methodName && ctx.query[methodName]) {
+      actionName = ctx.query[methodName];
+    }
+    debug(`router-rest: controller=${ctx.controller}, action=${ctx.action}`);
+    ctx.action = actionName.toLowerCase();
+    return next();
+  };
+};
