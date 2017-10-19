25fae17b37dd7c98b4e2c728f424798cdad0c859
welefen <welefen@gmail.com>
2017-10-19 18:56:25 +0800

    Remove duplicate item

--- packages/think-cli/template-ts/development.js
+++ packages/think-cli/template-ts/development.js
@@ -14,8 +14,7 @@ const instance = new Application({
       noImplicitAny: true,
       removeComments: true,
       preserveConstEnums: true,
-      suppressImplicitAnyIndexErrors: true,
-      sourceMap: true
+      suppressImplicitAnyIndexErrors: true
     }
   }],
   notifier: notifier.notify.bind(notifier),
