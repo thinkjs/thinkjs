e787cf2bcccb7571b4c8ee12612a0879ddd8f42e
lichengyin <lichengyin@360.cn>
2018-09-13 16:42:49 +0800

    Support autoUpdateRate

--- packages/think-session/lib/session.js
+++ packages/think-session/lib/session.js
@@ -6,27 +6,27 @@ const SESSION = Symbol('think-context-session');
  * default session options
  */
 const defaultOptions = {
-  maxAge: 24 * 3600 * 1000, //1 day
-}
+  maxAge: 24 * 3600 * 1000 // 1 day
+};
 /**
  * default cookie options
  * https://github.com/pillarjs/cookies#cookiesset-name--value---options--
  */
 const defaultCookieOptions = {
   name: 'thinkjs',
-  autoUpdate: false, //auto update cookie when is set maxAge
-  path: '/',  //a string indicating the path of the cookie
+  autoUpdateRate: 0.5,
+  path: '/', // a string indicating the path of the cookie
   httpOnly: true,
   sameSite: false,
   signed: false,
   overwrite: false,
-  encrypt: false //encrypt cookie data
-  //maxAge: '',
-  //expires: '',
-  //domain: '',
-  //secure: false,
-  //keys: []
-}
+  encrypt: false // encrypt cookie data
+  // maxAge: '',
+  // expires: '',
+  // domain: '',
+  // secure: false,
+  // keys: []
+};
 /**
  * Session Manage
  */
@@ -36,43 +36,38 @@ class Session {
    * @param {Object} ctx context
    * @param {Object} options session options
    */
-  constructor(ctx, options){
+  constructor(ctx, options = {}) {
     this.ctx = ctx;
-    const sessionConfig =  helper.parseAdapterConfig(ctx.config('session'), options);
+    const sessionConfig = helper.parseAdapterConfig(ctx.config('session'), options);
     this.options = helper.extend({}, defaultOptions, sessionConfig);
+    // transform humanize time to ms
+    this.options.maxAge = helper.ms(this.options.maxAge);
     assert(helper.isFunction(this.options.handle), 'session.handle must be a function');
     this.cookieOptions = Object.assign({}, defaultCookieOptions, ctx.config('cookie'), this.options.cookie);
+    this.cookieOptions.maxAge = this.options.maxAge;
   }
   /**
    * get session instance
    */
-  getSessionInstance(){
-    if(!this.ctx[SESSION]){
-      const handle = this.options.handle;
+  getSessionInstance() {
+    if (!this.ctx[SESSION]) {
+      const Handle = this.options.handle;
       let instance;
-      //store session data on cookie
-      if(handle.onlyCookie){
-        instance = new handle(this.cookieOptions, this.ctx);
-      }else{
-        let name = this.cookieOptions.name;
+      // store session data on cookie
+      if (Handle.onlyCookie) {
+        instance = new Handle(this.cookieOptions, this.ctx);
+      } else {
+        const name = this.cookieOptions.name;
         let cookie = this.ctx.cookie(name, undefined, this.cookieOptions);
         let fresh = false;
-        if(!cookie){
+        if (!cookie) {
           cookie = helper.uuid();
           this.ctx.cookie(name, cookie, this.cookieOptions);
           fresh = true;
-        }else{
-          const cookieOptions = this.cookieOptions;
-          //auto update cookie when expires or maxAge is set
-          if(cookieOptions.autoUpdate && cookieOptions.maxAge){
-            this.ctx.cookie(name, cookie, this.cookieOptions);
-          }
         }
-        //transform humanize time to ms
-        this.options.maxAge = helper.ms(this.options.maxAge);
         this.options.cookie = cookie;
         this.options.fresh = fresh;
-        instance = new handle(this.options, this.ctx);
+        instance = new Handle(this.options, this.ctx, this.cookieOptions);
       }
       this.ctx[SESSION] = instance;
     }
@@ -83,18 +78,18 @@ class Session {
    * @param {String} name 
    * @param {Mixed} value 
    */
-  run(name, value){
+  run(name, value) {
     const instance = this.getSessionInstance();
-    //delete session
-    if(name === null){
+    // delete session
+    if (name === null) {
       return Promise.resolve(instance.delete());
     }
-    //get session
-    if(value === undefined){
+    // get session
+    if (value === undefined) {
       return Promise.resolve(instance.get(name));
     }
     assert(helper.isString(name), 'session.name must be a string');
-    //set session
+    // set session
     return Promise.resolve(instance.set(name, value));
   }
 }
