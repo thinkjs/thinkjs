04b51b28e00d3e4e1cd332a50b24391f91c887ac
lizheming <secretlzm007@gmail.com>
2018-09-16 16:05:04 +0800

    add insert update feature on duplicate key

--- packages/think-model-mysql2/lib/parser.js
+++ packages/think-model-mysql2/lib/parser.js
@@ -1,5 +1,5 @@
 const helper = require('think-helper');
-const {Parser} = require('think-model-abstract');
+const { Parser } = require('think-model-abstract');
 
 module.exports = class MysqlParser extends Parser {
   /**
@@ -41,5 +41,40 @@ module.exports = class MysqlParser extends Parser {
           return '\\' + s;
       }
     });
+  }
+
+  /**
+   * get insert sql
+   * @param {Object} options
+   */
+  buildInsertSql(options, replace) {
+    const isUpdate = helper.isObject(options.update) || helper.isArray(options.update);
+    if (replace || !isUpdate) {
+      return super.buildInsertSql(options, replace);
+    }
+
+    const table = this.parseTable(options.table);
+    const values = options.values[0] !== '(' ? `(${options.values})` : options.values;
+
+    let sets = [];
+    if (helper.isArray(options.update)) {
+      sets = options.update.map(field => {
+        field = this.parseKey(field);
+        return field + '=' + `VALUES(${field})`;
+      });
+    } else {
+      for (const key in options.update) {
+        const value = this.parseValue(options.update[key]);
+        if (helper.isString(value) || helper.isNumber(value) || helper.isBoolean(value)) {
+          sets.push(this.parseKey(key) + '=' + value);
+        }
+      }
+    }
+
+    const duplicateUpdate = sets.length ? ' ON DUPLICATE KEY UPDATE ' + sets.join(',') : '';
+    const lock = this.parseLock(options.lock);
+    const comment = this.parseComment(options.comment);
+
+    return `INSERT INTO ${table} (${options.fields}) VALUES ${values}${duplicateUpdate}${lock}${comment}`;
   }
 };
