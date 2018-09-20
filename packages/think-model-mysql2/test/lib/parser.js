619a5d5bf3183d82046b1a4879a4c30c7f64ab9b
lizheming <secretlzm007@gmail.com>
2018-09-20 17:23:17 +0800

    complement parser test

--- packages/think-model-mysql2/test/lib/parser.js
+++ packages/think-model-mysql2/test/lib/parser.js
@@ -56,7 +56,147 @@ ava.test('escapeString, \\"', t => {
 });
 
 ava.test('parseKey is function', t => {
+  const cases = [
+    ['key', '`key`'],
+    ['    ', ''],
+    [' 3 ', '3'],
+    ['3,', '3,'],
+    ['3\'', '3\''],
+    ['3"', '3"'],
+    ['3*', '3*'],
+    ['3(', '3('],
+    ['3)', '3)'],
+    ['3 3', '3 3'],
+    ['`3`', '`3`'],
+    ['3.3', '3.3'],
+    ['li.zheming', 'li.zheming']
+  ];
+  t.plan(cases.length);
   const instance = new Base();
-  const key = instance.parseKey('key');
-  t.is(key, '`key`');
+  cases.forEach(([param, expect]) => t.is(instance.parseKey(param), expect));
+});
+
+ava.test('buildInsertSql with super', t => {
+  const instance = new Base();
+  instance.__proto__.__proto__.buildInsertSql = function() {
+    t.pass();
+    return 'lizheming';
+  };
+
+  const params = [
+    [{}, null],
+    [{}, true]
+  ];
+
+  t.plan(params.length * 2);
+  params.forEach(param =>
+    t.is(instance.buildInsertSql.apply(instance, param), 'lizheming')
+  );
+});
+
+ava.test('buildInsertSql with array update', t => {
+  const instance = new Base();
+  const options = {
+    table: 'user',
+    fields: 'id, name, email',
+    values: '1, "lizheming", "i@imnerd.org"',
+    update: ['id', 'title'],
+    lock: 'lock',
+    comment: 'comment'
+  };
+
+  instance.parseTable = function(table) {
+    t.is(table, options.table);
+    return table;
+  };
+
+  instance.parseKey = function(key) {
+    return '$' + key + '$';
+  };
+
+  instance.parseLock = function(lock) {
+    t.is(lock, 'lock');
+    return '';
+  };
+
+  instance.parseComment = function(comment) {
+    t.is(comment, 'comment');
+    return '';
+  };
+
+  t.is(
+    instance.buildInsertSql(options),
+    'INSERT INTO user (id, name, email) VALUES (1, "lizheming", "i@imnerd.org") ON DUPLICATE KEY UPDATE $id$=VALUES($id$),$title$=VALUES($title$)'
+  );
+});
+
+ava.test('buildInsertSql with object update', t => {
+  const instance = new Base();
+  const options = {
+    table: 'user',
+    fields: 'id, name, email',
+    values: '(1, "lizheming", "i@imnerd.org")',
+    update: {
+      name: 'lizheming111',
+      title: { a: 1 }
+    },
+    lock: 'lock',
+    comment: 'comment'
+  };
+
+  instance.parseTable = function(table) {
+    t.is(table, options.table);
+    return table;
+  };
+
+  instance.parseKey = function(key) {
+    return '$' + key + '$';
+  };
+
+  instance.parseValue = function(value) {
+    if (typeof value === 'string') {
+      t.is(value, 'lizheming111');
+      return '`' + value + '`';
+    } else {
+      t.deepEqual(value, { a: 1 });
+      return value;
+    }
+  };
+
+  instance.parseLock = function(lock) {
+    t.is(lock, 'lock');
+    return ' lock2';
+  };
+
+  instance.parseComment = function(comment) {
+    t.is(comment, 'comment');
+    return ' comment2';
+  };
+
+  t.is(
+    instance.buildInsertSql(options),
+    'INSERT INTO user (id, name, email) VALUES (1, "lizheming", "i@imnerd.org") ON DUPLICATE KEY UPDATE $name$=`lizheming111` lock2 comment2'
+  );
+});
+
+ava.test('buildInsertSql with empty update', t => {
+  const instance = new Base();
+  const options = {
+    table: 'user',
+    fields: 'id, name, email',
+    values: '(1, "lizheming", "i@imnerd.org")',
+    update: {
+      // name: 'lizheming111',
+      title: { a: 1 }
+    },
+    lock: 'lock',
+    comment: 'comment'
+  };
+
+  instance.parseLock = instance.parseComment = () => '';
+
+  t.is(
+    instance.buildInsertSql(options),
+    'INSERT INTO `user` (id, name, email) VALUES (1, "lizheming", "i@imnerd.org")'
+  );
 });
