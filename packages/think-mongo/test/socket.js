fee32a67505b96a7b0441042878bd8f34277228b
wrq <likegun94@gmail.com>
2019-02-26 09:47:11 +0800

    socket.js中pool大小取值顺序改为this.config.options.poolSize -> this.config.options.maxPoolSize -> this.config.connectionLimit -> 5

--- packages/think-mongo/test/socket.js
+++ packages/think-mongo/test/socket.js
@@ -23,25 +23,47 @@ test.serial('close', async t => {
   const ret = await socket.close(connection);
 });
 
-const connectionLimitConfig = {
-  database: 'think_db',
-  connectionLimit: 5
-};
-test.serial('pool size is config.connectionLimitConfig', async t => {
-  const socket = Socket.getInstance(connectionLimitConfig);
+test.serial('default value of pool size is 5', async t => {
+  const socket = Socket.getInstance(defaultConfig);
   const pool = await socket.pool;
   t.is(pool.max, 5);
 });
 
+const connectionLimitConfig = {
+  database: 'think_db',
+  connectionLimit: 10
+};
+
+test.serial('pool size is config.connectionLimitConfig', async t => {
+  const socket = Socket.getInstance(connectionLimitConfig);
+  const pool = await socket.pool;
+  t.is(pool.max, 10);
+});
+
+const maxPoolSizeConfig = {
+  database: 'think_db',
+  connectionLimit: 10,
+  options: {
+    maxPoolSize: 15
+  }
+};
+
+test.serial('pool size is config.options.maxPoolSize', async t => {
+  const socket = Socket.getInstance(maxPoolSizeConfig);
+  const pool = await socket.pool;
+  t.is(pool.max, 15);
+});
+
 const poolSizeConfig = {
   database: 'think_db',
-  connectionLimit: 5,
+  connectionLimit: 10,
   options: {
-    poolSize: 10
+    maxPoolSize: 15,
+    poolSize: 20
   }
 };
 test.serial('pool size is config.options.poolSize', async t => {
   const socket = Socket.getInstance(poolSizeConfig);
   const pool = await socket.pool;
-  t.is(pool.max, 10);
+  t.is(pool.max, 20);
 });
