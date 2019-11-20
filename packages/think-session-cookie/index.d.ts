46f0dd4b5826e42850202ac93a28bfaf35b38b21
lushijie <lushijie03@meituan.com>
2019-11-20 14:17:15 +0800

    add index.d.ts

---
+++ packages/think-session-cookie/index.d.ts
@@ -0,0 +1,9 @@
+declare class ThinkSessionCookie {
+  constructor(options: object, ctx: object, cookieOptions?: object)
+  autoUpdate() : void
+  get(name: string): Promise<any>
+  set(name: string, value): Promise<any>
+  delete(): Promise<any>
+}
+
+export = ThinkSessionCookie;
