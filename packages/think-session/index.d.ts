bd8860fbf6915b058ed889afc1d918a4929c8533
lushijie <lushijie03@meituan.com>
2019-11-20 13:59:55 +0800

    update index.d.ts

--- packages/think-session/index.d.ts
+++ packages/think-session/index.d.ts
@@ -1,30 +1,33 @@
+interface ThinkRedisCtx {
+  /**
+   * get session
+   * @memberOf SessionExtend
+   */
+  session(name: string): Promise<string>;
+  /**
+   * set session
+   * @memberOf SessionExtend
+   */
+  session(name: string, value: string): Promise<string>;
+
+  /**
+   * delete the whole session
+   * @memberOf SessionExtend
+   */
+  session(name: null): Promise<string>;
+}
+
 declare module 'thinkjs' {
-  interface Context extends ThinkSession.SessionExtend {
+  interface Context extends ThinkRedisCtx {
   }
 
-  interface Controller extends ThinkSession.SessionExtend {
+  interface Controller extends ThinkRedisCtx {
   }
 }
 
 declare namespace ThinkSession {
-  interface SessionExtend {
-    /**
-     * get session
-     * @memberOf SessionExtend
-     */
-    session(name: string): Promise<string>;
-    /**
-     * set session
-     * @memberOf SessionExtend
-     */
-    session(name: string, value: string): Promise<string>;
-
-    /**
-     * delete the whole session
-     * @memberOf SessionExtend
-     */
-    session(name: null): Promise<string>;
-  }
+  const controller: ThinkRedisCtx
+  const context: ThinkRedisCtx
 }
 
 export = ThinkSession;
