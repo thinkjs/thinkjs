07b2368ad0e54a29943b47212def01b3f1575ee0
lushijie <lushijie03@meituan.com>
2019-11-20 15:16:36 +0800

    update index.d.ts

--- packages/think-i18n/index.d.ts
+++ packages/think-i18n/index.d.ts
@@ -1,14 +1,18 @@
-declare module 'thinkjs' {
-  interface Controller {
-    getI18n(forceLocale?: string): object;
+interface ThinkI18nCtx {
+  getI18n(forceLocale?: string): object;
 
-    /**
-     * get current locale
-     * @memberOf I18NExtend
-     */
-    getLocale(): Array<string>;
-  }
+  /**
+   * get current locale
+   * @memberOf I18NExtend
+   */
+  getLocale(): Array<string>;
 }
 
-declare namespace ThinkI18N {}
+declare module 'thinkjs' {
+  interface Controller extends ThinkI18nCtx {}
+}
+
+declare namespace ThinkI18N {
+  const controller: ThinkI18nCtx
+}
 export = ThinkI18N;
