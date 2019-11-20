da20c7b64a97dc23ee68597334086e289d592724
lushijie <lushijie03@meituan.com>
2019-11-20 15:02:16 +0800

    update index.d.ts

--- packages/think-websocket/index.d.ts
+++ packages/think-websocket/index.d.ts
@@ -1,16 +1,21 @@
+interface ThinkWebsocketCtx {
+  readonly data: any;
+  readonly wsData: any;
+  readonly wsCallback: any;
+  readonly websocket: any;
+  readonly isWebsocket: boolean;
+
+  emit(event: string, data: any): void;
+  broadcast(event: string, data: any): void;
+}
+
 declare module 'thinkjs' {
-  interface Controller extends ThinkWebsocket.WebsocketExtend {}
-  interface Context extends ThinkWebsocket.WebsocketExtend {}
+  interface Controller extends ThinkWebsocketCtx {}
+  interface Context extends ThinkWebsocketCtx {}
 }
 
 declare namespace ThinkWebsocket {
-  interface WebsocketExtend {
-    readonly wsData: any;
-    readonly websocket: any;
-    readonly isWebsocket: boolean;
-
-    emit(event: string, data: any): void;
-    broadcast(event: string, data: any): void;
-  }
+  const controller: ThinkWebsocketCtx
+  const context: ThinkWebsocketCtx
 }
 export = ThinkWebsocket;
