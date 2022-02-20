d40eafe838efcf78eeb102e7c06efd50f83419ad
陌生人 <moshengrenx@foxmail.com>
2022-02-21 07:03:55 +0800

    导入fs模块时增加兼容低版本的判断

--- packages/think-store-file/index.js
+++ packages/think-store-file/index.js
@@ -9,7 +9,7 @@ const path = require('path');
 const assert = require('assert');
 const helper = require('think-helper');
 const Debounce = require('think-debounce');
-const { readFile, writeFile, unlink, appendFile, access, stat } = require('fs/promises');
+const { readFile, writeFile, unlink, appendFile, access, stat } = process.versions.node.split('.')[0] >= 14 ? require('fs/promises') : require('fs').promises;
 
 const debounceInstance = new Debounce();
 const getFilePath = Symbol('think-get-file-path');
@@ -18,124 +18,124 @@ const getFilePath = Symbol('think-get-file-path');
  * file store
  */
 class FileStore {
-  /**
+    /**
      * constructor
      * @param {String} storePath store file root path
      */
-  constructor(storePath) {
-    assert(storePath && path.isAbsolute(storePath), 'storePath need be an absolute path');
-    this.storePath = storePath;
-  }
+    constructor(storePath) {
+        assert(storePath && path.isAbsolute(storePath), 'storePath need be an absolute path');
+        this.storePath = storePath;
+    }
 
-  /**
+    /**
      * get file path
      * @param  {String} relativePath [description]
      * @return {String}     [description]
      */
-  [getFilePath](relativePath) {
-    const filePath = path.join(this.storePath, relativePath);
-    assert(filePath.indexOf(this.storePath) === 0, 'the file should be in storePath');
-    return filePath;
-  }
+    [getFilePath](relativePath) {
+        const filePath = path.join(this.storePath, relativePath);
+        assert(filePath.indexOf(this.storePath) === 0, 'the file should be in storePath');
+        return filePath;
+    }
 
-  /**
+    /**
      * get file data
      * @param  {String} relativePath   [relativePath]
      * @param  {Number} times [try times when can not get file content]
      * @return {Promise}       []
      */
-  get(relativePath) {
-    const filePath = this[getFilePath](relativePath);
-    if (!helper.isFile(filePath)) {
-      return Promise.resolve();
+    get(relativePath) {
+        const filePath = this[getFilePath](relativePath);
+        if (!helper.isFile(filePath)) {
+            return Promise.resolve();
+        }
+
+        function getFileContent(times = 1) {
+            return readFile(filePath, { encoding: 'utf8' }).then(content => {
+                if (!content && times <= 3) {
+                    return Promise.reject(new Error(`content empty, file path is ${filePath}`));
+                }
+                return content;
+            }).catch(err => {
+                if (times <= 3) {
+                    return helper.timeout(10).then(() => {
+                        return getFileContent(times + 1);
+                    });
+                }
+                return Promise.reject(err);
+            });
+        }
+
+        return debounceInstance.debounce(filePath, () => getFileContent());
     }
 
-    function getFileContent(times = 1) {
-      return readFile(filePath, { encoding: 'utf8' }).then(content => {
-        if (!content && times <= 3) {
-          return Promise.reject(new Error(`content empty, file path is ${filePath}`));
-        }
-        return content;
-      }).catch(err => {
-        if (times <= 3) {
-          return helper.timeout(10).then(() => {
-            return getFileContent(times + 1);
-          });
-        }
-        return Promise.reject(err);
-      });
-    }
-
-    return debounceInstance.debounce(filePath, () => getFileContent());
-  }
-
-  /**
+    /**
      * set file content
      * @param {String} relativePath     [relativePath]
      * @param {String} content []
      */
-  set(relativePath, content) {
-    const filePath = this[getFilePath](relativePath);
-    helper.mkdir(path.dirname(filePath));
-    return writeFile(filePath, content).then(() => {
-      return helper.chmod(filePath);
-    });
-  }
+    set(relativePath, content) {
+        const filePath = this[getFilePath](relativePath);
+        helper.mkdir(path.dirname(filePath));
+        return writeFile(filePath, content).then(() => {
+            return helper.chmod(filePath);
+        });
+    }
 
-  /**
+    /**
      * delete file
      * @param  {String} relativePath [relativePath]
      * @return {Promise}     []
      */
-  delete(relativePath) {
-    const filePath = this[getFilePath](relativePath);
-    if (!helper.isFile(filePath)) {
-      return Promise.resolve();
+    delete(relativePath) {
+        const filePath = this[getFilePath](relativePath);
+        if (!helper.isFile(filePath)) {
+            return Promise.resolve();
+        }
+
+        // mutiple process will cause error if just unlink(filePath);
+        return unlink(filePath).catch(err => {
+            if (err.code === 'ENOENT') return;
+            return Promise.reject(err);
+        });
     }
 
-    // mutiple process will cause error if just unlink(filePath);
-    return unlink(filePath).catch(err => {
-      if (err.code === 'ENOENT') return;
-      return Promise.reject(err);
-    });
-  }
-
-  /**
+    /**
      * append file content
      * @param  {String} relativePath [relativePath]
      * @param  {String | Buffer} data [String or Buffer data]
      * @return {Promise}     []
      */
-  append(relativePath, data) {
-    const filePath = this[getFilePath](relativePath);
-    if (!helper.isFile(filePath)) {
-      return Promise.resolve();
+    append(relativePath, data) {
+        const filePath = this[getFilePath](relativePath);
+        if (!helper.isFile(filePath)) {
+            return Promise.resolve();
+        }
+        return appendFile(filePath, data);
     }
-    return appendFile(filePath, data);
-  }
 
-  /**
+    /**
      * Does the file exist
      * @param  {String} relativePath [relativePath]
      * @return {Promise}     []
      */
-  has(relativePath) {
-    const filePath = this[getFilePath](relativePath);
-    return access(filePath, fs.constants.F_OK);
-  }
+    has(relativePath) {
+        const filePath = this[getFilePath](relativePath);
+        return access(filePath, fs.constants.F_OK);
+    }
 
-  /**
+    /**
      * get file info
      * @param  {String} relativePath [relativePath]
      * @return {Promise}     []
      */
-  info(relativePath) {
-    const filePath = this[getFilePath](relativePath);
-    if (!helper.isFile(filePath)) {
-      return Promise.resolve();
+    info(relativePath) {
+        const filePath = this[getFilePath](relativePath);
+        if (!helper.isFile(filePath)) {
+            return Promise.resolve();
+        }
+        return stat(filePath);
     }
-    return stat(filePath);
-  }
 }
 
 module.exports = FileStore;
